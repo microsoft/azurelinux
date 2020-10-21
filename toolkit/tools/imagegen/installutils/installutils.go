6fafcbafe5653100ee002bdce5dc085feed6dd22
MateuszMalisz <maliszmat@outlook.com>
2020-10-21 23:35:52 +0000

    Implement basic chage. Fix shadow update for when shadow DNE.

--- toolkit/tools/imagegen/installutils/installutils.go
+++ toolkit/tools/imagegen/installutils/installutils.go
@@ -36,6 +36,7 @@ const (
 	// /boot directory should be only accesible by root. The directories need the execute bit as well.
 	bootDirectoryFileMode = 0600
 	bootDirectoryDirMode  = 0700
+	shadowFile            = "/etc/shadow"
 )
 
 // PackageList represents the list of packages to install into an image
@@ -847,8 +848,15 @@ func addUsers(installChroot *safechroot.Chroot, users []configuration.User) (err
 	// If no root entry was specified in the config file, never expire the root password
 	if !rootUserAdded {
 		logger.Log.Debugf("No root user entry found in config file. Setting root password to never expire.")
+
+		// Ignore updating if there is no shadow file to update
+		if _, err2 := os.Stat(shadowFile); err2 != nil {
+			logger.Log.Debugf("No shadow file to update. Skipping.")
+			return
+		}
+
 		err = installChroot.UnsafeRun(func() error {
-			return shell.ExecuteLive(squashErrors, "chage", "-M", "-1", "root")
+			return chage("-1", "root")
 		})
 	}
 	return
@@ -904,8 +912,16 @@ func createUserWithPassword(installChroot *safechroot.Chroot, user configuration
 			logger.Log.Warnf("Ignoring UID for (%s) user, using default", rootUser)
 		}
 
-		// Update shadow file
-		err = updateUserPassword(installChroot.RootDir(), user.Name, hashedPassword)
+		if _, err2 := os.Stat(shadowFile); err2 != nil {
+			logger.Log.Debugf("No shadow file to update. Skipping.")
+		} else {
+			// Update shadow file
+			err = updateUserPassword(installChroot.RootDir(), user.Name, hashedPassword)
+			if err != nil {
+				logger.Log.Warnf("Encountered a problem when updating root user password: %s", err)
+				return
+			}
+		}
 		isRoot = true
 	} else {
 		homeDir = filepath.Join(userHomeDirPrefix, user.Name)
@@ -926,12 +942,108 @@ func createUserWithPassword(installChroot *safechroot.Chroot, user configuration
 
 	// Update password expiration
 	if user.PasswordExpiresDays != 0 {
+		// Ignore updating if there is no shadow file to update
+		if _, err2 := os.Stat(shadowFile); err2 != nil {
+			logger.Log.Debugf("No shadow file to update. Skipping.")
+			return
+		}
+
 		err = installChroot.UnsafeRun(func() error {
-			return shell.ExecuteLive(squashErrors, "chage", "-M", strconv.FormatUint(user.PasswordExpiresDays, passwordExpiresBase), user.Name)
+			return chage(strconv.FormatUint(user.PasswordExpiresDays, passwordExpiresBase), user.Name)
 		})
 	}
 
 	return
+}
+
+// chage works in the same way as invoking "chage -M passwordExpirationInDays username"
+// i.e. it sets the maximum password expiration date.
+func chage(passwordExpirationInDays string, username string) error {
+	var (
+		shadow             []string
+		passwordExpiration int64
+		err                error
+		usernameWithColon  = fmt.Sprintf("%s:", username)
+	)
+
+	shadow, err = file.ReadLines(shadowFile)
+	if err != nil {
+		return err
+	}
+
+	passwordExpiration, err = strconv.ParseInt(passwordExpirationInDays, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	for n, entry := range shadow {
+		done := false
+		// Entries in shadow are separated by colon and start with a username
+		// Finding one that starts like that means we've found our entry
+		if strings.HasPrefix(entry, usernameWithColon) {
+			// Each line in shadow contains 9 fields separated by colon ("") in the following order:
+			// login name, encrypted password, date of last password change,
+			// minimum password age, maximum password age, password warning period,
+			// password inactivity period, account expiration date, reserved field for future use
+			const (
+				loginNameField         = 0
+				encryptedPasswordField = 1
+				passwordChangedField   = 2
+				minPasswordAgeField    = 3
+				maxPasswordAgeField    = 4
+				warnPeriodField        = 5
+				inactivityPeriodField  = 6
+				expirationField        = 7
+				reservedField          = 8
+			)
+
+			fields := strings.Split(entry, ":")
+			// Any value other than 9 indicates error in parsing
+			if len(fields) != 9 {
+				return fmt.Errorf(`invalid shadow entry "%v" for user "%s": 9 fields expected, but %d found.`, fields, username, len(fields))
+			}
+
+			if passwordExpiration == -1 {
+				// If passwordExpiration is equal to -1, it means that password never expires.
+				// This is expressed by leaving account expiration date field (and fields after it) empty.
+				for _, fieldToChange := range []int{maxPasswordAgeField, warnPeriodField, inactivityPeriodField, expirationField, reservedField} {
+					fields[fieldToChange] = ""
+				}
+				// Each user is unique, so we are done here; save the changes and exit.
+				done = true
+			} else if passwordExpiration < -1 {
+				// Values smaller than -1 make no sense
+				return fmt.Errorf(`invalid value for maximum user's "%s" password expiration: %d`, username, passwordExpiration)
+			} else {
+				// If passwordExpiration has any other value, it's the maximum expiration date: set it accordingly
+				// To do so, we need to ensure that passwordChangedField holds a valid value and then sum it with passwordExpiration.
+				var (
+					passwordAge     int64
+					passwordChanged = fields[passwordChangedField]
+				)
+
+				if passwordChanged == "" {
+					// Set to the number of days since epoch
+					fields[passwordChangedField] = fmt.Sprintf("%d", int64(time.Since(time.Unix(0, 0)).Hours()/24))
+				}
+				passwordAge, err = strconv.ParseInt(fields[passwordChangedField], 10, 64)
+				if err != nil {
+					return err
+				}
+				fields[expirationField] = fmt.Sprintf("%d", passwordAge+passwordExpiration)
+
+				//Each user is unique, so we are done here; save the changes and exit.
+				done = true
+			}
+			if done {
+				shadow[n] = strings.Join(fields, ":")
+				err = file.Write(strings.Join(shadow, "\n"), shadowFile)
+				return err
+			}
+		}
+	}
+
+	return fmt.Errorf(`user "%s" not found when trying to change the password expiration date`, username)
 }
 
 func configureUserGroupMembership(installChroot *safechroot.Chroot, user configuration.User) (err error) {
@@ -1040,14 +1152,11 @@ func provisionUserSSHCerts(installChroot *safechroot.Chroot, user configuration.
 }
 
 func updateUserPassword(installRoot, username, password string) (err error) {
-	const (
-		shadowFilePath = "etc/shadow"
-		sedDelimiter   = "|"
-	)
+	const sedDelimiter = "|"
 
 	findPattern := fmt.Sprintf("%v:x:", username)
 	replacePattern := fmt.Sprintf("%v:%v:", username, password)
-	filePath := filepath.Join(installRoot, shadowFilePath)
+	filePath := filepath.Join(installRoot, shadowFile)
 	err = sed(findPattern, replacePattern, sedDelimiter, filePath)
 	if err != nil {
 		logger.Log.Warnf("Failed to write hashed password to shadow file")
