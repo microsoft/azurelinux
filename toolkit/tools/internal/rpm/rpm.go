c3eb125bf7a7192e737bf754dbe133b8ac37caf6
Joe Schmitt <joschmit@microsoft.com>
2020-10-23 16:02:32 -0700

    Add CreateCollapedNode

--- toolkit/tools/internal/rpm/rpm.go
+++ toolkit/tools/internal/rpm/rpm.go
@@ -232,7 +232,7 @@ func InstallRPM(rpmFile string) (err error) {
 }
 
 // QueryRPMProvides returns what an RPM file provides.
-// This includes any provides made by a generator and any files provided by the rpm.
+// This includes any provides made by a generator and files provided by the rpm.
 func QueryRPMProvides(rpmFile string) (provides []string, err error) {
 	// the l option in rpm returns a superset of generator provides and files installed by the rpm.
 	const queryProvidesOption = "-qlp"
