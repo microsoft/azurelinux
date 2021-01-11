002f43f0d4fdbe89b87d9df0a22b261c46261c23
Thomas Crain <thcrain@microsoft.com>
2021-01-11 13:05:35 -0800

    Suggested fixes- use tag terminology

--- .github/workflows/check_entangled_specs.py
+++ .github/workflows/check_entangled_specs.py
@@ -30,30 +30,30 @@ version_matching_groups = [
 ]
 
 
-def check_spec_attributes(base_path: str, attributes: List[str], groups: List[FrozenSet]) -> Set[FrozenSet]:
-    """Returns spec sets which violate matching rules for given attributes"""
+def check_spec_tags(base_path: str, tags: List[str], groups: List[FrozenSet]) -> Set[FrozenSet]:
+    """Returns spec sets which violate matching rules for given tags. """
     err_groups = set()
     for group in groups:
         variants = defaultdict(set)
 
         for spec_filename in group:
             parsed_spec = Spec.from_file(Path(base_path, spec_filename))
-            for attribute in attributes:
-                variants[attribute].add(getattr(
-                    parsed_spec, attribute))
+            for tag in tags:
+                variants[tag].add(getattr(
+                    parsed_spec, tag))
 
-        for attribute in attributes:
-            if len(variants[attribute]) > 1:
+        for tag in tags:
+            if len(variants[tag]) > 1:
                 err_groups.add(group)
     return err_groups
 
 
 def check_version_release_match_groups(base_path: str) -> Set[FrozenSet]:
-    return check_spec_attributes(base_path, ['version', 'release'], version_release_matching_groups)
+    return check_spec_tags(base_path, ['version', 'release'], version_release_matching_groups)
 
 
 def check_version_match_groups(base_path: str) -> Set[FrozenSet]:
-    return check_spec_attributes(base_path, ['version'], version_matching_groups)
+    return check_spec_tags(base_path, ['version'], version_matching_groups)
 
 
 def check_matches(base_path: str):
@@ -68,13 +68,13 @@ def check_matches(base_path: str):
 
         if len(version_match_errors):
             print(
-                '\nPlease update the following sets of specs to have the same version attributes:')
+                '\nPlease update the following sets of specs to have the same Version tags:')
             for e in version_match_errors:
                 printer.pprint(e)
 
         if len(version_release_match_errors):
             print(
-                '\nPlease update the following sets of specs to have the same version and release attributes:')
+                '\nPlease update the following sets of specs to have the same Version and Release tags:')
             for e in version_release_match_errors:
                 printer.pprint(e)
         sys.exit(1)
