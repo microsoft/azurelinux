4ab787dab034708a93344f979bc9505eb2633884
Christopher Co <christopher.co@microsoft.com>
2021-02-17 16:49:03 -0800

    create minimal aarch64 image that boots in qemu (#628)
    Update the kernel_cross script to also install the kernel image
    into a standalone kernel folder under /opt/cross/kernel
    
    Create a custom inittab and use busybox init. Add script to
    generate a minimal rootfs from the contents of sysroot. Compress
    the rootfs into a gzipped cpio archive which qemu can easily
    ingest as an initrd.
    
    Add a script to launch qemu-system-aarch64 with the special
    settings needed to boot our standalone kernel and rootfs as an
    initrd.
    
    Signed-off-by: Chris Co <chrco@microsoft.com>

---
+++ toolkit/scripts/toolchain/cross/launch_qemu.sh
@@ -0,0 +1,34 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Currently assumes you have already run busybox_cross.sh, kernel_cross.sh, and generate_minimal_rootfs.sh
+
+# Kernel should be located at /opt/cross/kernel
+# Rootfs should be located as a cpio.gz in /opt/cross/rootfs
+
+installDir="/opt/cross"
+kernelStandaloneInstallDir=${installDir}/kernel
+standaloneRootfsDir=${installDir}/rootfs
+kernelVersion="5.4.83"
+
+# Note: Current rootfs unpacks to be fairly big. 2GB memory was not enough size to hold the unpacked initramfs
+# so when QEMU ran, it would fail with:
+#
+# [   16.532507] ---[ end Kernel panic - not syncing: VFS: Unable to mount root fs on unknown-block(0,0) ]---
+#
+# But this is a lie - the true error is immediately after the unpacking initramfs message:
+#
+# [    0.321246] Unpacking initramfs...
+# [   15.549898] Initramfs unpacking failed: write error
+#
+# 4GB can hold the current rootfs. But tweak the -m parameter as necessary.
+qemu-system-aarch64 \
+    -M virt \
+    -cpu cortex-a57 \
+    -machine type=virt \
+    -smp 1 \
+    -m 4096 \
+    -kernel ${kernelStandaloneInstallDir}/vmlinuz-${kernelVersion} \
+    -initrd ${standaloneRootfsDir}/rootfs.cpio.gz \
+    -nographic \
+    -append "console=ttyAMA0"
