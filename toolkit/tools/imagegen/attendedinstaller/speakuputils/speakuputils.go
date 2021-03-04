42affddd168258c7d1b7489e03323beaa65e30d1
Thomas Crain <thcrain@microsoft.com>
2021-03-03 18:00:44 -0600

    Enable highlight tracking mode in speakup

--- toolkit/tools/imagegen/attendedinstaller/speakuputils/speakuputils.go
+++ toolkit/tools/imagegen/attendedinstaller/speakuputils/speakuputils.go
@@ -7,11 +7,22 @@ import (
 	"github.com/bendahl/uinput"
 )
 
+// CreateVirtualKeyboard creates and returns a virtual keyboard from the uinput package
 func CreateVirtualKeyboard() (keyboard uinput.Keyboard, err error) {
 	keyboard, err = uinput.CreateKeyboard("/dev/uinput", []byte("MarinerVirtualKeyboard"))
 	return
 }
 
+// SetHighlightTrackingMode is used once at startup to enable speakup's highlight tracking mode
+func SetHighlightTrackingMode(k uinput.Keyboard) (err error) {
+	if k == nil {
+		return
+	}
+	err = k.KeyPress(uinput.KeyKpasterisk)
+	return
+}
+
+// ClearSpeakupBuffer sends keypresses that will clear the speakup buffer
 func ClearSpeakupBuffer(k uinput.Keyboard) (err error) {
 	if k == nil {
 		return
@@ -20,6 +31,7 @@ func ClearSpeakupBuffer(k uinput.Keyboard) (err error) {
 	if err != nil {
 		return err
 	}
-	err = k.KeyPress(uinput.KeyKpenter)
+	// This could be any key that doesn't trigger input to a field
+	err = k.KeyPress(uinput.KeyRightctrl)
 	return
 }
