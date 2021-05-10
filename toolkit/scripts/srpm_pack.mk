f4e760aa5436a30a492e99e5aa68cb832a91a490
Mateusz Malisz <maliszmat@outlook.com>
2021-05-10 05:30:48 -0700

    Allow for SRPM to fail to download.

--- toolkit/scripts/srpm_pack.mk
+++ toolkit/scripts/srpm_pack.mk
@@ -41,7 +41,6 @@ ifeq ($(DOWNLOAD_SRPMS),y)
 
 .SILENT: $(STATUS_FLAGS_DIR)/build_srpms.flag
 
-#FIXME: rpmspec query below does not take srpm file into account
 $(STATUS_FLAGS_DIR)/build_srpms.flag: $(local_specs) $(local_spec_dirs) $(SPECS_DIR)
 	for spec in $(local_specs); do \
 		spec_file=$${spec} && \
@@ -56,7 +55,10 @@ $(STATUS_FLAGS_DIR)/build_srpms.flag: $(local_specs) $(local_spec_dirs) $(SPECS_
 			touch $(BUILD_SRPMS_DIR)/$${srpm_file} && \
 			break; \
 		done && echo "Downloaded $${url}/$${srpm_file}"; \
-	done || ( [ -f $(BUILD_SRPMS_DIR)/$${srpm_file} ] || $(call print_error,Failed to download $${srpm_file}) ); \
+	done || $(call print_error,Failed to download $${srpm_file}) ; \
+	echo "Removing empty (failed) SRPMS: "
+	find $(BUILD_SRPMS_DIR) -type f -empty -delete -print > $(LOGS_DIR)/pkggen/deleted-srpms.log
+	echo "Removed all empty SRPMS. Finished packing."
 	touch $@
 else
 $(STATUS_FLAGS_DIR)/build_srpms.flag: $(local_specs) $(local_spec_dirs) $(local_sources) $(SPECS_DIR) $(go-srpmpacker)
