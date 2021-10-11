13ca6cf39c83053149953b286916ddc51bf9d5af
Pawel Winogrodzki <pawelwi@microsoft.com>
2021-10-11 16:02:25 -0700

    Fixing parsing of the `ExclusiveArch` tag (#1511)

---
+++ toolkit/tools/internal/rpm/rpm_test.go
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package rpm
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"microsoft.com/pkggen/internal/logger"
+)
+
+const specsDir = "testdata"
+
+var defines = map[string]string{
+	"dist":       ".cmX",
+	"with_check": "1",
+}
+
+func TestMain(m *testing.M) {
+	logger.InitStderrLog()
+	os.Exit(m.Run())
+}
+
+func TestShouldSucceedForSupportedArchitectures(t *testing.T) {
+	specFilePath := filepath.Join(specsDir, "supported_unsupported_architectures.spec")
+
+	matches, err := SpecArchitectureMatchesCurrent(specFilePath, specsDir, defines)
+	assert.NoError(t, err)
+	assert.True(t, matches)
+}
+
+func TestShouldFailForUnsupportedArchitectures(t *testing.T) {
+	specFilePath := filepath.Join(specsDir, "unsupported_architectures.spec")
+
+	matches, err := SpecArchitectureMatchesCurrent(specFilePath, specsDir, defines)
+	assert.NoError(t, err)
+	assert.False(t, matches)
+}
