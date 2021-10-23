111099fc92acd7e63b8964bdc30223d863b32245
Andrew Phelps <anphel@microsoft.com>
2021-10-23 11:04:59 +0000

    remove findutils test change

--- toolkit/scripts/toolchain/container/toolchain_build_in_chroot.sh
+++ toolkit/scripts/toolchain/container/toolchain_build_in_chroot.sh
@@ -860,7 +860,7 @@ touch /logs/status_gawk_complete
 echo Findutils-4.8.0
 tar xf findutils-4.8.0.tar.xz
 pushd findutils-4.8.0
-sed -i 's/test-lock..EXEEXT.//' tests/Makefile.in
+#sed -i 's/test-lock..EXEEXT.//' tests/Makefile.in
 #sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' gl/lib/*.c
 #sed -i '/unistd/a #include <sys/sysmacros.h>' gl/lib/mountlist.c
 #echo "#define _IO_IN_BACKUP 0x100" >> gl/lib/stdio-impl.h
