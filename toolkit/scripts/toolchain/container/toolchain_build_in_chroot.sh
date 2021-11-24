86d684649645566cecfa7827a2520d2c69606f10
Max Brodeur-Urbas <35381493+MaxBrodeurUrbas@users.noreply.github.com>
2021-11-24 12:19:54 -0800

    Updating expat to v.2.4.1 (#1667)
    * upgrading expat to 2.4.1
    
    * updating references to expat 2.4.1
    
    * updating changelog
    
    * linting applied
    
    * cleaning up expat.spec
    
    * fixing changelog, adding global variable for underscored version
    
    * removing useless toolchain-sha256sums entry

--- toolkit/scripts/toolchain/container/toolchain_build_in_chroot.sh
+++ toolkit/scripts/toolchain/container/toolchain_build_in_chroot.sh
@@ -588,17 +588,17 @@ popd
 rm -rf gperf-3.1
 touch /logs/status_gperf_complete
 
-echo Expat-2.2.6
-tar xf expat-2.2.6.tar.bz2
-pushd expat-2.2.6
+echo Expat-2.4.1
+tar xf expat-2.4.1.tar.bz2
+pushd expat-2.4.1
 sed -i 's|usr/bin/env |bin/|' run.sh.in
 ./configure --prefix=/usr    \
             --disable-static \
-            --docdir=/usr/share/doc/expat-2.2.6
+            --docdir=/usr/share/doc/expat-2.4.1
 make -j$(nproc)
 make install
 popd
-rm -rf expat-2.2.6
+rm -rf expat-2.4.1
 touch /logs/status_expat_complete
 
 echo Perl-5.32.0
