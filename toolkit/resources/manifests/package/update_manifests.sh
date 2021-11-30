481ae1f9f67804be5317f93bd5af639f72e3efed
Andrew Phelps <anphel@microsoft.com>
2021-11-30 01:22:02 +0000

    add libnsl2 and libtirpc to pkggen_core

--- toolkit/resources/manifests/package/update_manifests.sh
+++ toolkit/resources/manifests/package/update_manifests.sh
@@ -76,9 +76,11 @@ remove_packages_for_pkggen_core () {
     sed -i '/libdb-utils/d' $TmpPkgGen
     sed -i '/libgpg-error-[[:alpha:]]/d' $TmpPkgGen
     sed -i '/libgcrypt-[[:alpha:]]/d' $TmpPkgGen
+    sed -i '/libnsl2-[[:alpha:]]/d' $TmpPkgGen
     sed -i '/libselinux-[[:alpha:]]/d' $TmpPkgGen
     sed -i '/libsepol-[[:alpha:]]/d' $TmpPkgGen
     sed -i '/libsolv-tools/d' $TmpPkgGen
+    sed -i '/libtirpc-[[:alpha:]]/d' $TmpPkgGen
     sed -i '/libxslt/d' $TmpPkgGen
     sed -i '/Linux-PAM/d' $TmpPkgGen
     sed -i '/lua-devel/d' $TmpPkgGen
@@ -257,6 +259,8 @@ generate_pkggen_core () {
         grep "^expat-" $TmpPkgGen
         grep "^libpipeline-" $TmpPkgGen
         grep "^gdbm-" $TmpPkgGen
+        grep "^libnsl2-" $TmpPkgGen
+        grep "^libtirpc-" $TmpPkgGen
         grep "^perl-" $TmpPkgGen
         grep "^texinfo-" $TmpPkgGen
         grep "^autoconf-" $TmpPkgGen
