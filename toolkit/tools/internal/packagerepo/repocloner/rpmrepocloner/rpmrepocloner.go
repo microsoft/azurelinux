6303761f70c04bffbae3c12b29f1abafece4d989
Andrew Phelps <anphel@microsoft.com>
2021-12-10 12:48:58 +0000

    skip jdk11 dependencies in rpmrepocloner.go

--- toolkit/tools/internal/packagerepo/repocloner/rpmrepocloner/rpmrepocloner.go
+++ toolkit/tools/internal/packagerepo/repocloner/rpmrepocloner/rpmrepocloner.go
@@ -270,10 +270,15 @@ func (r *RpmRepoCloner) Clone(cloneDeps bool, packagesToClone ...*pkgjson.Packag
 			pkgName,
 		}
 
-		if cloneDeps {
-			args = append([]string{"download", "--alldeps"}, args...)
-		} else {
+		// special case to skip dependencies for msopenjdk-11
+		if (pkgName == "msopenjdk-11-11.0.13+8_LTS-4.x86_64") || (pkgName == "msopenjdk-11-11.0.13+8-LTS-4.aarch64") {
 			args = append([]string{"download-nodeps"}, args...)
+		} else {
+			if cloneDeps {
+				args = append([]string{"download", "--alldeps"}, args...)
+			} else {
+				args = append([]string{"download-nodeps"}, args...)
+			}
 		}
 
 		err = r.chroot.Run(func() (err error) {
