a8ccdd4d943ef194507858a9286f89cc86ab6d4d
Pawel Winogrodzki <pawelwi@microsoft.com>
2022-02-07 21:44:43 -0800

    Removing unused 'php' packages.

--- SPECS-EXTENDED/php-pear/pecl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-exec /usr/bin/php -C \
-    -d include_path=/usr/share/pear \
-    -d date.timezone=UTC \
-    -d output_buffering=1 \
-    -d variables_order=EGPCS \
-    -d safe_mode=0 \
-    -d register_argc_argv="On" \
-    /usr/share/pear/peclcmd.php "$@"
