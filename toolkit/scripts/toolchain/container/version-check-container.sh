4bfd5d219c446d8b2214394e1c9ffb6967656d48
Andrew Phelps <anphel@microsoft.com>
2022-03-05 21:35:23 +0000

    switch to mariner container

--- toolkit/scripts/toolchain/container/version-check-container.sh
+++ toolkit/scripts/toolchain/container/version-check-container.sh
@@ -16,7 +16,7 @@ bison --version | head -n1
 if [ -h /usr/bin/yacc ]; then
   echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`";
 elif [ -x /usr/bin/yacc ]; then
-  echo yacc is `/usr/bin/yacc --version | head -n1`
+  echo yacc is `/usr/bin/yacc -V | head -n1`
 else
   echo "yacc not found"
 fi
