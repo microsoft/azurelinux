ab5a4462bad797d89dd2180b3deb55968a9873ed
Pawel Winogrodzki <pawelwi@microsoft.com>
2022-03-14 12:15:36 -0700

    Installing missing dependencies for .

--- toolkit/scripts/toolchain/build_official_toolchain_rpms.sh
+++ toolkit/scripts/toolchain/build_official_toolchain_rpms.sh
@@ -350,12 +350,17 @@ build_rpm_in_chroot_no_install krb5
 # curl needs libssh2
 chroot_and_install_rpms libssh2
 build_rpm_in_chroot_no_install curl
+
+# libxml2 needs python-xml and python3-xml
+# python-xml is built by building python2
+# python3-xml is built by building python3
+chroot_and_install_rpms python-xml
+chroot_and_install_rpms python3-libs
+chroot_and_install_rpms python3
+chroot_and_install_rpms python3-xml
 build_rpm_in_chroot_no_install libxml2
 
 # python-setuptools needs python-xml
-# python-xml is built by building python2
-chroot_and_install_rpms python-xml
-
 # cracklib needs python-setuptools
 chroot_and_install_rpms python-setuptools
 build_rpm_in_chroot_no_install cracklib
@@ -406,7 +411,6 @@ build_rpm_in_chroot_no_install swig
 # gpgme needs swig, gnupg2 and python3
 chroot_and_install_rpms swig
 chroot_and_install_rpms gnupg2
-chroot_and_install_rpms python3
 build_rpm_in_chroot_no_install gpgme
 
 # tdnf needs python3, gpgme, curl and libsolv
