16530eeae3fb9a37875281ac02f35158d1075eb4
Olivia Crain <thcrain@microsoft.com>
2022-03-18 16:46:35 -0700

    toolchain: Remove alsa-lib (#2543)

--- toolkit/resources/manifests/package/update_manifests.sh
+++ toolkit/resources/manifests/package/update_manifests.sh
@@ -41,7 +41,6 @@ generate_toolchain () {
 
 # Remove specific packages that are not needed in pkggen_core
 remove_packages_for_pkggen_core () {
-    sed -i '/alsa-lib-/d' $TmpPkgGen
     sed -i '/audit-devel/d' $TmpPkgGen
     sed -i '/ca-certificates-legacy/d' $TmpPkgGen
     sed -i '/libtasn1-d/d' $TmpPkgGen
