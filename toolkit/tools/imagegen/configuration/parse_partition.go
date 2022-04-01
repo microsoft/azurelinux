cedec049add11f3b30c7a0ff684a3305e5b3d979
Henry Li <lihl@microsoft.com>
2022-04-01 13:11:38 -0700

    save changes for parser

--- toolkit/tools/imagegen/configuration/parse_partition.go
+++ toolkit/tools/imagegen/configuration/parse_partition.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"fmt"
 	"strings"
+	"strconv"
 
 
 	//"microsoft.com/pkggen/imagegen/configuration"
@@ -17,6 +18,15 @@ import (
 )
 
 func main() {
+
+	var {
+		parseCmd			string
+		partitionFlags		[]string
+		paritionTableType	PartitionTableType
+		curDisk				configuration.Disk
+		diskInfo			map[string]configuration.Disk
+	}
+
 	file, err := os.Open("/home/henry/git/CBL-Mariner/toolkit/tools/imagegen/configuration/parse.sh")
 	if err != nil {
 		fmt.Println("Failed to open file")
@@ -24,11 +34,8 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	diskIndex := 0
-	disks := config.Disks
-	disks[diskIndex] = PartitionTableTypeNone
-	disks[diskIndex].MaxSize = 0
-	var parseCmd string
+	disks := []configuration.Disk{}
+	diskInfo = make(map[string]configuration.Disk)
 
 	for scanner.Scan() {
 		parseCmd = scanner.Text()
@@ -37,18 +44,42 @@ func main() {
 		// Check partition table type
 		if strings.Contains(parseCmd, "gpt") {
 			fmt.Printf("Partition table type: %s\n", "gpt")
-			disks[diskIndex].PartitionTableType = PartitionTableTypeGpt	
+			partitionTableType = PartitionTableTypeGpt	
 		} else if strings.Contains(parseCmd, "mbr") {
 			fmt.Printf("Partition table type: %s\n", "mbr")
-			disks[diskIndex].PartitionTableType = PartitionTableTypeMbr	
+			partitionTableType = PartitionTableTypeMbr	
 		} else {
-			// Look for actual partition schema which starts with the "part" command
-			partitionFlags := strings.Split(parseCmd, " ")
-			if partitionFlags[0] == "part" {
-				for _, partOpt := range partitionFlags {
-					var partition Partition
-					var partitionSetting PartitionSetting
+			// Check disk information
+			if strings.Contains(parseCmd, "--ondisk") {
+				partitionFlags = strings.Split(parseCmd, " ")
+				curDiskInfo := partitionFlags[len(partitionFlags)-1]
+				curDiskInfo = curDiskInfo[9 : len(curDiskInfo)]
+				
+				// Check whether this disk already exists or not
+				val, ok := diskInfo[curDiskInfo]
+				if ok {
+					curDisk = val
+				}
+				else {
+					// Create new disk struct and append it into the Disk array
+					newDisk := configuration.Disk{}
+					diskInfo[curDiskInfo] = newDisk
+					newDisk.PartitionTableType = paritionTableType
+					disks = append(disks, newDisk)
 
+					partitions := []configuration.Partition{}
+					partitionSettings := []configuration.PartitionSetting{}
+					newDisk.Partitions = partitions
+
+					// Deal with system config here for settings
+				}
+				
+				// Create new Partition and PartionSetting
+				partition := configuration.Partition{}
+				partitionSetting := configuration.PartitionSetting{}
+
+				// Loop through partition flags to fetch fstype, size etc.
+				for _, partOpt := range partitionFlags {
 					// Find Mount Point
 					partitionSetting.MountPoint = partOpt[1]
 
@@ -61,11 +92,26 @@ func main() {
 
 					// Find partition size
 					if strings.Contains(partOpt, "--size") {
-						partSize := partOpt[7 : len(partOpt)]
-						fmt.Printf("partition size: %s\n", partSize)
+						partSizeStr := partOpt[7 : len(partOpt)]
+						fmt.Printf("partition size: %s\n", partSizeStr)
+						
+						if len(partitions) == 0 {
+							partition.Start = 1
+						} else {
+							partition.Start = partitions[len(partitions)-1].End
+						}
+
+						if strings.Contains(parseCmd, "--grow") {
+							partition.End = 0
+						} else {
+							partition.End = partition.Start + strconv.Atoi(partSizeStr)
+						}
 					}
 				}
-			}
+
+				partitions = append(partitions, partition)
+				partitionSettings = append(partitionSettings, partitionSetting)
+			} 
 		}
 	}
 
