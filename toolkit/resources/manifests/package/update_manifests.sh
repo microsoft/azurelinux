518ad76ae4366348ca6781c00aa20c8df1280710
chalamalasetty <chalamalasetty@live.com>
2022-04-18 18:05:23 -0700

    Update update_manifests script with lang packages for chkconfig and newt

--- toolkit/resources/manifests/package/update_manifests.sh
+++ toolkit/resources/manifests/package/update_manifests.sh
@@ -303,8 +303,10 @@ generate_pkggen_core () {
         grep "^which-" $TmpPkgGen
         grep "^libselinux-" $TmpPkgGen
         grep "^slang-[0-9]" $TmpPkgGen
-        grep "^newt-" $TmpPkgGen
-        grep "^chkconfig-" $TmpPkgGen
+        grep "^newt-[0-9]" $TmpPkgGen
+        grep "^newt-lang-[0-9]" $TmpPkgGen
+        grep "^chkconfig-[0-9]" $TmpPkgGen
+        grep "^chkconfig-lang-[0-9]" $TmpPkgGen
         grep "^msopenjdk-" $TmpPkgGen
         grep "^pyproject-" $TmpPkgGen
         grep "^audit-" $TmpPkgGen
