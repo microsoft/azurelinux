5d1a1dc09d5abf83f1cf6657c9b2c9dbdded6af8
Henry Li <lihl@microsoft.com>
2022-04-22 11:50:13 -0700

    remove test files

--- toolkit/tools/imagegen/configuration/parse.sh
+++
@@ -1,6 +0,0 @@
-zerombr
-clearpart --drives=sda --all
-# Not using RAID since DISK2 is not present
-ignoredisk --only-use=/dev/sda
-part biosboot --fstype=fat32 --size=1 --ondisk=/dev/sda
-part / --fstype=ext4 --size=800 --grow --ondisk=/dev/sda
