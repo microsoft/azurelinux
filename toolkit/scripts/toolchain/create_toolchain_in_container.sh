9ba4c786f3ab51887b49e189c47f8a9c527b0dc2
Andrew Phelps <anphel@microsoft.com>
2022-05-05 18:09:54 +0000

    remove ./populated_toolchain/tools/libexec/gcc

--- toolkit/scripts/toolchain/create_toolchain_in_container.sh
+++ toolkit/scripts/toolchain/create_toolchain_in_container.sh
@@ -62,7 +62,7 @@ rm -rvf ./populated_toolchain
 mv ./lfs ./populated_toolchain
 rm -rvf ./populated_toolchain/.dockerenv
 rm -rvf ./populated_toolchain/sources
-rm -rvf ./populated_toolchain/tools
+rm -rvf ./populated_toolchain/tools/libexec/gcc
 tar czf toolchain_from_container.tar.gz populated_toolchain
 ls -la ./
 ls -la ./populated_toolchain
