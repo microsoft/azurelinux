19981b0d800bd524f080ec6fc6f3aace515eda9a
Neha Agarwal <nehaagarwal@microsoft.com>
2022-05-19 16:03:57 -0700

    Remove 99-dhcp-en.network for marketplace img

--- toolkit/imageconfigs/additionalconfigs/configure-image.sh
+++ toolkit/imageconfigs/additionalconfigs/configure-image.sh
@@ -1,2 +1,6 @@
 echo Enabling service for Azure Serial Console
 systemctl enable serial-getty@ttyS0.service
+if [[ -f "/etc/systemd/network/99-dhcp-en.network" ]]; then
+    echo Removing 99-dhcp-en.network file
+    rm /etc/systemd/network/99-dhcp-en.network
+fi
