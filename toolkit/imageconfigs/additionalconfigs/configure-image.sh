ab5b29b7068f85cc12bdc93699aa7f6f51b0298a
Neha Agarwal <nehaagarwal@microsoft.com>
2022-05-20 11:58:44 -0700

    Remove contents

--- toolkit/imageconfigs/additionalconfigs/configure-image.sh
+++ toolkit/imageconfigs/additionalconfigs/configure-image.sh
@@ -1,6 +1,7 @@
 echo Enabling service for Azure Serial Console
 systemctl enable serial-getty@ttyS0.service
 if [[ -f "/etc/systemd/network/99-dhcp-en.network" ]]; then
-    echo Removing 99-dhcp-en.network file
+    echo Removing contents of 99-dhcp-en.network file
     rm /etc/systemd/network/99-dhcp-en.network
+    touch /etc/systemd/network/99-dhcp-en.network
 fi
