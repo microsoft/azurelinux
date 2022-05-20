2aeee2cfeeff063820f7c1543eeb338d81a8f283
Neha Agarwal <nehaagarwal@microsoft.com>
2022-05-20 12:49:20 -0700

    Truncating, to preserve file permission

--- toolkit/imageconfigs/additionalconfigs/configure-image.sh
+++ toolkit/imageconfigs/additionalconfigs/configure-image.sh
@@ -2,6 +2,5 @@ echo Enabling service for Azure Serial Console
 systemctl enable serial-getty@ttyS0.service
 if [[ -f "/etc/systemd/network/99-dhcp-en.network" ]]; then
     echo Removing contents of 99-dhcp-en.network file
-    rm /etc/systemd/network/99-dhcp-en.network
-    touch /etc/systemd/network/99-dhcp-en.network
+    truncate -s 0 /etc/systemd/network/99-dhcp-en.network
 fi
