bd37c64ff93eeb1126a5e284d3a093f8984ed574
Neha Agarwal <nehaagarwal@microsoft.com>
2022-06-19 14:01:17 -0700

    Incorporate comments

--- toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
+++ toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
@@ -114,14 +114,15 @@ rm -rf gcc-11.2.0
 
 touch $LFS/logs/temptoolchain/status_gcc_pass1_complete
 
-echo Linux-5.15.48.1 API Headers
-tar xf kernel-5.15.48.1.tar.gz
-pushd CBL-Mariner-Linux-Kernel-rolling-lts-mariner-2-5.15.48.1
+KERNEL_VERSION="5.15.48.1"
+echo Linux-${KERNEL_VERSION} API Headers
+tar xf kernel-${KERNEL_VERSION}.tar.gz
+pushd CBL-Mariner-Linux-Kernel-rolling-lts-mariner-2-${KERNEL_VERSION}
 make mrproper
 make headers
 cp -rv usr/include/* /tools/include
 popd
-rm -rf CBL-Mariner-Linux-Kernel-rolling-lts-mariner-2-5.15.48.1
+rm -rf CBL-Mariner-Linux-Kernel-rolling-lts-mariner-2-${KERNEL_VERSION}
 
 touch $LFS/logs/temptoolchain/status_kernel_headers_complete
 
