7e1562e74089e971ca289226646c188e4aaeb955
(Apple) Xuanchen Li <apple20010209@163.com>
2022-07-07 09:53:57 -0700

    Added error handling; now checks every file in a list

--- toolkit/tools/internal/dashboard/dashboard.go
+++ toolkit/tools/internal/dashboard/dashboard.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"strings"
+	"time"
+)
+
+var (
+	targetCSV = []string{"create_worker_chroot.csv", "imageconfigvalidator.csv", "imagepkgfetcher.csv", "imager.csv", "roast.csv"}
+	CSVSize   = []int64{0, 0, 0, 0, 0}
+	// targetJSON = []string{}
 )
 
 func main() {
@@ -11,15 +19,42 @@ func main() {
 	wd, _ := os.Getwd()
 	idx := strings.Index(wd, "CBL-Mariner/toolkit")
 	wd = wd[0 : idx+19]
-	wd += "/tools/internal/timestamp/results/create_worker_chroot.csv"
-	path := wd
-	currentStat, _ := os.Stat(path)
-	for true {
-		newStat, _ := os.Stat(path)
-		if currentStat.Size() != newStat.Size() {
-			currentStat = newStat
-			fmt.Println(currentStat.Size())
+
+	// Use an infinite for loop to watch out for new updates
+	for {
+		// run this iteration periodically for smaller overhead
+		time.Sleep(2 * time.Second)
+
+		// Check if the target directory exists. Assume we only need to check one directory for now.
+		targetDir := wd + "/tools/internal/timestamp/results/"
+		currStat, err := os.Stat(targetDir)
+		if os.IsNotExist(err) {
+			// fmt.Printf("The target directory %s doesn't exist. \n", targetDir)
+			continue
+		}
+
+		// Check update for each target file.
+		for idx, filePath := range targetCSV {
+			filePath = targetDir + filePath
+			// fmt.Printf("Processing file %s. \n", filePath)
+			currStat, err = os.Stat(filePath)
+			// Check if the file exists.
+			if os.IsNotExist(err) {
+				// fmt.Printf("File doesn't exist. \n")
+				continue
+			}
+			getUpdate(currStat, idx, filePath)
 		}
 	}
 
+}
+
+// Check if the file has been updated, and get updated contents if it did.
+// Assumption: the file and its parent directories of the file have been created.
+func getUpdate(currStat fs.FileInfo, idx int, filePath string) {
+	if currStat.Size() != CSVSize[idx] {
+		CSVSize[idx] = currStat.Size()
+		fmt.Printf("%s has %d bytes\n", filePath, currStat.Size())
+	}
+	// fmt.Printf("No change in file size for %s \n.", filePath)
 }
