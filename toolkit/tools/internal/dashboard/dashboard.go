723d71f784f74d361146104944fb4d7b7c61f40c
(Apple) Xuanchen Li <apple20010209@163.com>
2022-07-07 16:20:26 -0700

    combined two slices into a map

--- toolkit/tools/internal/dashboard/dashboard.go
+++ toolkit/tools/internal/dashboard/dashboard.go
@@ -10,8 +10,16 @@ import (
 )
 
 var (
-	targetCSV = []string{"create_worker_chroot.csv", "imageconfigvalidator.csv", "imagepkgfetcher.csv", "imager.csv", "roast.csv"}
-	CSVSize   = []int64{0, 0, 0, 0, 0}
+	targetDir string
+	targetCSV = map[string][]int64 {
+		"create_worker_chroot.csv": []int64{0, 4}, 
+		"imageconfigvalidator.csv": []int64{0, 2}, 
+		"imagepkgfetcher.csv": []int64{0, 9}, 
+		"imager.csv": []int64{0, 4}, 
+		"roast.csv": []int64{0, 3},
+	}
+	// targetCSV = []string{"create_worker_chroot.csv", "imageconfigvalidator.csv", "imagepkgfetcher.csv", "imager.csv", "roast.csv"}
+	// CSVSize   = []int64{0, 0, 0, 0, 0}
 	// targetJSON = []string{}
 )
 
@@ -20,14 +28,14 @@ func main() {
 	wd, _ := os.Getwd()
 	idx := strings.Index(wd, "CBL-Mariner/toolkit")
 	wd = wd[0 : idx+19]
+	targetDir = wd + "/tools/internal/timestamp/results/"
 
 	// Use an infinite for loop to watch out for new updates
 	for {
 		// run this iteration periodically for smaller overhead
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 
 		// Check if the target directory exists. Assume we only need to check one directory for now.
-		targetDir := wd + "/tools/internal/timestamp/results/"
 		currStat, err := os.Stat(targetDir)
 		if os.IsNotExist(err) {
 			// fmt.Printf("The target directory %s doesn't exist. \n", targetDir)
@@ -35,10 +43,10 @@ func main() {
 		}
 
 		// Check update for each target file.
-		for idx, filePath := range targetCSV {
-			filePath = targetDir + filePath
+		for filePath, _ := range targetCSV {
+			// filePath = targetDir + filePath
 			// fmt.Printf("Processing file %s. \n", filePath)
-			currStat, err = os.Stat(filePath)
+			currStat, err = os.Stat(targetDir + filePath)
 			// Check if the file exists.
 			if os.IsNotExist(err) {
 				// fmt.Printf("File doesn't exist. \n")
@@ -53,21 +61,11 @@ func main() {
 
 // Check if the file has been updated, and get updated contents if it did.
 // Assumption: the file and its parent directories of the file have been created.
-// func getUpdate(currStat fs.FileInfo, idx int, filePath string) {
-// 	if currStat.Size() != CSVSize[idx] {
-// 		CSVSize[idx] = currStat.Size()
-// 		fmt.Printf("%s has %d bytes\n", currStat.Name(), currStat.Size())
-// 	}
-// 	// fmt.Printf("No change in file size for %s \n.", filePath)
-// }
-
-// Check if the file has been updated, and get updated contents if it did.
-// Assumption: the file and its parent directories of the file have been created.
 func getUpdate(currStat fs.FileInfo, idx int, filePath string) {
-	currNumLines := getNumLines(filePath)
-	if currNumLines != CSVSize[idx] {
-		CSVSize[idx] = currNumLines
-		fmt.Printf("%s has %d lines\n", currStat.Name(), currNumLines)
+	currNumLines := getNumLines(targetDir + filePath)
+	if currNumLines != targetCSV[filePath][0] {
+		targetCSV[filePath][0] = currNumLines
+		fmt.Printf("%s has %d lines \n", currStat.Name(), currNumLines)
 	}
 }
 
