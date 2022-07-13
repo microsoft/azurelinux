ec71c0f0598273a8bc30bed16e72a17ad9d8a59c
(Apple) Xuanchen Li <apple20010209@163.com>
2022-07-12 17:02:44 -0700

    added timestamp at the beginning of any bash script; fixed progress bar to display the correct %

--- toolkit/tools/internal/dashboard/dashboard.go
+++ toolkit/tools/internal/dashboard/dashboard.go
@@ -12,23 +12,24 @@ import (
 )
 
 var (
-	targetDir string
-	targetCSV = map[string][]int64{
-		"create_worker_chroot.csv": []int64{0, 4},
-		"imageconfigvalidator.csv": []int64{0, 2},
-		"imagepkgfetcher.csv":      []int64{0, 9},
-		"imager.csv":               []int64{0, 4},
-		"roast.csv":                []int64{0, 3},
+	currProgress  = 0
+	totalProgress = 24
+	targetDir     string
+	targetCSV     = map[string][]int{
+		"create_worker_chroot.csv": []int{0, 5},
+		"imageconfigvalidator.csv": []int{0, 2},
+		"imagepkgfetcher.csv":      []int{0, 9},
+		"imager.csv":               []int{0, 4},
+		"roast.csv":                []int{0, 3},
 	}
-	// targetCSV = []string{"create_worker_chroot.csv", "imageconfigvalidator.csv", "imagepkgfetcher.csv", "imager.csv", "roast.csv"}
-	// CSVSize   = []int64{0, 0, 0, 0, 0}
-	// targetJSON = []string{}
+	isInit = false // set to true the first time we detec the "init" file in build/timestamp folder.
+	// targetJSON = []string{} // for future version
 )
 
 func main() {
 	fmt.Println("Starting dashboard")
 	uiprogress.Start() // start rendering
-	bar := uiprogress.AddBar(22)
+	bar := uiprogress.AddBar(totalProgress)
 	bar.AppendCompleted()
 	bar.PrependElapsed()
 
@@ -41,6 +42,7 @@ func main() {
 	for {
 		// run this iteration periodically for smaller overhead
 		time.Sleep(1 * time.Second)
+		bar.Set(currProgress)
 
 		// Check if the target directory exists. Assume we only need to check one directory for now.
 		currStat, err := os.Stat(targetDir)
@@ -48,10 +50,17 @@ func main() {
 			continue
 		}
 
+		// Check if build has started; if so, add a timestamp to init.csv
+		if isInit == false {
+			checkInit()
+			// bar.Set(currProgress)
+		}
+
 		// Check update for each target file.
 		for filePath, _ := range targetCSV {
 			currStat, err = os.Stat(targetDir + filePath)
 			// Check if the file exists.
+			currStat, err := os.Stat(targetDir + filePath)
 			if os.IsNotExist(err) {
 				continue
 			}
@@ -70,23 +79,37 @@ func main() {
 
 // Check if the file has been updated, and get updated contents if it did.
 // Assumption: the file and its parent directories of the file have been created.
-func getUpdate(currStat fs.FileInfo, idx int, filePath string) {
-	currNumLines := getNumLines(targetDir + filePath)
+func getUpdate(currStat fs.FileInfo, filePath string) {
+	currNumLines := getNumLines(filePath)
 	if currNumLines != targetCSV[filePath][0] {
+		currProgress += currNumLines - targetCSV[filePath][0]
 		targetCSV[filePath][0] = currNumLines
 		fmt.Printf("%s has %d lines \n", currStat.Name(), currNumLines)
 	}
 }
 
-// Naive implementation (potentially inefficient for larger files)
-func getNumLines(filepath string) int64 {
-	file, _ := os.Open(filepath)
+// Naive implementation (potentially inefficient for larger files).
+func getNumLines(filepath string) int {
+	file, _ := os.Open(targetDir + filepath)
 	fileScanner := bufio.NewScanner(file)
 	count := 0
 
 	for fileScanner.Scan() {
 		count++
+		if count > int(targetCSV[filepath][0]) {
+			// fmt.Printf("[%d / %d] in %s: %s \n", count, targetCSV[filepath][1], filepath, fileScanner.Text())
+		}
 	}
 
-	return int64(count)
+	return count
+}
+
+// Checks if the build has started, and update the progress bar if it did start.
+func checkInit() {
+	_, err := os.Stat(targetDir + "/init")
+	if os.IsNotExist(err) {
+		return
+	}
+	isInit = true
+	currProgress += 1
 }
