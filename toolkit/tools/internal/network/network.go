530d61633bc9f41711b4567658b7a6c85043854a
Henry Li <69694695+henryli001@users.noreply.github.com>
2022-07-28 16:13:31 -0700

    Fix network access check during package repo file generation (#3204)
    * fix package repo network access error return
    
    * revert unnecessary changes
    
    * update network access check logic
    
    * update network access check logic
    
    * remove unnecessary changes and fix go formatting
    
    * fix comment
    
    Co-authored-by: Henry Li <lihl@microsoft.com>

--- toolkit/tools/internal/network/network.go
+++ toolkit/tools/internal/network/network.go
@@ -66,16 +66,30 @@ func DownloadFile(url, dst string, caCerts *x509.CertPool, tlsCerts []tls.Certif
 }
 
 // CheckNetworkAccess checks whether the installer environment has network access
+// This function is only executed within the ISO installation environment for kickstart-like unattended installation
 func CheckNetworkAccess() (err error, hasNetworkAccess bool) {
 	const (
 		retryAttempts = 10
 		retryDuration = time.Second
 		squashErrors  = false
+		activeStatus  = "active"
 	)
 
 	err = retry.Run(func() error {
-		err := shell.ExecuteLive(squashErrors, "ping", "-c", "1", "www.microsoft.com")
-		hasNetworkAccess = err == nil
+		err := shell.ExecuteLive(squashErrors, "systemctl", "restart", "systemd-networkd-wait-online")
+		if err != nil {
+			logger.Log.Errorf("Cannot start systemd-networkd-wait-online.service")
+			return err
+		}
+
+		stdout, stderr, err := shell.Execute("systemctl", "is-active", "systemd-networkd-wait-online")
+		if err != nil {
+			logger.Log.Errorf("Failed to query status of systemd-networkd-wait-online: %v", stderr)
+			return err
+		}
+
+		serviceStatus := strings.TrimSpace(stdout)
+		hasNetworkAccess = serviceStatus == activeStatus
 		if !hasNetworkAccess {
 			logger.Log.Warnf("No network access yet")
 		}
