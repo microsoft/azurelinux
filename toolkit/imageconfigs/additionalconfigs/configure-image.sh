bba2f844849cb08759f895cedb26e053bd5f6bfc
Chris Patterson <cpatterson@microsoft.com>
2022-07-29 16:10:14 -0400

    azure image: enable PHC for chrony.conf
    For more information, check out:
    https://docs.microsoft.com/en-us/azure/virtual-machines/linux/time-sync
    
    Signed-off-by: Chris Patterson <cpatterson@microsoft.com>

--- toolkit/imageconfigs/additionalconfigs/configure-image.sh
+++ toolkit/imageconfigs/additionalconfigs/configure-image.sh
@@ -4,3 +4,6 @@ if [[ -f "/etc/systemd/network/99-dhcp-en.network" ]]; then
     echo Removing contents of 99-dhcp-en.network file
     truncate -s 0 /etc/systemd/network/99-dhcp-en.network
 fi
+
+echo -e "\n# Enable virtual PTP clock source." >> /etc/chrony.conf
+echo "refclock PHC /dev/ptp_hyperv poll 3 dpoll -2 offset 0" >> /etc/chrony.conf
