cd3a0ac0f2df9638c203242d6243729c8789ba9f
james-zhou1 <james.zhou@berkeley.edu>
2022-08-03 16:46:04 -0700

    delayed progress bar removal

--- toolkit/tools/demo/demo.go
+++ toolkit/tools/demo/demo.go
@@ -16,10 +16,10 @@ import (
 
 var (
 	// Shared TimeStamp struct.
-	ts TimeStamp
-	fileName = "./time_demo.json"
+	ts            TimeStamp
+	fileName      = "./time_demo.json"
 	shortInterval = 500
-	longInterval = 1000
+	longInterval  = 1000
 )
 
 type TimeStamp struct {
@@ -41,7 +41,7 @@ type TimeStamp struct {
 }
 
 func main() {
-	// structure: 
+	// structure:
 	// 1A	-2A	-3A
 	//			\3B	-4A
 	//				\4B
@@ -68,28 +68,28 @@ func main() {
 	finishStep(&ts.Steps[0].Steps[1].Steps, longInterval) // 4A
 	appendStep(&ts.Steps[0].Steps[1].Steps, shortInterval, "4B", 0)
 	finishStep(&ts.Steps[0].Steps[1].Steps, longInterval) // 4B
-	finishStep(&ts.Steps[0].Steps, shortInterval) // 3B
-	finishStep(&ts.Steps, shortInterval) // 2A
+	finishStep(&ts.Steps[0].Steps, shortInterval)         // 3B
+	finishStep(&ts.Steps, shortInterval)                  // 2A
 	appendStep(&ts.Steps, shortInterval, "2B", 2)
 	appendStep(&ts.Steps[1].Steps, shortInterval, "3C", 2)
 	appendStep(&ts.Steps[1].Steps[0].Steps, shortInterval, "4C", 0)
 	appendStep(&ts.Steps[1].Steps[0].Steps[0].Steps, shortInterval, "5A", 0)
 	finishStep(&ts.Steps[1].Steps[0].Steps[0].Steps, longInterval) // 5A
-	finishStep(&ts.Steps[1].Steps[0].Steps, longInterval) // 4C
+	finishStep(&ts.Steps[1].Steps[0].Steps, longInterval)          // 4C
 	appendStep(&ts.Steps[1].Steps[0].Steps, shortInterval, "4D", 0)
 	finishStep(&ts.Steps[1].Steps[0].Steps, longInterval) // 4D
-	finishStep(&ts.Steps[1].Steps, shortInterval) // 3C
+	finishStep(&ts.Steps[1].Steps, shortInterval)         // 3C
 	appendStep(&ts.Steps[1].Steps, shortInterval, "3D", 1)
 	appendStep(&ts.Steps[1].Steps[1].Steps, shortInterval, "4E", 0)
 	finishStep(&ts.Steps[1].Steps[1].Steps, longInterval) // 4E
 	appendStep(&ts.Steps[1].Steps[1].Steps, shortInterval, "4F", 1)
 	finishStep(&ts.Steps[1].Steps[1].Steps, longInterval) // 4F
-	finishStep(&ts.Steps[1].Steps, shortInterval) // 3D
-	finishStep(&ts.Steps, shortInterval) // 2B
+	finishStep(&ts.Steps[1].Steps, shortInterval)         // 3D
+	finishStep(&ts.Steps, shortInterval)                  // 2B
 	appendStep(&ts.Steps, shortInterval, "2C", 1)
 	appendStep(&ts.Steps[2].Steps, shortInterval, "3E", 2)
 	finishStep(&ts.Steps[2].Steps, longInterval) // 3E
-	finishStep(&ts.Steps, shortInterval) // 2C
+	finishStep(&ts.Steps, shortInterval)         // 2C
 
 }
 
@@ -118,7 +118,7 @@ func (t *TimeStamp) Progress() float64 {
 	}
 }
 
-func appendStep(steps *[]TimeStamp, unitTime int, name string, expectedSteps int){ 
+func appendStep(steps *[]TimeStamp, unitTime int, name string, expectedSteps int) {
 	time.Sleep(time.Millisecond * time.Duration(unitTime))
 	now := time.Now()
 	*steps = append(*steps, TimeStamp{Name: name, StartTime: &now, EndTime: nil, ExpectedSteps: expectedSteps})
@@ -132,7 +132,7 @@ func appendStep(steps *[]TimeStamp, unitTime int, name string, expectedSteps int
 func finishStep(steps *[]TimeStamp, unitTime int) {
 	time.Sleep(time.Millisecond * time.Duration(unitTime))
 	now := time.Now()
-	(*steps)[len(*steps) - 1].EndTime = &now
+	(*steps)[len(*steps)-1].EndTime = &now
 	err := jsonutils.WriteJSONFile(fileName, &ts)
 	if err != nil {
 		fmt.Printf("unable to write to json file\n")
