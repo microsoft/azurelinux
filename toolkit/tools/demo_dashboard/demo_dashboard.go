3628b95f2c8f4f0ddf90b1a499ec5112531ef6aa
james-zhou1 <james.zhou@berkeley.edu>
2022-08-03 16:53:29 -0700

    new files

---
+++ toolkit/tools/demo_dashboard/demo_dashboard.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"math"
+	"time"
+
+	"github.com/gosuri/uiprogress"
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/jsonutils"
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/logger"
+)
+
+var dashboardProgress = uiprogress.New()
+
+type TimeStamp struct {
+	Name          string      `json:"Name"`
+	StartTime     *time.Time  `json:"StartTime"`
+	EndTime       *time.Time  `json:"EndTime"`
+	ExpectedSteps int         `json:"ExpectedSteps"`
+	Steps         []TimeStamp `json:"Steps"`
+}
+
+func (t *TimeStamp) Progress() float64 {
+	progress := 0.0
+	maxProgress := math.Max(float64(t.ExpectedSteps), float64(len(t.Steps)))
+
+	if t.StartTime == nil {
+		return 0.0
+	}
+
+	if t.EndTime != nil {
+		return 1.0
+	}
+
+	for _, step := range t.Steps {
+		progress += step.Progress()
+	}
+
+	if maxProgress == 0.0 {
+		return 0.0
+	} else {
+		// We don't want to mark complete until we have an end time for this
+		//   step, just max it out at 95%
+		return math.Min(progress/maxProgress, 1)
+	}
+}
+
+var progressName []string
+var progressValue []int
+var progressLevel []int
+
+func CheckProgress(ts *TimeStamp, level int) {
+	progressName = append(progressName, ts.Name)
+	progressValue = append(progressValue, int(ts.Progress()*100))
+	progressLevel = append(progressLevel, level)
+	for _, step := range ts.Steps {
+		progress := step.Progress()
+		if progress != 0.0 && progress != 1.0 {
+			CheckProgress(&step, level+1)
+		}
+	}
+}
+
+func main() {
+	waitTime := time.Millisecond * 500
+	// var wg sync.WaitGroup
+
+	logger.InitStderrLog()
+
+	dashboardProgress.Start()
+	for {
+		var ts TimeStamp
+		err := jsonutils.ReadJSONFile("/home/jameszhou/CBL-Mariner/toolkit/tools/demo/time_demo.json", &ts)
+		if err != nil {
+			return
+		}
+		progressName = []string{}
+		progressValue = []int{}
+		progressLevel = []int{}
+		CheckProgress(&ts, 0)
+		for i := 0; i < len(progressName); i++ {
+			name := progressName[i]
+			value := progressValue[i]
+			level := progressLevel[i]
+			indent := ""
+			for i := 0; i < level; i++ {
+				indent += "  "
+			}
+			bar := dashboardProgress.AddBar(100)
+			bar.AppendCompleted()
+			bar.PrependFunc(func(b *uiprogress.Bar) string {
+				return name + indent
+			})
+			bar.Set(value)
+			// wg.Add(1)
+		}
+		time.Sleep(waitTime)
+		for i := 0; i < len(progressName); i++ {
+			dashboardProgress.Bars = dashboardProgress.Bars[:len(dashboardProgress.Bars)-1]
+		}
+		time.Sleep(waitTime)
+	}
+
+}
+
+func StartProgress() {
+	dashboardProgress.Start()
+}
+
+func StopProgress() {
+	dashboardProgress.Stop()
+}
+
+func AddProgressBar(total int) *uiprogress.Bar {
+	return dashboardProgress.AddBar(total)
+}
+
+func ListenProgress() {
+	dashboardProgress.Listen()
+}
