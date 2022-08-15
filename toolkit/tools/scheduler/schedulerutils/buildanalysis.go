df87ab5cb3e339b26037e63dea734fcca16ef613
MaxBrodeurUrbas <murbas98@gmail.com>
2022-08-15 11:06:02 -0700

    adding very basic buildanalysis class

---
+++ toolkit/tools/scheduler/schedulerutils/buildanalysis.go
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package schedulerutils
+
+import (
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/logger"
+)
+
+type BuildAnalysis struct {
+	TotalBuildTime float32
+	IdleTime  float32
+	WastedTime float32
+}
+
+func NewBuildAnalysis() (l *BuildAnalysis) {
+	return &BuildAnalysis{
+		TotalBuildTime: 0.0,
+		IdleTime: 0.0,
+		WastedTime: 0.0,
+	}
+}
+
+func (b *BuildAnalysis) RecordTotalBuildTime(buildTime float32) {
+	b.TotalBuildTime = buildTime;
+}
