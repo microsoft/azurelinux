a3f80bc8e8f2321e87e2d466afbb5e0a08889426
Pawel Winogrodzki <pawelwi@microsoft.com>
2022-09-21 13:44:01 -0700

    Script clean-up.

--- toolkit/scripts/livepatching/new_livepatch.sh
+++ toolkit/scripts/livepatching/new_livepatch.sh
@@ -24,14 +24,19 @@ LIVEPATCH_SPECS_DIR="$REPO_ROOT/SPECS/livepatch"
 KERNEL_VERSION="$(mariner_rpmspec -q --queryformat="%{VERSION}\n" --srpm "$KERNEL_SPEC_PATH" 2>/dev/null)"
 KERNEL_VERSION_RELEASE="$(mariner_rpmspec -q --queryformat="%{VERSION}-%{RELEASE}\n" --srpm "$KERNEL_SPEC_PATH" 2>/dev/null)"
 
-echo "Generating empty livepatch spec for kernel ($KERNEL_VERSION_RELEASE)."
+echo "Generating empty livepatch spec for kernel ($KERNEL_VERSION_RELEASE) under ($LIVEPATCH_SPECS_DIR)."
 
 mkdir -p "$REPO_ROOT/SPECS/livepatch"
 
 LIVEPATCH_CONFIG_FILE_NAME="config-$KERNEL_VERSION_RELEASE"
 LIVEPATCH_PUBLIC_KEY_FILE="mariner-$KERNEL_VERSION_RELEASE.pem"
+
+echo "Copying kernel config ($LIVEPATCH_CONFIG_FILE_NAME) and trusted key ($LIVEPATCH_PUBLIC_KEY_FILE)."
+
 cp "$KERNEL_CONFIG_PATH" "$LIVEPATCH_SPECS_DIR/$LIVEPATCH_CONFIG_FILE_NAME"
 cp "$KERNEL_PUBLIC_KEY_PATH" "$LIVEPATCH_SPECS_DIR/$LIVEPATCH_PUBLIC_KEY_FILE"
+
+echo "Generating signatures."
 
 KERNEL_TARBALL_FILE="kernel-$KERNEL_VERSION.tar.gz"
 CONFIG_HASH="$(jq -r ".Signatures.config" "$KERNEL_SIGNATURES_PATH")"
@@ -47,6 +52,10 @@ jq -n \
     '{"Signatures": {($config_name): $config_hash, ($kernel_name): $kernel_hash, ($pem_name): $pem_hash}}' \
     > "$LIVEPATCH_SPECS_DIR/livepatch-$KERNEL_VERSION_RELEASE.signatures.json"
 
-LIVEPATCH_SPEC_PATH="$LIVEPATCH_SPECS_DIR/livepatch-$KERNEL_VERSION_RELEASE.spec"
+LIVEPATCH_SPEC_NAME="livepatch-$KERNEL_VERSION_RELEASE.spec"
+LIVEPATCH_SPEC_PATH="$LIVEPATCH_SPECS_DIR/$LIVEPATCH_SPEC_NAME"
+
+echo "Generating the new livepatch spec ($LIVEPATCH_SPEC_NAME)."
+
 sed "s/@KERNEL_VERSION_RELEASE@/$KERNEL_VERSION_RELEASE/" "$SCRIPT_FOLDER/template_livepatch.spec" > "$LIVEPATCH_SPEC_PATH"
-"$COMMON_SCRIPTS_FOLDER"/update_spec.sh "Original version for CBL-Mariner.\n- License verified." "$LIVEPATCH_SPEC_PATH"
+"$COMMON_SCRIPTS_FOLDER"/update_spec.sh "Original version for CBL-Mariner.\n- License verified." "$LIVEPATCH_SPEC_PATH" 1>/dev/null
