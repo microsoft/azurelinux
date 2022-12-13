8954fe74af2b5af4af957ed124105fb74f5037a6
Daniel McIlvaney <damcilva@microsoft.com>
2022-12-13 13:11:06 -0800

    update tool

--- SPECS/ruby/get_gem_versions.sh
+++ SPECS/ruby/get_gem_versions.sh
@@ -13,27 +13,29 @@ if [[ -z "$RUBY_VER" ]]; then
   exit 1
 fi
 
+PAD_SIZE=23 # (Manually calculated based on the length of '%global error_highlight_version...')
+
 # Grab the 'rubygems' field seperately
 curl https://stdgems.org/default_gems.json 2>/dev/null                | \
 jq  '.gems[] | [.gem, .versions["'$RUBY_VER'"]] | select(.[0]=="rubygems")' | \
 jq -r '.[0], .[1]'                                                    | \
-xargs printf '%%global %s_version %s\n'
+xargs printf '%%global %s_version %s\n'                               | \
+xargs printf "%s %-${PAD_SIZE}s %s\n"
 
 # Print the comments to make life easier
 printf '# Add version for default gems from https://stdgems.org/\n'
 printf '# A helpful one-liner script to check the current default versions is available via RUBY_VER=3.1 ./get_gem_versions.sh\n'
 
 # -get .json data
-# - query each entry for its gem + version matchign $RUBY_VER
+# - query each entry for its gem + version matchign $RUBY_VER (but not rubygems)
 # - Print each array entry
 # - put two per line using paste
 # - add '%global' and '_version' to each pkg
 # - fix spacing
-PAD_SIZE=23 # (Manually calculated based on the length of '%global error_highlight_version...')
 curl https://stdgems.org/default_gems.json 2>/dev/null                | \
 jq  '.gems[] | [.gem, .versions["'$RUBY_VER'"]] | select(.[1]!=null and .[0]!="rubygems")' | \
 jq -r '.[0], .[1]'                                                    | \
 tr '-' '_'                                                            | \
 paste -d " " - -                                                      | \
-xargs printf '%%global %s_version %s\n'                              | \
+xargs printf '%%global %s_version %s\n'                               | \
 xargs printf "%s %-${PAD_SIZE}s %s\n"
