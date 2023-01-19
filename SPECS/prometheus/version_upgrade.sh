75eb4e509320f16706066f41e649299253f57da7
Osama Esmail <osamaesmail@microsoft.com>
2023-01-18 16:55:37 -0800

    reformatting script

--- SPECS/prometheus/version_upgrade.sh
+++ SPECS/prometheus/version_upgrade.sh
@@ -13,14 +13,83 @@
 # 10. Make some changes to Makefile.common again and compress our custom prometheus and promu folders into their respective tarballs.
 # 11. Print SHA256 of prometheus and promu tarballs
 
+set -e
 
-START_DIR=$(pwd)
-PROM_TMP=$(mktemp -d)
+SRC_TARBALL=""
+OUT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PKG_VERSION=""
 
-PROMETHEUS_VERSION=2.37.0
+# parameters:
+#
+# --srcTarball  : src tarball file
+#                 this file contains the 'initial' source code of the component
+#                 and should be replaced with the new/modified src code
+# --outFolder   : folder where to copy the new tarball(s)
+# --pkgVersion  : package version
+#
+PARAMS=""
+while (( "$#" )); do
+    case "$1" in
+        --srcTarball)
+        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+            SRC_TARBALL=$2
+            shift 2
+        else
+            echo "Error: Argument for $1 is missing" >&2
+            exit 1
+        fi
+        ;;
+        --outFolder)
+        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+            OUT_FOLDER=$2
+            shift 2
+        else
+            echo "Error: Argument for $1 is missing" >&2
+            exit 1
+        fi
+        ;;
+        --pkgVersion)
+        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+            PKG_VERSION=$2
+            shift 2
+        else
+            echo "Error: Argument for $1 is missing" >&2
+            exit 1
+        fi
+        ;;
+        -*|--*=) # unsupported flags
+        echo "Error: Unsupported flag $1" >&2
+        exit 1
+        ;;
+        *) # preserve positional arguments
+        PARAMS="$PARAMS $1"
+        shift
+        ;;
+  esac
+done
+
+echo "--srcTarball   -> $SRC_TARBALL"
+echo "--outFolder    -> $OUT_FOLDER"
+echo "--pkgVersion   -> $PKG_VERSION"
+
+if [ -z "$PKG_VERSION" ]; then
+    echo "--pkgVersion parameter cannot be empty"
+    exit 1
+fi
+
+echo "-- create temp folder"
+TEMPDIR=$(mktemp -d)
+function cleanup {
+    echo "+++ cleanup -> remove $TEMPDIR"
+    rm -rf $TEMPDIR
+}
+trap cleanup EXIT
+
+echo "Starting Prometheus source tarball creation"
+PROMETHEUS_VERSION=$PKG_VERSION
 PROMETHEUS_URL="https://github.com/prometheus/prometheus/archive/refs/tags/v$PROMETHEUS_VERSION.tar.gz"
 
-cd "$PROM_TMP"
+cd "$TEMPDIR"
 wget -c $PROMETHEUS_URL -O "prometheus-$PROMETHEUS_VERSION.tar.gz"
 tar -xzf "prometheus-$PROMETHEUS_VERSION.tar.gz"
 #PROMU_VERSION is found in prometheus-$PROMETHEUS_VERSION/Makefile.common
@@ -33,9 +102,9 @@ tar -xzf "promu-$PROMU_VERSION.tar.gz"
 cd "promu-$PROMU_VERSION"
 make build
 go mod vendor
-cp -r vendor "$PROM_TMP/temp_vendor"
+cp -r vendor "$TEMPDIR/temp_vendor"
 
-cd "$PROM_TMP"
+cd "$TEMPDIR"
 rm -rf "promu-$PROMU_VERSION"
 tar -xzf "promu-$PROMU_VERSION.tar.gz"
 cp -r "temp_vendor/vendor" "promu-$PROMU_VERSION"
@@ -52,7 +121,7 @@ cd "prometheus-$PROMETHEUS_VERSION"
 make build
 go mod vendor
 
-cd "$PROM_TMP"
+cd "$TEMPDIR"
 cp -r "prometheus-$PROMETHEUS_VERSION/vendor" temp_vendor
 cp "prometheus-$PROMETHEUS_VERSION/web/ui/.npmrc" temp_vendor
 cp -r "prometheus-$PROMETHEUS_VERSION/web/ui/.npm_cache" temp_vendor
@@ -68,9 +137,7 @@ sed -i "s/curl -s -L \$(PROMU_URL) | tar -xvzf - -C \$(PROMU_TMP)//g" prometheus
 sed -i "s/cp \$(PROMU_TMP)\/promu-\$(PROMU_VERSION).\$(GO_BUILD_PLATFORM)\/promu/cp promu-\$(PROMU_VERSION)/g" prometheus-$PROMETHEUS_VERSION/Makefile.common
 sed -i "s/rm -r \$(PROMU_TMP)//g" prometheus-$PROMETHEUS_VERSION/Makefile.common
 
-tar -czf "$START_DIR/prometheus-$PROMETHEUS_VERSION.tar.gz" prometheus-$PROMETHEUS_VERSION
-sha256sum "$START_DIR/prometheus-$PROMETHEUS_VERSION.tar.gz"
-tar -czf "$START_DIR/promu-$PROMU_VERSION.tar.gz" promu-$PROMU_VERSION
-sha256sum "$START_DIR/promu-$PROMU_VERSION.tar.gz"
+tar -czf "$OUT_FOLDER/prometheus-$PROMETHEUS_VERSION.tar.gz" prometheus-$PROMETHEUS_VERSION
+tar -czf "$OUT_FOLDER/promu-$PROMU_VERSION.tar.gz" promu-$PROMU_VERSION
 
-rm -rf $PROM_TMP
+echo "Source tarball $OUT_FOLDER/prometheus-$PROMETHEUS_VERSION.tar.gz successfully created!"
