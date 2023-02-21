bc4ff22ecbf3e0404112eaa9564ae2dd79d92852
Osama Esmail <osamaesmail@microsoft.com>
2023-02-21 10:07:15 -0800

    fixing one bug

--- toolkit/tools/scheduler/schedulerutils/buildworker.go
+++ toolkit/tools/scheduler/schedulerutils/buildworker.go
@@ -250,7 +250,7 @@ func buildSRPMFile(agent buildagents.BuildAgent, buildAttempts int, checkAttempt
 
 	// temporary solution; potential fix: once stable, fail builds if %check section fails?
 	if err != nil && checkFailed {
-		logger.Log.Debugf("Tests failed for '%'. Ignoring since the package built correctly. Error: %v", srpmFile, checkErr)
+		logger.Log.Debugf("Tests failed for '%'. Ignoring since the package built correctly. Error: %v", srpmFile, err)
 		err = nil
 	}
 	return
