37baf2dd5567146322610b46124eaf06f8ae0d7f
Pawel Winogrodzki <pawelwi@microsoft.com>
2023-03-01 16:53:27 -0800

    Updating script.

--- SPECS/ca-certificates/cert2certdata.sh
+++ SPECS/ca-certificates/cert2certdata.sh
@@ -4,10 +4,13 @@
 
 set -e
 
-if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 <space-separated list of CRT/DER/PEM files>" >&2
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <CA certificate in DER/PEM format> <trust flags in the format accepted by the '-t' argument for 'nss-addbuiltin'>" >&2
     exit 1
 fi
+
+cert_file="$1"
+trust_flags="$2"
 
 temp_dir=$(mktemp -d)
 function cleanup {
@@ -16,33 +19,18 @@ function cleanup {
 }
 trap cleanup EXIT
 
-for cert_file in "$@"
-do
-    if ! openssl x509 -in "$cert_file" -text -noout &>/dev/null
-    then
-        echo "File '$cert_file' is not a valid certificate." >&2
-        exit 1
-    fi
+if ! openssl x509 -in "$cert_file" -text -noout &>/dev/null
+then
+    echo "File '$cert_file' is not a valid certificate." >&2
+    exit 1
+fi
 
-    # The 'nss-addbuiltin' tool requires certificates in the DER format.
-    openssl x509 -in "$cert_file" -outform DER -out "$temp_dir/cert.der"
-    cert_file="$temp_dir/cert.der"
+# The 'nss-addbuiltin' tool requires certificates in the DER format.
+openssl x509 -in "$cert_file" -outform DER -out "$temp_dir/cert.der"
+cert_file="$temp_dir/cert.der"
 
-    cert_label="$(openssl x509 -noout -subject -in "$cert_file" | grep -oP "(?<=CN = ).*")"
+cert_label="$(openssl x509 -noout -subject -in "$cert_file" | grep -oP "(?<=CN = ).*")"
 
-    # We always want to set "CKA_TRUST_SERVER_AUTH" to "CKT_NSS_TRUSTED_DELEGATOR".
-    server_auth_flag="C"
-
-    email_protection_flag="C"
-    if ! openssl x509 -in "$cert_file" -purpose -text -noout | grep -q "Any Purpose CA : Yes"
-    then
-        email_protection_flag=""
-    fi
-
-    # We always want to set "CKA_TRUST_CODE_SIGNING" to "CKT_NSS_MUST_VERIFY_TRUST".
-    code_signing_flag=""
-
-    nss-addbuiltin -n "$cert_label" -t "$server_auth_flag,$email_protection_flag,$code_signing_flag" -i "$cert_file" >> "$temp_dir/certdata.txt"
-done
+nss-addbuiltin -n "$cert_label" -t "$trust_flags" -i "$cert_file" >> "$temp_dir/certdata.txt"
 
 mv "$temp_dir/certdata.txt" certdata.txt
