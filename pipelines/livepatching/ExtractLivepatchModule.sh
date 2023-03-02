4082cc10ac914b09c1fc5ffa227d55f9d1b85442
Pawel Winogrodzki <pawelwi@microsoft.com>
2023-03-02 14:56:40 -0800

    Adding signing logic.

--- pipelines/livepatching/ExtractLivepatchModule.sh
+++ pipelines/livepatching/ExtractLivepatchModule.sh
@@ -44,7 +44,7 @@ fi
 
 tmpdir=$(mktemp -d)
 function cleanup {
-    echo " Cleaning up '$tmpdir'."
+    echo "Cleaning up '$tmpdir'."
     rm -rf "$tmpdir"
 }
 trap cleanup EXIT
@@ -55,3 +55,6 @@ for livepatch_rpm in "$tmpdir"/**/livepatch-*cm2-+([0-9])*.rpm
 do
     rpm_extract_files "$livepatch_rpm" "*.ko" "$tmpdir"
 done
+
+echo "Moving livepatch modules to '$ARTIFACTS_DIR'."
+mv -v ./*.ko "$ARTIFACTS_DIR"
