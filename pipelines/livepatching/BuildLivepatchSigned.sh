b94b21c7fe772a02ec83943227abd0a8b018693a
Pawel Winogrodzki <pawelwi@microsoft.com>
2023-03-14 19:19:24 -0700

    Add RPM snapshot support for building signed RPMs.

--- pipelines/livepatching/BuildLivepatchSigned.sh
+++ pipelines/livepatching/BuildLivepatchSigned.sh
@@ -11,14 +11,27 @@ source "$ROOT_DIR/pipelines/common/libs/build_tools.sh"
 source "$ROOT_DIR/pipelines/common/libs/file_tools.sh"
 
 build_package_signed() {
+    local package_cache_summary
     local package_name
+    local toolkit_dir
+    local use_rpms_snapshot
 
     package_name="$1"
+    use_rpms_snapshot="$2"
+
+    toolkit_dir="toolkit"
+
+    if $use_rpms_snapshot
+    then
+        package_cache_summary="$toolkit_dir/rpms_snapshot.json"
+        echo "-- Using RPMs snapshot as package cache summary."
+    fi
 
     sudo make -C "toolkit" -j"$(nproc)" build-packages \
         CONFIG_FILE= \
         REBUILD_TOOLS=y \
         PACKAGE_BUILD_LIST="$package_name" \
+        PACKAGE_CACHE_SUMMARY="$package_cache_summary" \
         SRPM_PACK_LIST="$package_name-signed" \
         SRPM_FILE_SIGNATURE_HANDLING=update \
         SPECS_DIR="../SPECS-SIGNED" \
@@ -77,13 +90,15 @@ verify_built_package() {
 # -k -> kernel version to be livepatched
 # -l -> published logs directory path
 # -o -> built artifacts' output directory path
-while getopts "a:k:l:o:" OPTIONS
+# -s -> use toolkit's RPMs snapshot to populate the packages cache
+while getopts "a:k:l:o:s:" OPTIONS
 do
   case "${OPTIONS}" in
     a ) ARTIFACTS_DIR=$OPTARG ;;
     k ) KERNEL_VERSION=$OPTARG ;;
     l ) LOG_PUBLISH_DIR=$OPTARG ;;
     o ) ARTIFACTS_OUTPUT_DIR=$OPTARG ;;
+    s ) USE_RPMS_SNAPSHOT="$(parse_pipeline_boolean "$OPTARG")" ;;
 
     \? )
         echo "ERROR: Invalid Option: -$OPTARG" 1>&2
@@ -96,7 +111,7 @@ do
   esac
 done
 
-print_variables_with_check ARTIFACTS_DIR KERNEL_VERSION LOG_PUBLISH_DIR ARTIFACTS_OUTPUT_DIR
+print_variables_with_check ARTIFACTS_DIR KERNEL_VERSION LOG_PUBLISH_DIR ARTIFACTS_OUTPUT_DIR USE_RPMS_SNAPSHOT
 
 TEMP_DIR="$(mktemp -d)"
 trap temp_dir_cleanup EXIT
@@ -111,7 +126,7 @@ find "out/RPMS" -name "livepatch-$KERNEL_VERSION*.rpm" -and -not -name "*debugin
 hydrate_signed_sources "$KERNEL_VERSION" "$ARTIFACTS_DIR"
 
 # Making sure we publish build logs even if the build fails.
-build_package_signed "livepatch-$KERNEL_VERSION" || BUILD_SUCCEEDED=false
+build_package_signed "livepatch-$KERNEL_VERSION" "$USE_RPMS_SNAPSHOT" || BUILD_SUCCEEDED=false
 
 publish_build_logs "$LOG_PUBLISH_DIR"
 
