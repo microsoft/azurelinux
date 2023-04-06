e431ebee15732298d634ef39816f63254e33eae9
Muhammad Falak R Wani <falakreyaz@gmail.com>
2023-04-06 10:57:49 +0530

    SPECS-EXTENDED: drop 40 packages that have runtime-unmet dependencies
    Remove all packages that have unmet runtime deps and
    add them one-by-one in subsequent commits and ensure
    we do not introduce any runtime unmet depenndency.
    
    Following Packages are removed:
    - aqute-bnd
    - copy-jdk-configs
    - dump
    - foomatic
    - gnome-menus
    - httpcomponents-client
    - jansi-native
    - javapackages-tools-meta
    - jsch-agent-proxy
    - kdump-anaconda-addon
    - koan
    - libomp
    - libwmf
    - migrationtools
    - ortp
    - parboiled
    - perl-Alien-Libxml2
    - perl-TestML
    - plexus-component-api
    - plexus-io
    - plymouth
    - python-cliff
    - python-hacking
    - python-keyring
    - python-kombu
    - python-os-service-types
    - python-pycdlib
    - python-stestr
    - python-stevedore
    - python-testrepository
    - rust-packaging
    - setroubleshoot
    - snakeyaml
    - stratis-cli
    - subunit
    - targetd
    - varnish-modules
    - varnish
    - xdg-user-dirs
    - xguest
    
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- SPECS-EXTENDED/aqute-bnd/repack-tarball.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VERSION=`sed -rn 's/^Version:\s*([0-9.]+)/\1/p' aqute-bnd.spec`
-wget https://github.com/bndtools/bnd/archive/$VERSION.REL.tar.gz
-gunzip $VERSION.REL.tar.gz
-tar tf $VERSION.REL.tar | grep -E '\.(.ar|exe|tar\.(gz|bz2|xz)|zip)$' | xargs tar --delete --file $VERSION.REL.tar
-gzip $VERSION.REL.tar
