f30a22a4ec75c6c8e89c01314556df4652c8f09d
Daniel McIlvaney <damcilva@microsoft.com>
2023-04-17 12:09:25 -0700

    Revert "Rename to QUICKREBUILD"
    This reverts commit 96c09a1532562bc3cadc86c808c454b0f141b241.

--- toolkit/quickbuild_autoconfig.mk
+++ toolkit/quickbuild_autoconfig.mk
@@ -4,41 +4,41 @@
 # Contains:
 #	- build optimization flag auto configuration
 
-######## QUICKREBUILD AUTO CONFIGURE ########
+######## QUICKBUILD AUTO CONFIGURE ########
 
-# The QUICKREBUILD* flags are special flags that will try to build the toolchain and packages as quickly as possible. They will
+# The QUICKBUILD* flags are special flags that will try to build the toolchain and packages as quickly as possible. They will
 # automatically set REBUILD_TOOLS, USE_CCACHE, REBUILD_TOOLCHAIN, DELTA_BUILD, INCREMENTAL_TOOLCHAIN, and ALLOW_TOOLCHAIN_DOWNLOAD_FAIL to 'y'.
-QUICKREBUILD           ?= n
-QUICKREBUILD_TOOLCHAIN ?= n
-QUICKREBUILD_PACKAGES  ?= n
+QUICKBUILD           ?= n
+QUICKBUILD_TOOLCHAIN ?= n
+QUICKBUILD_PACKAGES  ?= n
 
-ifeq ($(QUICKREBUILD),y)
-QUICKREBUILD_TOOLCHAIN = y
-QUICKREBUILD_PACKAGES  = y
+ifeq ($(QUICKBUILD),y)
+QUICKBUILD_TOOLCHAIN = y
+QUICKBUILD_PACKAGES  = y
 REBUILD_TOOLS=y
 endif
 
-######## QUICKREBUILD TOOLCHAIN ########
+######## QUICKBUILD TOOLCHAIN ########
 
-ifeq ($(QUICKREBUILD_TOOLCHAIN),y)
+ifeq ($(QUICKBUILD_TOOLCHAIN),y)
 # If any of these are already set to 'n', report an error
 ifneq ($(filter n,$(REBUILD_TOOLCHAIN)),)
-$(error QUICKREBUILD cannot be used with REBUILD_TOOLCHAIN explicitly set to 'n')
+$(error QUICKBUILD cannot be used with REBUILD_TOOLCHAIN explicitly set to 'n')
 endif
 ifneq ($(filter n,$(INCREMENTAL_TOOLCHAIN)),)
-$(error QUICKREBUILD cannot be used with INCREMENTAL_TOOLCHAIN explicitly set to 'n')
+$(error QUICKBUILD cannot be used with INCREMENTAL_TOOLCHAIN explicitly set to 'n')
 endif
 ifneq ($(filter n,$(ALLOW_TOOLCHAIN_DOWNLOAD_FAIL)),)
-$(error QUICKREBUILD cannot be used with ALLOW_TOOLCHAIN_DOWNLOAD_FAIL explicitly set to 'n')
+$(error QUICKBUILD cannot be used with ALLOW_TOOLCHAIN_DOWNLOAD_FAIL explicitly set to 'n')
 endif
 REBUILD_TOOLCHAIN             = y
 INCREMENTAL_TOOLCHAIN         = y
 ALLOW_TOOLCHAIN_DOWNLOAD_FAIL = y
 endif
 
-######## QUICKREBUILD PACKAGES ########
+######## QUICKBUILD PACKAGES ########
 
-ifeq ($(QUICKREBUILD_PACKAGES),y)
+ifeq ($(QUICKBUILD_PACKAGES),y)
 # If any of these are already set to 'n', report an error
 ifneq ($(filter n,$(DELTA_BUILD)),)
 $(error DELTA_BUILD cannot be used with REBUILD_TOOLCHAIN explicitly  set to 'n')
