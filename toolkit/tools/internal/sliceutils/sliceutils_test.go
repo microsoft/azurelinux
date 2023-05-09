dd618cdae73dce7f797378ec25c8fadebd277bef
Pawel Winogrodzki <pawelwi@microsoft.com>
2023-05-09 06:49:58 -0700

    Adding slice utils for PackageVers.

--- toolkit/tools/internal/sliceutils/sliceutils_test.go
+++ toolkit/tools/internal/sliceutils/sliceutils_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/logger"
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/pkgjson"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,21 +17,70 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestShouldCreateEmptySliceFromNil(t *testing.T) {
+func TestPackageVersSetToSliceShouldCreateEmptySliceFromNil(t *testing.T) {
+	outputSlice := PackageVersSetToSlice(nil)
+
+	assert.NotNil(t, outputSlice)
+	assert.Empty(t, outputSlice)
+}
+
+func TestPackageVersSetToSliceShouldCreateEmptySliceFromEmptySet(t *testing.T) {
+	outputSlice := PackageVersSetToSlice(map[*pkgjson.PackageVer]bool{})
+
+	assert.NotNil(t, outputSlice)
+	assert.Empty(t, outputSlice)
+}
+
+func TestPackageVersSetToSliceShouldReturnValuesForAllTrueElementsInSet(t *testing.T) {
+	existingPackageVer := &pkgjson.PackageVer{Name: "A"}
+	missingPackageVer := &pkgjson.PackageVer{Name: "X"}
+	inputSet := map[*pkgjson.PackageVer]bool{
+		existingPackageVer: true,
+		missingPackageVer:  false,
+	}
+	outputSlice := PackageVersSetToSlice(inputSet)
+
+	assert.NotNil(t, outputSlice)
+	assert.Len(t, outputSlice, 2)
+	assert.Contains(t, outputSlice, existingPackageVer)
+	assert.NotContains(t, outputSlice, missingPackageVer)
+}
+
+func TestPackageVersShouldMatch(t *testing.T) {
+	packageVer1 := &pkgjson.PackageVer{Name: "A"}
+	packageVer2 := &pkgjson.PackageVer{Name: "A"}
+
+	assert.True(t, PackageVerMatch(packageVer1, packageVer2))
+}
+
+func TestPackageVersShouldNotMatch(t *testing.T) {
+	packageVer1 := &pkgjson.PackageVer{Name: "A"}
+	packageVer2 := &pkgjson.PackageVer{Name: "B"}
+
+	assert.False(t, PackageVerMatch(packageVer1, packageVer2))
+}
+
+func TestPackageVerShouldNotMatchNil(t *testing.T) {
+	packageVer1 := &pkgjson.PackageVer{Name: "A"}
+
+	assert.False(t, PackageVerMatch(packageVer1, nil))
+}
+
+func TestStringsSetToSliceShouldCreateEmptySliceFromNil(t *testing.T) {
 	outputSlice := StringsSetToSlice(nil)
 
 	assert.NotNil(t, outputSlice)
 	assert.Empty(t, outputSlice)
 }
 
-func TestShouldCreateEmptySliceFromEmptySet(t *testing.T) {
+func TestStringsSetToSliceShouldCreateEmptySliceFromEmptySet(t *testing.T) {
 	outputSlice := StringsSetToSlice(map[string]bool{})
 
 	assert.NotNil(t, outputSlice)
 	assert.Empty(t, outputSlice)
 }
 
-func TestShouldReturnValuesForAllTrueElementsInSet(t *testing.T) {
+func TestStringsSetToSliceShouldReturnValuesForAllTrueElementsInSet(t *testing.T) {
 	inputSet := map[string]bool{
 		"A": true,
 		"B": true,
