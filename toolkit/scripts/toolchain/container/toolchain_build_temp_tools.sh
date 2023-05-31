9db7f8a8fe674e717cf8072e46fc756c5fdabe1d
dallasd1 <dadelan@microsoft.com>
2023-05-31 11:52:18 -0700

    Upgrade python3 to 3.7.16

--- toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
+++ toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
@@ -543,15 +543,15 @@ rm -rf perl-5.30.3
 
 touch $LFS/logs/temptoolchain/status_perl_complete
 
-echo Python-3.7.13
-tar xf Python-3.7.13.tar.xz
-pushd Python-3.7.13
+echo Python-3.7.16
+tar xf Python-3.7.16.tar.xz
+pushd Python-3.7.16
 sed -i '/def add_multiarch_paths/a \        return' setup.py
 ./configure --prefix=/tools --without-ensurepip
 make -j$(nproc)
 make install
 popd
-rm -rf Python-3.7.13
+rm -rf Python-3.7.16
 
 touch $LFS/logs/temptoolchain/status_python_complete
 
