25758473249aa16edafab883f79cdb5a315ef5e7
Trung <tvuong@microsoft.com>
2023-06-07 11:56:24 -0700

    Add flag to disable PMC repository (#5607)

--- toolkit/scripts/imggen.mk
+++ toolkit/scripts/imggen.mk
@@ -93,6 +93,10 @@ ifeq ($(DISABLE_UPSTREAM_REPOS),y)
 imagepkgfetcher_extra_flags += --disable-upstream-repos
 endif
 
+ifeq ($(DISABLE_DEFAULT_REPOS),y)
+imagepkgfetcher_extra_flags += --disable-default-repos
+endif
+
 ifeq ($(USE_PREVIEW_REPO),y)
 imagepkgfetcher_extra_flags += --use-preview-repo
 endif
