3b0cfcc0162bb0b669b6b1b70ae4514d6159dbab
Rachel Menge <rachelmenge@microsoft.com>
2023-06-19 19:47:38 -0400

    Create map using regex

--- toolkit/scripts/kernel_sources_analysis.py
+++ toolkit/scripts/kernel_sources_analysis.py
@@ -5,6 +5,7 @@
 # Usage: python3 check_new_kernel_configs.py --required_configs <path to json of required configs> --config_str <string of diff for config file>
 
 import os
+import re
 import json
 
 def get_data_from_config(input_file):
@@ -34,13 +35,19 @@ def extract_config_arch(input_config_data):
         print("Error: Could not find architecture in config file")
         return None
 
+# Regex matching pairs of kernel config name and its value.
+#
+# Sample input:
+#   CONFIG_ABC=y
+#   CONFIG_XYZ=""
+#   # CONFIG_111 is not set
+#
+# Result:
+#   {
+#     'CONFIG_ABC': 'y',
+#     'CONFIG_XYZ': '""',
+#     'CONFIG_111': 'is not set'
+#   }
+CONFIG_TO_VALUE_REGEX=re.compile(r'(CONFIG_\w+)[ =](.*)')
 def create_map_of_config_values(input_config_data):
-    config_map = {}
-    for line in input_config_data.split('\n'):
-        if "=" in line:
-            config_map[line.split('=')[0]] = line.split('=')[1]
-        # Find configs that are not set
-        # Example: # CONFIG_FOO is not set
-        elif "is not set" in line:
-            config_map[line.split()[1]] = "is not set"
-    return config_map
+    return dict(CONFIG_TO_VALUE_REGEX.findall(input_config_data))
