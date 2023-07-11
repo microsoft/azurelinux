ad37bfd7fb705c449330c18968d31df852598220
Daniel McIlvaney <damcilva@microsoft.com>
2023-07-10 17:03:12 -0700

    Enable the graphpkgfetcher to pull build nodes from upstream repos if available V2 (#5595)
    ---------
    
    Co-authored-by: Pawel Winogrodzki <pawelwi@microsoft.com>

--- toolkit/tools/scheduler/schedulerutils/buildworker.go
+++ toolkit/tools/scheduler/schedulerutils/buildworker.go
@@ -38,6 +38,7 @@ type BuildRequest struct {
 	PkgGraph       *pkggraph.PkgGraph
 	AncillaryNodes []*pkggraph.PkgNode
 	CanUseCache    bool
+	IsDelta        bool
 }
 
 // BuildResult represents the results of a build agent trying to build a given node.
@@ -49,6 +50,7 @@ type BuildResult struct {
 	Node           *pkggraph.PkgNode
 	Skipped        bool
 	UsedCache      bool
+	WasDelta       bool
 }
 
 // selectNextBuildRequest selects a job based on priority:
@@ -99,6 +101,7 @@ func BuildNodeWorker(channels *BuildChannels, agent buildagents.BuildAgent, grap
 		res := &BuildResult{
 			Node:           req.Node,
 			AncillaryNodes: req.AncillaryNodes,
+			WasDelta:       req.IsDelta,
 		}
 
 		switch req.Node.Type {
