9018b05f7da332b469bf8fc38d6ef8378e1eed6f
jslobodzian <joslobo@microsoft.com>
2023-07-14 17:52:35 -0700

    Remove k3s from Mariner (#5814)
    * Remove k8s from Mariner
    
    * Fix license check error

--- toolkit/scripts/spec_source_attributions.py
+++ toolkit/scripts/spec_source_attributions.py
@@ -21,7 +21,6 @@ VALID_SOURCE_ATTRIBUTIONS = {
     "OpenMamba":                      r'\n-\s+Initial CBL-Mariner import from OpenMamba(\.|\n|$)',
     "OpenSUSE":                       r'\n-\s+Initial CBL-Mariner import from openSUSE \w+ \(license: same as "License" tag\)(\.|\n|$)',
     "Photon":                         r'\n-\s+Initial CBL-Mariner import from Photon \(license: Apache2\)(\.|\n|$)',
-    "Rancher":                        r'\n-\s+Initial CBL-Mariner import from Rancher \(license: ASL 2\.0\)(\.|\n|$)',
     "Sysbench source":                r'\n-\s+Initial CBL-Mariner import from Sysbench source \(license: GPLv2\+\)(\.|\n|$)',
     "RPM software management source": r'\n-\s+Initial CBL-Mariner import from RPM software management source \(license: GPLv2\+\)(\.|\n|$)'
 }
