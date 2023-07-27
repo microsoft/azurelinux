8f699ba244aabd52be2723c1c61344f376e5a5f4
Daniel McIlvaney <damcilva@microsoft.com>
2023-07-27 16:46:51 -0700

    revert chroot script, add progress

--- toolkit/tools/pkggen/worker/create_worker_chroot.sh
+++ toolkit/tools/pkggen/worker/create_worker_chroot.sh
@@ -22,10 +22,25 @@ chroot_builder_folder=$chroot_base/$chroot_name
 chroot_archive=$chroot_base/$chroot_name.tar.gz
 chroot_log="$log_path"/$chroot_name.log
 
+# We have two major steps per entry in the packages file: install the RPM, then add to database
+total_steps=$(wc -l < "$packages")
+total_steps=$((total_steps * 2))
+current_step=0
+# Print "<progress>%" and increment current_step
+function increment_progress() {
+    # Increment global counter current_step
+    current_step=$((current_step + 1))
+}
+function format_progress() {
+    progress=$((current_step * 100 / total_steps))
+    printf "%s%%" "$progress"
+}
+
 install_one_toolchain_rpm () {
     error_msg_tail="Inspect $chroot_log for more info. Did you hydrate the toolchain?"
 
-    echo "Adding RPM to worker chroot: $1." | tee -a "$chroot_log"
+    echo "Adding RPM to worker chroot $(format_progress): $1." | tee -a "$chroot_log"
+    increment_progress
 
     full_rpm_path=$(find "$rpm_path" -name "$1" -type f 2>>"$chroot_log")
     if [ ! $? -eq 0 ] || [ -z "$full_rpm_path" ]
@@ -36,37 +51,6 @@ install_one_toolchain_rpm () {
 
     echo "Found full path for package $1 in $rpm_path: ($full_rpm_path)" >> "$chroot_log"
     rpm -i -v --nodeps --noorder --force --root "$chroot_builder_folder" --define '_dbpath /var/lib/rpm' "$full_rpm_path" &>> "$chroot_log"
-
-    if [ ! $? -eq 0 ]
-    then
-        echo "Elevated install failed for package $1, aborting. $error_msg_tail" | tee -a "$chroot_log"
-        exit 1
-    fi
-}
-
-install_multiple_toolchain_rpms () {
-    error_msg_tail="Inspect $chroot_log for more info. Did you hydrate the toolchain?"
-
-    # Create empty array
-    declare -a full_rpm_paths
-
-    for rpm in "$@"
-    do
-        echo "    Batching install of RPM to worker chroot: $rpm." | tee -a "$chroot_log"
-        full_rpm_path=$(find "$rpm_path" -name "$rpm" -type f 2>>"$chroot_log")
-        if [ ! $? -eq 0 ] || [ -z "$full_rpm_path" ]
-        then
-            echo "Failed to locate package $1 in ($rpm_path), aborting. $error_msg_tail" | tee -a "$chroot_log"
-            exit 1
-        fi
-        echo "Found full path for package $1 in $rpm_path: ($full_rpm_path)" >> "$chroot_log"
-
-        # add to array
-        full_rpm_paths+=("$full_rpm_path")
-    done
-
-    echo "Installing batched packages..."
-    rpm -i -v --nodeps --force --root "$chroot_builder_folder" --define '_dbpath /var/lib/rpm' "${full_rpm_paths[@]}" &>> "$chroot_log"
 
     if [ ! $? -eq 0 ]
     then
@@ -91,16 +75,13 @@ mknod -m 600 $chroot_builder_folder/dev/console c 5 1
 mknod -m 666 $chroot_builder_folder/dev/null c 1 3
 mknod -m 444 $chroot_builder_folder/dev/urandom c 1 9
 
-# Start with filesystem
+# Start with filesystem, then macros, then everything else
 install_one_toolchain_rpm "$(cat "$packages" | grep "^filesystem-.*rpm$")"
+install_one_toolchain_rpm "$(cat "$packages" | grep "^mariner-rpm-macros-.*rpm$")"
 
-# Convert the file $packages into an array
-packages_to_install=()
 while read -r package || [ -n "$package" ]; do
-    packages_to_install+=("$package")
+    install_one_toolchain_rpm "$package"
 done < "$packages"
-
-install_multiple_toolchain_rpms "${packages_to_install[@]}"
 
 # If the host machine rpm version is >= 4.16 (such as Mariner 2.0), it will create an "sqlite" rpm database backend incompatible with Mariner 1.0 (which uses "bdb")
 # To resolve this, enter the 1.0 chroot after the packages are installed, and use the older rpm tool in the chroot to re-create the database in "bdb" format.
@@ -121,7 +102,8 @@ else
     while read -r package || [ -n "$package" ]; do
         full_rpm_path=$(find "$rpm_path" -name "$package" -type f 2>>"$chroot_log")
         cp $full_rpm_path $chroot_builder_folder/$package
-        echo "Adding RPM DB entry to worker chroot: $package." | tee -a "$chroot_log"
+        echo "Adding RPM DB entry to worker chroot $(format_progress): $package." | tee -a "$chroot_log"
+        increment_progress
         chroot "$chroot_builder_folder" rpm -i -v --nodeps --noorder --force --dbpath="$TEMP_DB_PATH" --justdb "$package" &>> "$chroot_log"
         chroot "$chroot_builder_folder" rm $package
     done < "$packages"
