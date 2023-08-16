73cdd22b16a4ff8c727e762a6d2eccfacfd1b539
Bala <kumaran.4353@gmail.com>
2023-08-16 10:35:23 +0530

    Update toolkit/tools/internal/pkggraph/pkggraph.go
    Co-authored-by: Pawel Winogrodzki <pawelwi@microsoft.com>

--- toolkit/tools/internal/pkggraph/pkggraph.go
+++ toolkit/tools/internal/pkggraph/pkggraph.go
@@ -1283,7 +1283,7 @@ func (g *PkgGraph) MakeDAG() (err error) {
 
 // MakeDAG ensures the graph is a directed acyclic graph (DAG).
 // If the graph is not a DAG, this routine will attempt to resolve any cycles to make the graph a DAG.
-func (g *PkgGraph) MakeDAGusingUpstreamRepos(resolveCyclesFromUpstream, ignoreVersionToResolveSelfDep bool, cloner *rpmrepocloner.RpmRepoCloner) (err error) {
+func (g *PkgGraph) MakeDAGUsingUpstreamRepos(resolveCyclesFromUpstream, ignoreVersionToResolveSelfDep bool, cloner *rpmrepocloner.RpmRepoCloner) (err error) {
 	timestamp.StartEvent("convert to DAG", nil)
 	defer timestamp.StopEvent(nil)
 	var cycle []*PkgNode
