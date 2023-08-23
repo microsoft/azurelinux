d367e4560e0271c53df735a25c729d58bca1fa89
Daniel McIlvaney <damcilva@microsoft.com>
2023-08-23 16:49:50 -0700

    Fix retry backoff sleep non-determinism (#6040)
    * Fix retry backoff sleep non-determinism
    
    * Split into two selects

--- toolkit/tools/internal/retry/retry.go
+++ toolkit/tools/internal/retry/retry.go
@@ -35,6 +35,15 @@ func calculateLinearDelay(failCount int, sleep time.Duration) time.Duration {
 // backoffSleep sleeps for the given duration, unless the cancel channel is triggered or closed first. The cancel channel
 // may be nil in which case the sleep will always complete.
 func backoffSleep(delay time.Duration, cancel <-chan struct{}) (cancelled bool) {
+	// Check if we were cancelled before we sleep to avoid a race condition with
+	// delay=0 and an immediate cancel.
+	select {
+	case <-cancel:
+		cancelled = true
+		return
+	default:
+	}
+
 	select {
 	case <-time.After(delay):
 	case <-cancel:
