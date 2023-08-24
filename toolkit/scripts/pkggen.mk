06fb0785068677ebc87c47cd3255e98271ca4f31
Pawel Winogrodzki <pawelwi@microsoft.com>
2023-08-24 09:31:15 -0700

    Adjusting cache hydration to latest 'main'.

--- toolkit/scripts/pkggen.mk
+++ toolkit/scripts/pkggen.mk
@@ -300,11 +300,11 @@ compress-srpms:
 # Seed the cached RPMs folder files from the archive.
 hydrate-cached-rpms:
 	$(if $(CACHED_PACKAGES_ARCHIVE),,$(error Must set CACHED_PACKAGES_ARCHIVE=<path>))
-	@echo Unpacking cache RPMs from $(CACHED_PACKAGES_ARCHIVE) into $(CACHED_RPMS_DIR)
-	@tar -xf $(CACHED_PACKAGES_ARCHIVE) -C $(CACHED_RPMS_DIR) --strip-components 1 --skip-old-files --touch --checkpoint=100000 --checkpoint-action=echo="%T"
+	@echo Unpacking cache RPMs from $(CACHED_PACKAGES_ARCHIVE) into $(remote_rpms_cache_dir)
+	@tar -xf $(CACHED_PACKAGES_ARCHIVE) -C $(remote_rpms_cache_dir) --strip-components 1 --skip-old-files --touch --checkpoint=100000 --checkpoint-action=echo="%T"
 # The cached RPMs directory has a flat structure, so we need to move the RPMs into the cache's root directory.
-	@find $(CACHED_RPMS_DIR) -mindepth 2 -name "*.rpm" -exec mv {} $(CACHED_RPMS_DIR) \;
-	@find $(CACHED_RPMS_DIR) -mindepth 1 -type d -and ! -name repodata -exec rm -fr {} +
+	@find $(remote_rpms_cache_dir) -mindepth 2 -name "*.rpm" -exec mv {} $(remote_rpms_cache_dir) \;
+	@find $(remote_rpms_cache_dir) -mindepth 1 -type d -and ! -name repodata -exec rm -fr {} +
 
 # Seed the RPMs folder with the any missing files from the archive.
 hydrate-rpms:
