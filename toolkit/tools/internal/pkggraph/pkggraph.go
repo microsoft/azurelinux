3064d35e474e29df9530c54200bfe9f170e5284b
Bala <balakumaran.kannan@microsoft.com>
2023-08-29 08:20:32 +0000

    Remove unnecessary assignment in pkggraph.go

--- toolkit/tools/internal/pkggraph/pkggraph.go
+++ toolkit/tools/internal/pkggraph/pkggraph.go
@@ -304,7 +304,7 @@ func (g *PkgGraph) validateNodeForLookup(pkgNode *PkgNode) (valid bool, err erro
 			// It is possible that
 			// 1. LocalRun and RemoteRun co-exist in a graph
 			// 2. Multiple RemoteRun nodes can exist in a graph
-			haveDuplicateNode = false
+			// So, this is not considered as a duplicate node
 		case TypeTest:
 			haveDuplicateNode = existingLookup.TestNode != nil
 		}
