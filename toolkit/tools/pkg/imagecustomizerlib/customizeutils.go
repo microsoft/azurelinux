0ef7e3ec96155068423e6488381716dcd3953038
Chris Gunn <chrisgun@microsoft.com>
2023-10-04 15:59:09 -0700

    Image Customizer: Add/update users. (#6336)

--- toolkit/tools/pkg/imagecustomizerlib/customizeutils.go
+++ toolkit/tools/pkg/imagecustomizerlib/customizeutils.go
@@ -6,13 +6,18 @@ package imagecustomizerlib
 import (
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/imagecustomizerapi"
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/imagegen/installutils"
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/file"
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/logger"
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/safechroot"
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/safemount.go"
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/shell"
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/userutils"
 	"golang.org/x/sys/unix"
 )
 
@@ -32,6 +37,11 @@ func doCustomizations(baseConfigPath string, config *imagecustomizerapi.SystemCo
 	}
 
 	err = copyAdditionalFiles(baseConfigPath, config.AdditionalFiles, imageChroot)
+	if err != nil {
+		return err
+	}
+
+	err = addOrUpdateUsers(config.Users, baseConfigPath, imageChroot)
 	if err != nil {
 		return err
 	}
@@ -115,6 +125,98 @@ func runScripts(baseConfigPath string, scripts []imagecustomizerapi.Script, imag
 	}
 
 	err = mount.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addOrUpdateUsers(users []imagecustomizerapi.User, baseConfigPath string, imageChroot *safechroot.Chroot) error {
+	for _, user := range users {
+		err := addOrUpdateUser(user, baseConfigPath, imageChroot)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func addOrUpdateUser(user imagecustomizerapi.User, baseConfigPath string, imageChroot *safechroot.Chroot) error {
+	var err error
+
+	logger.Log.Infof("Adding/updating user (%s)", user.Name)
+
+	password := user.Password
+	if user.PasswordPath != "" {
+		// Read password from file.
+		passwordFullPath := filepath.Join(baseConfigPath, user.PasswordPath)
+
+		passwordFileContents, err := os.ReadFile(passwordFullPath)
+		if err != nil {
+			return fmt.Errorf("failed to read password file (%s): %w", passwordFullPath, err)
+		}
+
+		password = string(passwordFileContents)
+	}
+
+	// Hash the password.
+	hashedPassword := password
+	if !user.PasswordHashed {
+		hashedPassword, err = userutils.HashPassword(user.Password)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Check if the user already exists.
+	userExists, err := userutils.UserExists(user.Name, imageChroot)
+	if err != nil {
+		return err
+	}
+
+	if userExists {
+		// Update the user's password.
+		err = installutils.UpdateUserPassword(imageChroot.RootDir(), user.Name, hashedPassword)
+		if err != nil {
+			return err
+		}
+	} else {
+		var uidStr string
+		if user.UID != nil {
+			uidStr = strconv.Itoa(*user.UID)
+		}
+
+		// Add the user.
+		err = userutils.AddUser(user.Name, hashedPassword, uidStr, imageChroot)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Set user's password expiry.
+	if user.PasswordExpiresDays != nil {
+		err = installutils.Chage(imageChroot, *user.PasswordExpiresDays, user.Name)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Set user's groups.
+	err = installutils.ConfigureUserGroupMembership(imageChroot, user.Name, user.PrimaryGroup, user.SecondaryGroups)
+	if err != nil {
+		return err
+	}
+
+	// Set user's SSH keys.
+	err = installutils.ProvisionUserSSHCerts(imageChroot, user.Name, user.SSHPubKeyPaths)
+	if err != nil {
+		return err
+	}
+
+	// Set user's startup command.
+	err = installutils.ConfigureUserStartupCommand(imageChroot, user.Name, user.StartupCommand)
 	if err != nil {
 		return err
 	}
