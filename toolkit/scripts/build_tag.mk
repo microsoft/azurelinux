c1d0ad8e0df1c7043f7c3838e577a5337489dbf6
Chris Gunn <chrisgun@microsoft.com>
2023-10-26 16:18:46 -0700

    Image Customizer: Add tool version.
    Add a version to the image customizer tool. This version and the
    version format will be different from the CBL-Mariner OS.
    
    This change updates the `Makefile` files to handle this new version.
    It also ensures that the `--version` CLI option reports the calculated
    version.

--- toolkit/scripts/build_tag.mk
+++ toolkit/scripts/build_tag.mk
@@ -8,7 +8,8 @@
 
 DIST_TAG           ?= .cm2
 # Running 'git' as the owner of the repo, so it doesn't complain about the repo not belonging to root.
-BUILD_NUMBER       ?= $(call shell_real_build_only, if [ -n "$$UID" ] && [ "$$UID" -eq 0 ]; then runuser -u $$(stat -c "%U" $(PROJECT_ROOT)) -- git rev-parse --short HEAD; else git rev-parse --short HEAD; fi)
+GIT_COMMIT_ID := $(call shell_real_build_only, if [ -n "$$UID" ] && [ "$$UID" -eq 0 ]; then runuser -u $$(stat -c "%U" $(PROJECT_ROOT)) -- git rev-parse --short HEAD; else git rev-parse --short HEAD; fi)
+BUILD_NUMBER ?= $(GIT_COMMIT_ID)
 # an empty BUILD_NUMBER breaks the build later on
 ifeq ($(BUILD_NUMBER),)
    BUILD_NUMBER = non-git
@@ -16,12 +17,13 @@ endif
 # Staticly define BUILD_NUMBER so it is set only once
 BUILD_NUMBER := $(BUILD_NUMBER)
 RELEASE_MAJOR_ID   ?= 2.0
+DATETIME_AS_VERSION := $(shell date +'%Y%m%d.%H%M')
 # use minor ID defined in file (if exist) otherwise define it
 # note this file must be single line
 ifneq ($(wildcard $(OUT_DIR)/version-minor-id.config),)
    RELEASE_MINOR_ID ?= .$(shell cat $(OUT_DIR)/version-minor-id.config)
 else
-   RELEASE_MINOR_ID ?= .$(shell date +'%Y%m%d.%H%M')
+   RELEASE_MINOR_ID ?= .$(DATETIME_AS_VERSION)
 endif
 RELEASE_VERSION    ?= $(RELEASE_MAJOR_ID)$(RELEASE_MINOR_ID)
 
@@ -31,3 +33,8 @@ RELEASE_VERSION := $(RELEASE_VERSION)
 
 # Image tag - empty by default. Does not apply to the initrd.
 IMAGE_TAG          ?=
+
+# Image Customizer Version
+IMAGE_CUSTOMIZER_VERSION ?= 0.1.0
+IMAGE_CUSTOMIZER_VERSION_PREVIEW ?= -dev.$(DATETIME_AS_VERSION)+$(GIT_COMMIT_ID)
+IMAGE_CUSTOMIZER_FULL_VERSION := $(IMAGE_CUSTOMIZER_VERSION)$(IMAGE_CUSTOMIZER_VERSION_PREVIEW)
