75f30df72e0e5752da30ee3dda665465cbf789c1
Sam Meluch <sam.meluch@microsoft.com>
2023-11-02 13:46:33 -0700

    fix if syntax... again...

--- toolkit/scripts/sodiff/mariner-sodiff.sh
+++ toolkit/scripts/sodiff/mariner-sodiff.sh
@@ -13,22 +13,22 @@ while getopts "r:f:v:o:e:" opt; do
     esac
 done
 
-if [[ -z "$rpms_folder"]]; then
+if [[ -z "$rpms_folder" ]]; then
     echo "INVALID ARBUMENT: RPMS_FOLDER is empty"
     exit 1
 fi
 
-if [[ -z "$repo_file_path"]]; then
+if [[ -z "$repo_file_path" ]]; then
     echo "INVALID ARBUMENT: REPO_FILE_PATH is empty"
     exit 1
 fi
 
-if [[ -z "$mariner_version"]]; then
+if [[ -z "$mariner_version" ]]; then
     echo "INVALID ARBUMENT: MARINER_VERSION is empty"
     exit 1
 fi
 
-if [[ -z "$sodiff_out_dir"]]; then
+if [[ -z "$sodiff_out_dir" ]]; then
     echo "INVALID ARBUMENT: SODIFF_OUT_DIR is empty"
     exit 1
 fi
