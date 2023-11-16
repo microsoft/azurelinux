7e0dbc4368b98b09f7afbd461bbc357acb5d2776
George Mileka <gmileka@microsoft.com>
2023-11-16 14:46:17 -0800

    Separate base from user start-up scripts.

---
+++ toolkit/mic-iso-gen/files/stock/iso-image-installer/iso-image-installer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Running iso-image-installer.sh v1435."
+
+if [[ -z $iso_media_mount_dir ]]; then
+    echo "iso_media_mount_dir is not configured. Exiting."
+    /bin/bash
+fi
+
+image_file_path=$(cat $iso_media_mount_dir/artifacts/host-configuration.json | jq -r '.imaging.images[0].url')
+image_target_device=$(cat $iso_media_mount_dir/artifacts/host-configuration.json | jq -r '.storage.disks[0].device')
+
+echo "Installing $image_file_path to $image_target_device..."
+dd if=$image_file_path of=$image_target_device bs=4M
+
+# Copy file to the rootfs
+#
+# rootfs_target_partition=${image_target_device}2
+# rootfs_mount_dir=/mnt/sda2
+# mkdir -p $rootfs_mount_dir
+# mount $rootfs_target_partition $rootfs_mount_dir
+# cp $iso_media_mount_dir/artifacts/host-configuration.yaml $rootfs_mount_dir/etc/
+# sleep 1s
+# umount $rootfs_target_partition
+
+echo "Ejecting the CD-ROM..."
+umount /dev/cdrom
+eject /dev/cdrom
+
+echo "Rebooting..."
+reboot
