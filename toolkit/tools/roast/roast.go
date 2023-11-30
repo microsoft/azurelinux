a2d3743631d6d554905a81552b3be2c0faadb5c2
George Mileka <gmileka@microsoft.com>
2023-11-30 11:31:39 -0800

    Comment out roast changes.

--- toolkit/tools/roast/roast.go
+++ toolkit/tools/roast/roast.go
@@ -47,7 +47,8 @@ var (
 	inputDir  = exe.InputDirFlag(app, "A directory containing a .RAW image or a rootfs directory")
 	outputDir = exe.OutputDirFlag(app, "A destination directory for the output image")
 
-	// configFile = app.Flag("config", "Path to the image config file.").Required().ExistingFile()
+	// george - comment this out.
+	configFile = app.Flag("config", "Path to the image config file.").Required().ExistingFile()
 	tmpDir     = app.Flag("tmp-dir", "Directory to store temporary files while converting.").Required().String()
 
 	releaseVersion = app.Flag("release-version", "Release version to add to the output artifact name").String()
@@ -100,7 +101,7 @@ func main() {
 	logger.Log.Infof("inputDir = %s", inDirPath)
 	logger.Log.Infof("outputDir = %s", outDirPath)
 	logger.Log.Infof("tmpDir = %s", tmpDirPath)
-
+/*
 	newInitrdFile, err := 
 	  convertArtifact("initrd", outDirPath, "initrd", "", inDirPath , false, true)
 	if err != nil {
@@ -108,7 +109,7 @@ func main() {
 	}
 
 	logger.Log.Infof("newInitrdFile = %s", newInitrdFile)
-/*
+*/	
 	config, err := configuration.Load(*configFile)
 	if err != nil {
 		logger.Log.Panicf("Failed loading image configuration. Error: %s", err)
@@ -118,7 +119,6 @@ func main() {
 	if err != nil {
 		logger.Log.Panic(err)
 	}
-*/
 }
 
 func generateImageArtifacts(workers int, inDir, outDir, releaseVersion, imageTag, tmpDir string, config configuration.Config) (err error) {
