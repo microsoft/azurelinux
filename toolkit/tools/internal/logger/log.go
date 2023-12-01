bc2a9009312396cd1d915723d5521d049383a5e2
Daniel McIlvaney <damcilva@microsoft.com>
2023-12-01 11:52:42 -0800

    Give logs to calling user

--- toolkit/tools/internal/logger/log.go
+++ toolkit/tools/internal/logger/log.go
@@ -15,6 +15,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/marinertoolusers"
 	"github.com/sirupsen/logrus"
 )
 
@@ -63,6 +64,11 @@ func initLogFile(filePath string) (err error) {
 	}
 
 	file, err := os.Create(filePath)
+	if err != nil {
+		return
+	}
+
+	err = marinertoolusers.GiveSinglePathToUser(filePath, marinertoolusers.GetMarinerBuildUser())
 	if err != nil {
 		return
 	}
