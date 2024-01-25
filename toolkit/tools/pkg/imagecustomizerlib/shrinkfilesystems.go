7682413cc2db839a80fa1f00fd48ae18c92b648f
amritakohli <amritakohli@microsoft.com>
2024-01-24 18:13:10 -0800

    remove comment

--- toolkit/tools/pkg/imagecustomizerlib/shrinkfilesystems.go
+++ toolkit/tools/pkg/imagecustomizerlib/shrinkfilesystems.go
@@ -7,7 +7,6 @@ import (
 	"github.com/microsoft/CBL-Mariner/toolkit/tools/internal/shell"
 	"regexp"
 	"strconv"
-	// "strings"
 )
 
 func shrinkFilesystems(imageLoopDevice string, outputImageFile string) error {
