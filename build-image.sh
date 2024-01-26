9620e51e2055d29f1953152667a03a1a349f2c00
George Mileka <gmileka@microsoft.com>
2024-01-26 13:58:15 -0800

    Minor update to build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -3,6 +3,12 @@
 set -x
 set -e
 
+pushd /home/george/git/CBL-Mariner-POC
+
+sudo rm -f ./mic-build/out/mic-20240124-1514.vhdx
+sudo rm -rf ./out/images/baremetal
+sudo rm -rf ./build/imagegen/baremetal
+
 pushd /home/george/git/CBL-Mariner-POC/toolkit
 
 sudo make image \
