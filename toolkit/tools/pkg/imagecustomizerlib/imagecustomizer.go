94126f7fa4586aba9cd8687735b92e2199d64df2
George Mileka <gmileka@microsoft.com>
2024-01-30 11:44:00 -0800

    Minor diff fixing.

--- toolkit/tools/pkg/imagecustomizerlib/imagecustomizer.go
+++ toolkit/tools/pkg/imagecustomizerlib/imagecustomizer.go
@@ -99,6 +99,7 @@ func CustomizeImage(buildDir string, baseConfigPath string, config *imagecustomi
 	// Convert image file to raw format, so that a kernel loop device can be used to make changes to the image.
 	rawImageFile := filepath.Join(buildDirAbs, BaseImageName)
 
+	logger.Log.Infof("Create raw base image: %s", rawImageFile)
 	err = shell.ExecuteLiveWithErr(1, "qemu-img", "convert", "-O", "raw", imageFile, rawImageFile)
 	if err != nil {
 		return fmt.Errorf("failed to convert image file to raw format:\n%w", err)
@@ -135,8 +136,8 @@ func CustomizeImage(buildDir string, baseConfigPath string, config *imagecustomi
 
 	// Create final output image file if requested.
 	if outputImageFormat != "" && outputImageFormat != "iso" {
-
 		logger.Log.Infof("Writing: %s", outputImageFile)
+
 		outDir := filepath.Dir(outputImageFile)
 		os.MkdirAll(outDir, os.ModePerm)
 
@@ -146,8 +147,8 @@ func CustomizeImage(buildDir string, baseConfigPath string, config *imagecustomi
 		}
 
 	} else if outputImageFormat == "iso" {
-
 		logger.Log.Infof("creating a liveos iso from the customized image...")
+
 		err = createLiveOSIsoImage(buildDir, rawImageFile, outputImageDir, outputImageBase)
 		if err != nil {
 			return err
