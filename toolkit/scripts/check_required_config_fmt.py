d1fde7711f6ae3a3d5fa69f7e97121784ca8be53
Vince Perri <viperri@microsoft.com>
2024-01-30 18:02:10 +0000

    format with python3 black

--- toolkit/scripts/check_required_config_fmt.py
+++ toolkit/scripts/check_required_config_fmt.py
@@ -7,13 +7,17 @@ import sys
 import tempfile
 import os
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Load the JSON file from the command line argument.
-    parser = argparse.ArgumentParser(description='Check that a JSON file is sorted and indented')
-    parser.add_argument('--required_configs', help='path to JSON of required configs', required=True)
+    parser = argparse.ArgumentParser(
+        description="Check that a JSON file is sorted and indented"
+    )
+    parser.add_argument(
+        "--required_configs", help="path to JSON of required configs", required=True
+    )
     args = parser.parse_args()
     required_configs = args.required_configs
-    with open(required_configs, 'r') as f:
+    with open(required_configs, "r") as f:
         content = json.load(f)
 
     # Sort lists, which is not something done by json.dump.
@@ -23,7 +27,7 @@ if __name__ == '__main__':
         info["value"] = sorted(info["value"])
 
     # Sort the JSON file and write it to a temporary file.
-    with tempfile.NamedTemporaryFile(mode='w') as f:
+    with tempfile.NamedTemporaryFile(mode="w") as f:
         json.dump(content, f, indent=4, sort_keys=True)
 
         # Flush the file to disk before passing it to 'git diff' below so
@@ -36,9 +40,8 @@ if __name__ == '__main__':
         # exit with a non-zero exit code. Otherwise, print nothing and exit
         # successfully.
         try:
-            subprocess.check_call(['git', 'diff',
-                '--no-index',
-                required_configs,
-                f.name])
+            subprocess.check_call(
+                ["git", "diff", "--no-index", required_configs, f.name]
+            )
         except subprocess.CalledProcessError:
             sys.exit(1)
