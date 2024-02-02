3aee8e2cf664a3df9630b0fdc5769983f48cbc27
Lanze Liu <86434077+liulanze@users.noreply.github.com>
2024-02-01 22:39:36 -0800

    Bugfix adding dracut config recommended white spaces. (#7622)
    Co-authored-by: lanzeliu <lanzeliu@microsoft.com>

--- toolkit/tools/pkg/imagecustomizerlib/customizeverity.go
+++ toolkit/tools/pkg/imagecustomizerlib/customizeverity.go
@@ -69,8 +69,9 @@ func buildDracutModule(dracutModuleName string, dracutDriverName string, imageCh
 	// Check if the dracut module configuration file already exists.
 	if _, err := os.Stat(dracutConfigFile); os.IsNotExist(err) {
 		lines := []string{
-			"add_dracutmodules+=\"" + dracutModuleName + "\"",
-			"add_drivers+=\"" + dracutDriverName + "\"",
+			// Add white spaces on both sides for dracut config syntax.
+			"add_dracutmodules+=\" " + dracutModuleName + " \"",
+			"add_drivers+=\" " + dracutDriverName + " \"",
 		}
 		err = file.WriteLines(lines, dracutConfigFile)
 		if err != nil {
