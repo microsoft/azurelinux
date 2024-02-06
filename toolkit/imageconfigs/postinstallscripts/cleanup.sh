c04cab7faf3fc3818746b30ed113785b581b0392
Roaa Sakr <romoh@microsoft.com>
2024-02-06 10:07:17 -0800

    updates
    Signed-off-by: Roaa Sakr <romoh@microsoft.com>

--- toolkit/imageconfigs/postinstallscripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-# cleanup symlinks created by the toolkit that are not needed for base images
-rm -rf /srv
-
-# cleanup any logs that may have been created during the build
-rm -rf /var/log/*
---
+++ toolkit/imageconfigs/scripts/cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+
+# cleanup symlinks created by the toolkit that are not needed for base images
+if [ -L /srv ]; then
+  echo "Removing /srv symlink"
+  rm /srv
+else
+  echo "/srv symlink does not exist"
+fi
+
+# cleanup any logs that may have been created during the build
+if [ -L /var/log ]; then
+  echo "Clearing /var/log"
+  rm -rf /var/log/*
+else
+  echo "/var/log does not exist"
+fi
