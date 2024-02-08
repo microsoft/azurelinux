e83a9ead62be519a17ca19d5df814ac7538b99dd
George Mileka <gmileka@users.noreply.github.com>
2024-02-08 13:46:53 -0800

    Add support for additional files in mic generated isos. (#7660)

---
+++ toolkit/tools/imagecustomizerapi/additionalfilesmap.go
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package imagecustomizerapi
+
+import (
+	"errors"
+	"fmt"
+)
+
+type AdditionalFilesMap map[string]FileConfigList
+
+func (afmap AdditionalFilesMap) IsValid() error {
+	var aggregateErr error
+	for sourcePath, fileConfigList := range afmap {
+		if len(sourcePath) == 0 {
+			aggregateErr = errors.Join(aggregateErr, fmt.Errorf("invalid source path. Source path cannot be empty."))
+		}
+		err := fileConfigList.IsValid()
+		if err != nil {
+			aggregateErr = errors.Join(aggregateErr, fmt.Errorf("invalid file configs for (%s):\n%w", sourcePath, err))
+		}
+	}
+	return aggregateErr
+}
