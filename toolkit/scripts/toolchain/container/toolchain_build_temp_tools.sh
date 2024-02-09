8aca47ef4778937d47015b28817d1ebddd03ba49
Rachel Menge <rachelmenge@microsoft.com>
2024-02-09 14:54:01 -0500

    Address missing Dockerfile for raw toolchain

--- toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
+++ toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
@@ -86,7 +86,7 @@ rm -rf gcc-13.2.0
 
 touch $LFS/logs/temptoolchain/status_gcc_pass1_complete
 
-KERNEL_VERSION="6.1.58.1"
+KERNEL_VERSION="6.6.14.1"
 echo Linux-${KERNEL_VERSION} API Headers
 tar xf kernel-${KERNEL_VERSION}.tar.gz
 pushd CBL-Mariner-Linux-Kernel-rolling-lts-mariner-3-${KERNEL_VERSION}
