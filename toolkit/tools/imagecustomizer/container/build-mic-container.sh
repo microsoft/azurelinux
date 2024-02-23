3b704aa5fb415e61b6c16afd2324ff6b58d2c29e
George Mileka <gmileka@users.noreply.github.com>
2024-02-23 09:26:37 -0800

    [MIC] Enable MIC to run in a container. (#7919)

---
+++ toolkit/tools/imagecustomizer/container/build-mic-container.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# set -x
+set -e
+
+scriptDir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+enlistmentRoot=$scriptDir/../../../..
+
+function showUsage() {
+    echo
+    echo "usage:"
+    echo
+    echo "build-mic-container.sh \\"
+    echo "    -r <container-registry> \\"
+    echo "    -n <container-name> \\"
+    echo "    -t <container-tag>"
+    echo
+}
+
+while getopts ":r:n:t:" OPTIONS; do
+  case "${OPTIONS}" in
+    r ) containerRegistery=$OPTARG ;;
+    n ) containerName=$OPTARG ;;
+    t ) containerTag=$OPTARG ;;
+  esac
+done
+
+if [[ -z $containerRegistery ]]; then
+    echo "missing required argument '-r containerRegistry'"
+    showUsage
+    exit 1
+fi
+
+if [[ -z $containerName ]]; then
+    echo "missing required argument '-n containerName'"
+    showUsage
+    exit 1
+fi
+
+if [[ -z $containerTag ]]; then
+    echo "missing required argument '-t containerTag'"
+    showUsage
+    exit 1
+fi
+
+# ---- main ----
+
+containerStagingFolder=$(mktemp -d)
+
+function cleanUp() {
+    local exit_code=$?
+    sudo rm -rf $containerStagingFolder
+    exit $exit_code
+}
+trap 'cleanUp' ERR
+
+micLocalFile=$enlistmentRoot/toolkit/tools/imagecustomizer/imagecustomizer
+micContainerFolder=/usr/bin
+
+containerFullPath=$containerRegistery/$containerName/$containerTag
+dockerFile=$enlistmentRoot/toolkit/tools/imagecustomizer/container/Dockerfile.mic-container
+
+# stage those files that need to be in the container
+mkdir -p ${containerStagingFolder}${micContainerFolder}
+cp $micLocalFile ${containerStagingFolder}${micContainerFolder}
+touch ${containerStagingFolder}/.mariner-toolkit-ignore-dockerenv
+
+# build the container
+pushd $containerStagingFolder
+docker build -f $dockerFile . -t $containerFullPath
+popd
+
+# clean-up
+cleanUp
