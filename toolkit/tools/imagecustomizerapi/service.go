c4815fc2f1295ba5a8a96c867911f5ce2c27d86d
Chris Gunn <chrisgun@microsoft.com>
2024-03-04 16:15:09 -0800

    Image Customizer: Simplify services API.
    Allow the user to specify the name of a service to enable/disable
    as just a string, instead of an object with a `Name` field. This
    ensures the API matches what is in the documentation.
    
    Note: This doesn't exclude us from having a more complex API for
    services in the future, as we can write a custom unmarshaller that can
    handle both strings and objects. (Like what was done for `FileConfig`.)

--- toolkit/tools/imagecustomizerapi/service.go
+++ toolkit/tools/imagecustomizerapi/service.go
@@ -7,12 +7,8 @@ import (
 	"fmt"
 )
 
-type Service struct {
-	Name string `yaml:"Name"`
-}
-
-func (s *Service) IsValid() error {
-	if s.Name == "" {
+func serviceNameIsValid(name string) error {
+	if name == "" {
 		return fmt.Errorf("name of service may not be empty")
 	}
 
@@ -20,20 +16,20 @@ func (s *Service) IsValid() error {
 }
 
 type Services struct {
-	Enable  []Service `yaml:"Enable"`
-	Disable []Service `yaml:"Disable"`
+	Enable  []string `yaml:"Enable"`
+	Disable []string `yaml:"Disable"`
 }
 
 func (s *Services) IsValid() error {
 	for i, service := range s.Enable {
-		if err := service.IsValid(); err != nil {
-			return fmt.Errorf("invalid service '%s' in Service.Enable at index %d: %w", service.Name, i, err)
+		if err := serviceNameIsValid(service); err != nil {
+			return fmt.Errorf("invalid service '%s' in Service.Enable at index %d: %w", service, i, err)
 		}
 	}
 
 	for i, service := range s.Disable {
-		if err := service.IsValid(); err != nil {
-			return fmt.Errorf("invalid service '%s' in Service.Disable at index %d: %w", service.Name, i, err)
+		if err := serviceNameIsValid(service); err != nil {
+			return fmt.Errorf("invalid service '%s' in Service.Disable at index %d: %w", service, i, err)
 		}
 	}
 
