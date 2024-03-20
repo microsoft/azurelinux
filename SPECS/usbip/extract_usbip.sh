6e99c3c7c6e520ddd58975d6445d82dd3958f091
Daniel McIlvaney <damcilva@microsoft.com>
2024-03-20 15:34:39 -0700

    Update usbip to 6.6.14.1 (#8476)
    Co-authored-by: Pawel Winogrodzki <pawelwi@microsoft.com>

--- SPECS/usbip/extract_usbip.sh
+++ SPECS/usbip/extract_usbip.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-if [ "q$1" == "q" ]; then
-	echo "Usage: $0 <kernel tar path>"
+if [ "q$1" = "q" ]; then
+	echo "Usage: $0 <kernel version> <distro version>"
+	exit 1
+fi
+if [ "q$2" = "q" ]; then
+	echo "Usage: $0 <kernel version> <distro version>"
 	exit 1
 fi
 echo "Extracting linux source"
-tar -xvf $1
+tar -xvf "$1".tar.gz
 if [ "$?" -ne "0" ]; then
 	echo "Error extracting kernel source"
 	exit 1
@@ -12,7 +16,7 @@ fi
 if [ -d "usbip-$1" ]; then
 	rm -rf "usbip-$1"
 fi
-mv "CBL-Mariner-Linux-Kernel-rolling-lts-mariner-2-${1}"/tools/usb/usbip "usbip-$1"
+mv "CBL-Mariner-Linux-Kernel-rolling-lts-mariner-${2}-${1}"/tools/usb/usbip "usbip-$1"
 echo "Creating usbip archive"
 tar -cJvf "usbip-$1".tar.xz "usbip-$1"
 rm -rf "linux-$1"
