7f3d07993d1e8849fae6fa8abcfdc8525d4b4d8c
elainezhao96 <102555676+elainezhao96@users.noreply.github.com>
2024-03-26 10:42:09 -0700

    Image customizer: kernel module API refactor (#8495)

--- toolkit/tools/pkg/imagecustomizerlib/customizeutils.go
+++ toolkit/tools/pkg/imagecustomizerlib/customizeutils.go
@@ -71,7 +71,7 @@ func doCustomizations(buildDir string, baseConfigPath string, config *imagecusto
 		return err
 	}
 
-	err = loadOrDisableModules(config.OS.Modules, imageChroot)
+	err = loadOrDisableModules(config.OS.Modules, imageChroot.RootDir())
 	if err != nil {
 		return err
 	}
@@ -353,33 +353,6 @@ func enableOrDisableServices(services imagecustomizerapi.Services, imageChroot *
 		})
 		if err != nil {
 			return fmt.Errorf("failed to disable service (%s):\n%w", service, err)
-		}
-	}
-
-	return nil
-}
-
-func loadOrDisableModules(modules imagecustomizerapi.Modules, imageChroot *safechroot.Chroot) error {
-	var err error
-
-	for _, module := range modules.Load {
-		logger.Log.Infof("Loading kernel module (%s)", module.Name)
-		moduleFileName := module.Name + ".conf"
-		moduleFilePath := filepath.Join(imageChroot.RootDir(), "/etc/modules-load.d/", moduleFileName)
-		err = file.Write(module.Name, moduleFilePath)
-		if err != nil {
-			return fmt.Errorf("failed to write module load configuration: %w", err)
-		}
-	}
-
-	for _, module := range modules.Disable {
-		logger.Log.Infof("Disabling kernel module (%s)", module.Name)
-		moduleFileName := module.Name + ".conf"
-		moduleFilePath := filepath.Join(imageChroot.RootDir(), "/etc/modprobe.d/", moduleFileName)
-		data := fmt.Sprintf("blacklist %s\n", module.Name)
-		err = file.Write(data, moduleFilePath)
-		if err != nil {
-			return fmt.Errorf("failed to write module disable configuration: %w", err)
 		}
 	}
 
