7609a2b88edfec9324b955df8c515ec9dce76bcc
Sam Meluch <109628994+sameluch@users.noreply.github.com>
2024-04-05 12:28:35 -0700

    Upgrade rabbitmq to Version 3.13.0 (#8670)

--- SPECS/rabbitmq-server/executeRabbitMQTests.sh
+++ SPECS/rabbitmq-server/executeRabbitMQTests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ####   TESTING DISCLAIMER   ####
-# This script follows the steps to run the tests for RabbitMQ v3.11.9 as of 2/22/23 on Azure Linux. As a result, this script can likely 
+# This script follows the steps to run the tests for RabbitMQ v3.13.0 as of 4/1/24 on Azure Linux. As a result, this script can likely 
 # be used as a baseline to execute the test for future releases/upgrades to verify the package integrity. That said, some things may need
 # to be updated in order to make the tests succeed such as updating the links for erlang, elixir, bazel, and/or mandoc in order to have
 # some or all tests succeed.
 #
 # Additionally, there are 3 tests that remain as failutres due to some additional dependencies on large packages 'aws' and 'dotnet', or in
-# one case, an excluded command from openldap 'slapd' (see //SPECS/openldap/openldap.spec for details). With the other 506 test suites 
+# one case, an excluded command from openldap 'slapd' (see //SPECS/openldap/openldap.spec for details). With the other test suites 
 # passing, I decided to leave these as is since they also don't impact the running of the entire test suite.
 #
 # The 3 expected failures are the following:
@@ -15,9 +15,22 @@
 # //deps/rabbitmq_auth_backend_ldap:system_SUITE
 # //deps/rabbitmq_ampq1_0:system_SUITE - shard 1 of 2 specifically
 #
-# RabbitMQ depends on bazel 'latest'. As of 2/22/23, the latest bazel version is '6.0.0'.
+# Additional failures in v3.13.0: 
+# //deps/rabbit:feature_flags_with_unprivileged_user_SUITE-mixed - both shards fail for Assertion error
+# //deps/rabbit:feature_flags_with_unprivileged_user_SUITE - both shards fail for Assertion error
+# //deps/rabbit:feature_flag_v2_SUITE - rpc_calls Assertion error
+# //deps/rabbit:per_vhost_msg_store_SUITE - badmatch(?)
+# //deps/rabbit:per_vhost_msg_store_SUITE-mixed - badmatch(?)
+# //deps/rabbitmq_federation:federation_status_command_SUITE - Condition did not materialize in the expected period of time (aka timeout)
+# //deps/rabbitmq_federation:federation_status_command_SUITE-mixed - Condition did not materialize in the expected period of time (aka timeout)
+
+# Skipped tests:
+# //deps/rabbitmq_cli:check_formatted
+# //deps/rabbitmq_cli:compile_warnings_as_errors
 #
-# Some tests also rely on the 'mandoc' package. As of 2/22/23, this package was not available in Azure Linux. As a result, the package is
+# RabbitMQ depends on bazel 'latest'. This can be updated as needed using the "BAZEL_DEP_VERSION" variable for the purposes of this script
+#
+# Some tests also rely on the 'mandoc' package. As of 4/1/24, this package was not available in Azure Linux. As a result, the package is
 # pulled from source and installed. These test break the entire run, so this dependency must be pulled in for the entire suite to succeed.
 #
 # The elixir package relies on having the UTF-8 character encoding set in your locale in order to function properly. Because of this, you 
@@ -32,6 +45,11 @@
 #### END TESTING DISCLAIMER ####
 
 ####   SCRIPT ASSUMPTIONS   ####
+# This test generates A LOT of files, you will need 2.6M+ inodes to run the full SUITE. (aka about 50GB free in ext4 filesystem)
+#
+# You have pulled the rabbitmq source from github as it contains additional bazel build files which our normal source does not
+# (And checked out the appropriate tag + applied any local patches from Azure Linux)
+#
 # The following line has been added to `$RABBIT_MQ_DIR/.bazelrc`:
 #   build --cache_test_results=no
 #
@@ -51,6 +69,9 @@ DAEMONIZE_DIR="$PARENT_DIR/daemonize"
 
 BAZEL_INSTALL_DIR="/usr/local/bin"
 
+BAZEL_DEP_VERSION="7.1.1"
+MANDOC_DEP_VERSION="1.14.6"
+
 # Store old PATH to restore later
 EXISTING_USER_PATH="$PATH"
 
@@ -69,29 +90,33 @@ echo ""
 # Remove existing/old test directories
 rm -rf $BAZEL_DIR $MANDOC_DIR $DAEMONIZE_DIR
 
+# Ensure rabbitmq deps are installed
+tdnf install -y erlang elixir libxslt xmlto python python3-simplejson zip unzip rsync glibc-lang
+
 # Required dependencies are installed
-dnf install msopenjdk-11 wget git build-essential python3 zip unzip
+tdnf install -y msopenjdk-17 wget git build-essential python python-pip zip unzip kernel-headers binutils zlib-devel
 
 # Get Dependency sources
-wget https://github.com/bazelbuild/bazel/releases/download/6.0.0/bazel-6.0.0-dist.zip
-wget https://mandoc.bsd.lv/snapshots/mandoc-1.14.6.tar.gz
+wget https://github.com/bazelbuild/bazel/releases/download/$BAZEL_DEP_VERSION/bazel-$BAZEL_DEP_VERSION-dist.zip
+wget https://mandoc.bsd.lv/snapshots/mandoc-$MANDOC_DEP_VERSION.tar.gz
 git clone http://github.com/bmc/daemonize.git
 
-# Install bazel 6.0.0
+# Install bazel
 mkdir $BAZEL_DIR
-mv bazel-6.0.0-dist.zip $BAZEL_DIR/bazel-6.0.0-dist.zip
+mv bazel-$BAZEL_DEP_VERSION-dist.zip $BAZEL_DIR/bazel-$BAZEL_DEP_VERSION-dist.zip
 pushd $BAZEL_DIR
-unzip bazel-6.0.0-dist.zip
+unzip bazel-$BAZEL_DEP_VERSION-dist.zip
 env EXTRA_BAZEL_ARGS="--tool_java_runtime_version=local_jdk" bash ./compile.sh
 cp output/bazel $BAZEL_INSTALL_DIR/bazel
 popd
 
-# Install mandoc 1.14.6
+# Install mandoc
 mkdir $MANDOC_DIR
-mv mandoc-1.14.6.tar.gz $MANDOC_DIR/mandoc-1.14.6.tar.gz
+mv mandoc-$MANDOC_DEP_VERSION.tar.gz $MANDOC_DIR/mandoc-$MANDOC_DEP_VERSION.tar.gz
 pushd $MANDOC_DIR
-tar -zxvf mandoc-1.14.6.tar.gz
-cd mandoc-1.14.6
+tar -zxvf mandoc-$MANDOC_DEP_VERSION.tar.gz
+cd mandoc-$MANDOC_DEP_VERSION
+sh configure
 make
 make install
 popd
@@ -114,7 +139,15 @@ popd
 # Run tests using bazel
 pushd $RABBIT_MQ_DIR
 bazel clean
-bazel test //... --test_env="LC_ALL=en_US.UTF-8"
+# --noincompatible_sandbox_hermetic_tmp is required for runs due to flakey(?) failures noticed during
+# runs of this script for when using bazel-7.1.1. These failures had 'ebin' notied as a "dangling
+# symbolic link" causing most tests to not even start without the added flag. Azure Linux does not have 
+# support for older jdk versions nor older bazel versions as a result so we cannot roll back to 6.4.0 
+# before the issue was present upstream.
+# See the below issues for details:
+#   Actual Error: https://github.com/bazelbuild/bazel/issues/20886
+#   Workaround: https://github.com/bazelbuild/bazel/issues/21215
+bazel test //... --test_env="LC_ALL=en_US.UTF-8" --noincompatible_sandbox_hermetic_tmp
 popd
 
 # Restore PATH to original state
