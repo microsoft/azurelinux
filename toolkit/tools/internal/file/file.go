2dcaf034c4db9e1ed464281ed8a8ec4a8480a6e4
Daniel McIlvaney <damcilva@microsoft.com>
2024-04-05 16:10:44 -0700

    Auto clean tdnf cache (#8368)

--- toolkit/tools/internal/file/file.go
+++ toolkit/tools/internal/file/file.go
@@ -177,6 +177,26 @@ func RemoveFileIfExists(path string) (err error) {
 	return
 }
 
+// RemoveDirectoryContents will delete the contents of a directory, but not the
+// directory itself. If the directory does not exist, it will return an error.
+func RemoveDirectoryContents(path string) (err error) {
+	dir, err := os.ReadDir(path)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range dir {
+		childPath := filepath.Join(path, entry.Name())
+		logger.Log.Debugf("Removing (%s)", childPath)
+		err = os.RemoveAll(childPath)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
 // GenerateSHA1 calculates a sha1 of a file
 func GenerateSHA1(path string) (hash string, err error) {
 	file, err := os.Open(path)
