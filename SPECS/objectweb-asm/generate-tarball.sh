e94f9c22dddd2790df51b2d9792a74106fb74de7
Mitch Zhu <mitchzhu@microsoft.com>
2024-04-12 13:32:51 -0700

    objectweb-asm: upgrade to 9.6 (#8711)

--- SPECS/objectweb-asm/generate-tarball.sh
+++ SPECS/objectweb-asm/generate-tarball.sh
@@ -6,20 +6,23 @@ version="$(sed -n 's/Version:\s*//p' *.spec)"
 gittag="ASM_${version//./_}"
 
 # RETRIEVE
-wget "https://gitlab.ow2.org/asm/asm/repository/${gittag}/archive.tar.gz#/${name}-${version}.tar.gz" -O "${name}-${version}.orig.tar.gz"
+wget "https://gitlab.ow2.org/asm/asm/-/archive/${gittag}/asm-${gittag}.tar.gz" -O "${name}-${version}.orig.tar.gz"
 
 rm -rf tarball-tmp
 mkdir tarball-tmp
-cd tarball-tmp
-tar xf "../${name}-${version}.orig.tar.gz"
+pushd tarball-tmp
+tar -xzf "../${name}-${version}.orig.tar.gz"
 
 # Rename dir not to contain commit
-mv asm-${gittag}-* ${name}-${version}
+mv asm-${gittag} ${name}-${version}
 
 # CLEAN TARBALL
 # Remove all jar files
 find -name '*.jar' -delete
+# Remove all class files except those in asm-test, which are shipped alongside appropriately licensed source
+find */asm{,-analysis,-commons} -name '*.class' -delete
+rm -r */gradle
 
-tar cJf "../${name}-${version}.tar.xz" *
-cd ..
+tar -czf "../${name}-${version}.tar.gz" *
+popd
 rm -r tarball-tmp "${name}-${version}.orig.tar.gz"
