d7683fc070dd3d9beedc251585bc89d261d2f47c
Neha Agarwal <58672330+neha170@users.noreply.github.com>
2024-04-30 09:49:00 -0700

    fix log format in go-tools (#8887)

--- toolkit/tools/graphanalytics/graphanalytics.go
+++ toolkit/tools/graphanalytics/graphanalytics.go
@@ -325,9 +325,9 @@ func printSlice(pairList []mapPair, valueDescription string, maxResults int) {
 			break
 		}
 
-		logger.Log.Infof("%d: %s - %d %s", i+1, pair.key, len(pair.value), valueDescription)
+		logger.Log.Infof("%d: (%s) - %d (%s)", i+1, pair.key, len(pair.value), valueDescription)
 		for _, value := range pair.value {
-			logger.Log.Debugf("--> %s", value)
+			logger.Log.Debugf("--> (%s)", value)
 		}
 	}
 }
