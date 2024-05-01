09d4a60b9a8a6b6331eef7f681c052893ae587f9
Mandeep Plaha <mandeepplaha@microsoft.com>
2024-05-01 11:41:53 -0700

    use docker buildx

--- .pipelines/containerSourceData/scripts/BuildBaseContainers.sh
+++ .pipelines/containerSourceData/scripts/BuildBaseContainers.sh
@@ -234,7 +234,7 @@ function docker_build {
     pushd "$build_dir" > /dev/null
 
     echo "+++ Build image: $image_full_name"
-    docker build . \
+    docker buildx build . \
         --build-arg EULA="$EULA_FILE_NAME" \
         --build-arg BASE_IMAGE="$temp_image" \
         -t "$image_full_name" \
@@ -259,7 +259,7 @@ function docker_build_custom {
     pushd "$WORK_DIR" > /dev/null
 
     echo "+++ Build image: $image_full_name"
-    docker build . \
+    docker buildx build . \
         --build-arg BASE_IMAGE="$BASE_IMAGE_NAME" \
         --build-arg FINAL_IMAGE="$final_image_to_use" \
         --build-arg AZL_VERSION="$AZL_VERSION" \
@@ -290,7 +290,7 @@ function docker_build_marinara {
 
     sed -E "s|^FROM mcr\..*installer$|FROM $BASE_BUILDER as installer|g" -i "dockerfile-$MARINARA"
 
-    docker build . \
+    docker buildx build . \
         -t "$MARINARA_IMAGE_NAME" \
         -f dockerfile-$MARINARA \
         --build-arg AZL_VERSION="$AZL_VERSION" \
