a018a17008fe184a0b42124566fccb5bc502e63c
Chris Gunn <chrisgun@microsoft.com>
2024-05-08 16:56:01 -0700

    Image Customizer: Make grub config customization more robust. (#8918)

---
+++ toolkit/tools/internal/grub/commands.go
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package grub
+
+type Command struct {
+	Name string
+	Args []Token
+}
+
+// Split the tokens into lines, using (unescaped) newlines and semicolons as separator tokens.
+// Note: Technically this is incorrect, since some constructs (e.g. "then") supporting having the subsequent command on
+// the same line. But to avoid needing to write a full parser, this code instead assumes that the grub config files are
+// at least somewhat sensibly formatted.
+func SplitTokensIntoLines(tokens []Token) [][]Token {
+	lines := [][]Token(nil)
+	line := []Token(nil)
+
+	for _, token := range tokens {
+		switch token.Type {
+		case NEWLINE, SEMICOLON:
+			if len(line) > 0 {
+				lines = append(lines, line)
+			}
+			line = nil
+
+		default:
+			line = append(line, token)
+		}
+	}
+
+	if len(line) > 0 {
+		lines = append(lines, line)
+	}
+
+	return lines
+}
+
+// IsTokenKeyword checks if a token is keyword.
+func IsTokenKeyword(token Token, keyword string) bool {
+	return token.Type == WORD &&
+		len(token.SubWords) == 1 &&
+		token.SubWords[0].Type == KEYWORD_STRING &&
+		token.SubWords[0].Value == keyword
+}
+
+// FindCommandAll looks for all the lines that contain a command with the provided name.
+func FindCommandAll(lines [][]Token, command string) [][]Token {
+	commandLines := [][]Token(nil)
+
+	for _, line := range lines {
+		if len(line) >= 1 && IsTokenKeyword(line[0], command) {
+			commandLines = append(commandLines, line)
+		}
+	}
+
+	return commandLines
+}
