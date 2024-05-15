e63b8dcc4fd2921883afcdcdd25f9179295cf66b
George Mileka <gmileka@microsoft.com>
2024-05-15 11:41:45 -0700

    Create a PXE bootable iso.

---
+++ toolkit/tools/imagecustomizer/pxe-azl-livenet/artifacts/90azl-livenet/module-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# called by dracut
+check() {
+    return 0
+}
+
+# called by dracut
+depends() {
+    echo livenet
+    return 0
+}
+
+# called by dracut
+install() {
+    inst_simple "$moddir/liveos-artifacts-download.service" "/etc/systemd/system/liveos-artifacts-download.service"
+    inst_script "$moddir/liveos-artifacts-download.sh" "/sbin/liveos-artifacts-download"
+    inst_script "$moddir/schedule-liveos-artifacts-download.sh" "/sbin/schedule-liveos-artifacts-download"
+    inst_hook pre-udev 30 "$moddir/azl-livenet-genrules.sh"
+    # systemctl --root "$initdir" enable liveos-artifacts-download
+    dracut_need_initqueue
+}
