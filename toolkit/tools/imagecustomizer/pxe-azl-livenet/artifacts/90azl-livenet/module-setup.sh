d871402fe26e4de48c54ad41e14f1371b1a360da
George Mileka <gmileka@microsoft.com>
2024-05-16 13:09:28 -0700

    Move the azl-livenet service out of the host systemd folder.

--- toolkit/tools/imagecustomizer/pxe-azl-livenet/artifacts/90azl-livenet/module-setup.sh
+++ toolkit/tools/imagecustomizer/pxe-azl-livenet/artifacts/90azl-livenet/module-setup.sh
@@ -13,7 +13,7 @@ depends() {
 
 # called by dracut
 install() {
-    inst_simple /etc/systemd/system/liveos-artifacts-download.service
+    inst_simple "$moddir/liveos-artifacts-download.service" "/etc/systemd/system/liveos-artifacts-download.service"
     inst_script "$moddir/liveos-artifacts-download.sh" "/sbin/liveos-artifacts-download"
     inst_script "$moddir/schedule-liveos-artifacts-download.sh" "/sbin/schedule-liveos-artifacts-download"
     inst_hook pre-udev 30 "$moddir/azl-livenet-genrules.sh"
