952659ef002da7c125dcf0f2ce5685c57c0dc117
George Mileka <gmileka@microsoft.com>
2024-05-16 12:26:07 -0700

    Fix execute permissions on scripts and also switch to using inst_script

--- toolkit/tools/imagecustomizer/pxe-azl-livenet/artifacts/90azl-livenet/module-setup.sh
+++ toolkit/tools/imagecustomizer/pxe-azl-livenet/artifacts/90azl-livenet/module-setup.sh
@@ -14,9 +14,8 @@ depends() {
 # called by dracut
 install() {
     inst_simple /etc/systemd/system/liveos-artifacts-download.service
-    # todo: consider using: inst_script
-    inst_simple /usr/local/bin/liveos-artifacts-download.sh
-    inst_simple /usr/local/bin/schedule-liveos-artifacts-download.sh
+    inst_script "$moddir/liveos-artifacts-download.sh" "/sbin/liveos-artifacts-download"
+    inst_script "$moddir/schedule-liveos-artifacts-download.sh" "/sbin/schedule-liveos-artifacts-download"
     inst_hook pre-udev 30 "$moddir/azl-livenet-genrules.sh"
     # systemctl --root "$initdir" enable liveos-artifacts-download
     dracut_need_initqueue
