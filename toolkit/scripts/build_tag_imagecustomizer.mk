8f81e78ba75e86b22845871825491d847d5d52ab
Chris Gunn <chrisgun@microsoft.com>
2024-06-04 14:51:08 -0700

    Feedback updates

--- toolkit/scripts/build_tag_imagecustomizer.mk
+++ toolkit/scripts/build_tag_imagecustomizer.mk
@@ -4,12 +4,12 @@
 # Azure Linux Image Customizer version.
 # This is using semantic versioning.
 #
-# IMAGE_CUSTOMIZER_VERSION should have the format:
+# image_customizer_version should have the format:
 #
 #   <major>.<minor>.<patch>
 #
 # and should hold the value of the next (or current) official release, not the previous official
 # release.
-IMAGE_CUSTOMIZER_VERSION ?= 0.3.0
+image_customizer_version ?= 0.3.0
 IMAGE_CUSTOMIZER_VERSION_PREVIEW ?= -dev.$(DATETIME_AS_VERSION)+$(GIT_COMMIT_ID)
-IMAGE_CUSTOMIZER_FULL_VERSION := $(IMAGE_CUSTOMIZER_VERSION)$(IMAGE_CUSTOMIZER_VERSION_PREVIEW)
+image_customizer_full_version := $(image_customizer_version)$(IMAGE_CUSTOMIZER_VERSION_PREVIEW)
