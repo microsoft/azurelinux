246707f0d5345bca477b294414029808bc8473f2
Sharath Srikanth Chellappa <sharathsr@microsoft.com>
2024-06-14 14:26:59 -0700

    Adding patch to libvirt - v15

--- .pipelines/containerSourceData/kubevirt/configuration-files/virt-launcher-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-virtqemud -d
-echo "Virtqemud daemon has successfully been created. Starting virt-launcher process."
-
-/usr/bin/virt-launcher
