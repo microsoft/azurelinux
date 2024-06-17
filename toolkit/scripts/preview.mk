7d8b2b2321c0871267d810ae4abe7223bcb72d65
Daniel McIlvaney <damcilva@microsoft.com>
2024-06-17 13:46:57 -0700

    Fix ordering issues with new preview repo handling in derivative builds (#9400)

--- toolkit/scripts/preview.mk
+++ toolkit/scripts/preview.mk
@@ -18,20 +18,24 @@ preview_repo_source := $(PROJECT_ROOT)/SPECS/azurelinux-repos/azurelinux-officia
 
 # Stop immediately if the preview repo file is missing and cannot be created.
 ifeq ($(wildcard $(preview_repo_source))$(wildcard $(preview_repo)),)
-	$(warning )
-	$(warning ######################### ERROR #########################)
-	$(warning 'USE_PREVIEW_REPO=y' is set but all of the following failed:)
-	$(warning - packaged default '$(preview_repo)' is missing)
-	$(warning - backup source    '$(preview_repo_source)' is missing)
-	$(warning ######################### ERROR #########################)
-	$(error )
+    $(warning )
+    $(warning ######################### ERROR #########################)
+    $(warning 'USE_PREVIEW_REPO=y' is set but all of the following failed:)
+    $(warning - packaged default '$(preview_repo)' is missing)
+    $(warning - backup source    '$(preview_repo_source)' is missing)
+    $(warning ######################### ERROR #########################)
+    $(error )
 endif
 
-# Copy the preview repo file to the expected repo location.
+# Copy the preview repo file to the expected repo location, but only if we  have a target to copy from.
+ifeq ($(wildcard $(preview_repo_source)),)
+$(preview_repo): ;
+else
 $(preview_repo): $(preview_repo_source)
 	@echo "Regenerating preview repo file from '$<' to '$@'"
 	mkdir -p $(dir $@)
 	cp $< $@
+endif
 
 ######## VARIABLE CONFIGURATION ########
 
