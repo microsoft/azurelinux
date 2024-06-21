ae35867d059b42a70f1362ba74eab0cbc208e2b5
Henry Beberman <henry.beberman@microsoft.com>
2024-06-21 22:34:27 +0000

    add repart configs, confirmed working with fsgrowing

--- toolkit/imageconfigs/additionalconfigs/coal.sh
+++ toolkit/imageconfigs/additionalconfigs/coal.sh
@@ -13,7 +13,7 @@ mkdir -p /data/overlays/root/work
 sed -i "s/\/ ext4 defaults/\/ ext4 defaults,ro/" /etc/fstab
 
 # Ensure data partition is mounted in initrd along with overlay
-sed -i "s/data ext4 defaults/data ext4 defaults,x-initrd.mount/" /etc/fstab
+sed -i "s/data ext4 defaults/data ext4 defaults,x-initrd.mount,x-systemd.growfs/" /etc/fstab
 echo "overlay /etc overlay x-initrd.mount,x-systemd.requires-mounts-for=/sysroot/data,lowerdir=/sysroot/etc,upperdir=/sysroot/data/overlays/etc/upper,workdir=/sysroot/data/overlays/etc/work 0 0" >> /etc/fstab
 echo "overlay /home overlay x-initrd.mount,x-systemd.requires-mounts-for=/sysroot/data,lowerdir=/sysroot/home,upperdir=/sysroot/data/overlays/home/upper,workdir=/sysroot/data/overlays/home/work 0 0" >> /etc/fstab
 echo "overlay /var overlay x-initrd.mount,x-systemd.requires-mounts-for=/sysroot/data,lowerdir=/sysroot/var,upperdir=/sysroot/data/overlays/var/upper,workdir=/sysroot/data/overlays/var/work 0 0" >> /etc/fstab
@@ -26,6 +26,9 @@ echo "overlay /root overlay x-initrd.mount,x-systemd.requires-mounts-for=/sysroo
 echo "add_drivers+=\" overlay \"" >> /etc/dracut.conf.d/01-coal.conf
 # Enable systemd-repart in the initrd
 echo "add_dracutmodules+=\" systemd-repart \"" >> /etc/dracut.conf.d/01-coal.conf
+
+mkdir -p /etc/repart.d
+echo -e "[Partition]\nType=esp" > /etc/repart.d/10-coal.conf; echo -e "[Partition]\nType=linux-generic" > /etc/repart.d/11-coal.conf; echo -e "[Partition]\nType=linux-generic" > /etc/repart.d/12-coal.conf; echo -e "[Partition]\nType=linux-generic" > /etc/repart.d/13-coal.conf
 
 # Regenerate initrd with locale in it
 dracut --force --regenerate-all --include /usr/lib/locale /usr/lib/locale
