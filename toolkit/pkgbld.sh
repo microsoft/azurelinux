c8a22d08ab7d608d528dc482e54db47f885d837e
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-30 08:54:33 +0530

    pkgbld: add support for custom args for build
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- toolkit/pkgbld.sh
+++ toolkit/pkgbld.sh
@@ -15,25 +15,31 @@ USEPKGBLDCACHE=y
 helpFuncion () {
 	echo ""
 	echo "Build a package(s) for 3.0 locally:"
-	echo "-h  : Print detailed usage"
-        echo "-s  : Spec Dir [Default SPECS] [Possible values: ../SPECS-EXTENDED, ../SPECS]"
-        echo "-b  : DAILY_BUILD_ID of the format of 3-0-YYYYMMDD [Default 'lkg']"
-        echo "-c  : Run with check disabled/enabled [Default disabled]"
-        echo "-p  : Package to build/rebuild [Default all]"
-        echo "-r  : Refresh worker chroot [Default true]"
-        echo "-f  : Force a Rebuild"
-        echo "-n  : Cleanup input-srpms expand-srpms"
-        echo "-d  : Show the command to be executed."
-	echo ""
+	echo "  -h  : Print detailed usage"
+        echo "  -s  : Spec Dir [Default SPECS] [Possible values: ../SPECS-EXTENDED, ../SPECS]"
+        echo "  -b  : DAILY_BUILD_ID of the format of 3-0-YYYYMMDD [Default 'lkg']"
+        echo "  -c  : Run with check disabled/enabled [Default disabled]"
+	echo "  -p  : Package(s) to build/rebuild [Default all] (space seperated list)"
+        echo "  -r  : Refresh worker chroot [Default true]"
+        echo "  -f  : Force a Rebuild"
+        echo "  -n  : Cleanup input-srpms expand-srpms"
+        echo "  -d  : [DryRun] Show the command to be executed"
 	echo ""
 	echo "Examples:"
 	echo "[inside the toolkit dir]"
-	echo "Build vim & golang locally:"
-	echo "./build.sh -p \"vim golang\""
+	echo ""
+	echo "Build vim & golang-1.18 locally:"
+	echo "  $0 -p \"vim golang-1.18\""
+	echo ""
+	echo "Build vim & golang-1.18 locally with check enabled:"
+	echo "  $0 -c -p \"vim golang-1.18\""
 	echo ""
 	echo "Build cpptest from extended:"
-	echo "./build.sh -s=../SPECS-EXTENDED -p cpptest"
+	echo "  $0 -s ../SPECS-EXTENDED -p cpptest"
 	echo ""
+	echo "Extra build options can be supplied at the end of all the defined options"
+	echo "Ignore tests of packages among the build"
+	echo "  $0 -c -p \"pkg1 pkg2 pkg3\"  -- 'TEST_IGNORE_LIST=\"pkg1 pkg3\"' "
 }
 
 
@@ -41,7 +47,7 @@ while getopts "hs:b:cp:rfnd" opt
 do
 	case "$opt" in
 		h ) helpVar=y;;
-		s ) specsVar="$OPTARG";;
+		s ) specsVar="$OPTARG" ;;
 		b ) buildidVar="$OPTARG";;
 		c ) checkVar=y;;
 		p ) packageVar="$OPTARG";;
@@ -51,6 +57,7 @@ do
 		d ) dryrunVar=y;;
 	esac
 done
+shift $((OPTIND - 1))
 
 if [ -n "$helpVar" ]
 then
@@ -114,7 +121,8 @@ then
 	echo "     REFRESH_WORKER_CHROOT=$WCREFRESH \ ";
 	echo "     USE_PACKAGE_BUILD_CACHE=$USEPKGBLDCACHE \ ";
 	echo "     SPECS_DIR=$SDIR \ ";
-	echo "     -j $(nproc)";
+	echo "     -j $(nproc) \ ";
+	echo "     $@ ";
 	exit 0
 fi
 
@@ -132,4 +140,5 @@ sudo make build-packages \
 	USE_PACKAGE_BUILD_CACHE=$USEPKGBLDCACHE \
 	REPO_LIST=$REPOLIST \
 	SPECS_DIR=$SDIR \
-	-j $(nproc)
+	-j $(nproc) \
+	$@
