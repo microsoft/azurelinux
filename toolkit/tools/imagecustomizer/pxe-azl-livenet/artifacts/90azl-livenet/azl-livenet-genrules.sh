b1f81dc0478907cc53ba171492dbd7e6c1dcd99a
George Mileka <gmileka@microsoft.com>
2024-09-27 11:44:20 -0700

    Tested basic scenario.

--- toolkit/tools/imagecustomizer/pxe-azl-livenet/artifacts/90azl-livenet/azl-livenet-genrules.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -x
-
-systemctl enable liveos-artifacts-download
-/sbin/initqueue --settled --onetime --unique /sbin/schedule-liveos-artifacts-download
--- toolkit/tools/imagecustomizer/pxe-azl-livenet/artifacts/90azl-livenet/schedule-liveos-artifacts-download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -x
-
-systemctl enable liveos-artifacts-download
-systemctl start liveos-artifacts-download &
