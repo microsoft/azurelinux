04ba75a52dc348b119c54cfa5bef467600b31e8d
George Mileka <gmileka@microsoft.com>
2024-09-27 13:04:36 -0700

    Clean-up deploy-pxe-artifacts.sh.

--- toolkit/tools/imagecustomizer/pxe-azl-livenet/deploy-pxe-artifacts.sh
+++ toolkit/tools/imagecustomizer/pxe-azl-livenet/deploy-pxe-artifacts.sh
@@ -16,13 +16,27 @@ mount_dir=/mnt/$(basename $sourceIsoPath)
 
 # ---- constants ----
 
-# required arguments
 tftpbootLocalDir="/var/lib/tftpboot"
 httpLocalDir="/etc/httpd/marineros"
-httpRoot="http://192.168.0.1/marineros"
+httpPlaceHolder="iso-publish-path"
+httpRoot="http://192.168.0.1/marineros/liveos"
 isoRelativePath=liveos/
 
 # ---- helper functions ----
+
+function mountIso() {
+    local isoPath=$1
+    local mountDir=$2
+    sudo rm -rf $mountDir
+    sudo mkdir -p $mountDir
+    sudo mount $isoPath $mountDir
+}
+
+function unmountIso() {
+    local mountDir=$1    
+    sudo umount $mountDir
+    sudo rm -r $mountDir
+}
 
 function clean_tftp_folder() {
     rm -rf $tftpbootLocalDir/boot
@@ -44,26 +58,39 @@ function copy_file () {
 }
 
 function deploy_tftp_folder() {
-    local isoRelativePath=$1
+    local artifactsRootDir=$1
+    local tftpbootLocalDir=$2
 
-    copy_file $mount_dir/efi/boot/grubx64.efi $tftpbootLocalDir/grubx64.efi
-    copy_file $mount_dir/efi/boot/bootx64.efi $tftpbootLocalDir/bootx64.efi
+    copy_file $artifactsRootDir/efi/boot/grubx64.efi $tftpbootLocalDir/grubx64.efi
+    copy_file $artifactsRootDir/efi/boot/bootx64.efi $tftpbootLocalDir/bootx64.efi
 
     mkdir -p $tftpbootLocalDir/boot
-    copy_file $mount_dir/boot/vmlinuz $tftpbootLocalDir/boot/vmlinuz
-    copy_file $mount_dir/boot/initrd.img $tftpbootLocalDir/boot/initrd.img
+    copy_file $artifactsRootDir/boot/vmlinuz $tftpbootLocalDir/boot/vmlinuz
+    copy_file $artifactsRootDir/boot/initrd.img $tftpbootLocalDir/boot/initrd.img
 
     mkdir -p $tftpbootLocalDir/boot/grub2
-    copy_file $mount_dir/boot/grub2/grub-pxe.cfg $tftpbootLocalDir/boot/grub2/grub.cfg
+    copy_file $artifactsRootDir/boot/grub2/grub-pxe.cfg $tftpbootLocalDir/boot/grub2/grub.cfg
 
+    # todo: use variables instead of hard-coding
     sed -i 's/iso-publish-path/192.168.0.1\/marineros\/liveos/g' $tftpbootLocalDir/boot/grub2/grub.cfg
 }
 
 function deploy_http_folder() {
-    local isoRelativePath=$1
+    local sourceIsoPath=$1
+    local isoRelativePath=$2
+    local httpLocalDir=$3
+
     mkdir -p $httpLocalDir/liveos
     chmod 755 $httpLocalDir/liveos
     copy_file $sourceIsoPath $httpLocalDir/$isoRelativePath/$(basename $sourceIsoPath)
+}
+
+function deploy() {
+    local artifactsRootDir=$1
+
+    deploy_http_folder $sourceIsoPath $isoRelativePath $httpLocalDir
+    deploy_tftp_folder $artifactsRootDir $tftpbootLocalDir
+    systemctl restart httpd
 }
 
 function clean() {
@@ -71,27 +98,21 @@ function clean() {
     clean_http_folder
 }
 
-function deploy() {
-    sudo rm -rf $mount_dir
-    sudo mkdir -p $mount_dir
-    sudo mount $sourceIsoPath $mount_dir
-
-    deploy_http_folder $isoRelativePath
-    deploy_tftp_folder $isoRelativePath
-    systemctl restart httpd
-
-    sudo umount $mount_dir
-    sudo rm -r $mount_dir
+function listDeployedFiles() {
+    sudo find $tftpbootLocalDir -type f
+    sudo find $httpLocalDir -type f
 }
 
 # ---- main ----
 
 clean
-deploy
+mountIso $sourceIsoPath $mount_dir
+deploy $mount_dir
+unmountIso $mount_dir
 
+# todo: open only what's necessary
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 
-sudo find $tftpbootLocalDir -type f
-sudo find $httpLocalDir -type f
+listDeployedFiles
