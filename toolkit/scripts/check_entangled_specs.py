66e370f744a694933dd7646560f4c3daf54f7619
Pawel Winogrodzki <pawelwi@microsoft.com>
2024-09-30 16:19:12 -0700

    Fixed spec entanglement PR check (#10585)

--- toolkit/scripts/check_entangled_specs.py
+++ toolkit/scripts/check_entangled_specs.py
@@ -2,14 +2,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from typing import FrozenSet, List, Set
-from pyrpm.spec import Spec
-
-import argparse
 from collections import defaultdict
-from pathlib import Path
+from os import path
+from typing import FrozenSet, List, Set
+import argparse
 import pprint
 import sys
+
+from pyrpm.spec import replace_macros, Spec
 
 version_release_matching_groups = [
     frozenset([
@@ -75,10 +75,10 @@ def check_spec_tags(base_path: str, tags: List[str], groups: List[FrozenSet]) ->
         variants = defaultdict(set)
 
         for spec_filename in group:
-            parsed_spec = Spec.from_file(Path(base_path, spec_filename))
+            parsed_spec = Spec.from_file(path.join(base_path, spec_filename))
             for tag in tags:
-                variants[tag].add(getattr(
-                    parsed_spec, tag))
+                tag_value = get_tag_value(parsed_spec, tag)
+                variants[tag].add(tag_value)
 
         for tag in tags:
             if len(variants[tag]) > 1:
@@ -116,6 +116,13 @@ def check_matches(base_path: str):
             for e in version_release_match_errors:
                 printer.pprint(e)
         sys.exit(1)
+
+
+def get_tag_value(spec: "Spec", tag: str) -> str:
+    value = getattr(spec, tag)
+    if value:
+        value = replace_macros(value, spec)
+    return value
 
 
 if __name__ == '__main__':
