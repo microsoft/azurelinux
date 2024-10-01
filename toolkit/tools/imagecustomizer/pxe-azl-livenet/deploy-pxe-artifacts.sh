35069de783e15fc9d0540319a4a5a1bf9aa7b75a
George Mileka <gmileka@microsoft.com>
2024-10-01 10:27:31 -0700

    Update deploy-pxe-artifacts.sh.

--- toolkit/tools/imagecustomizer/pxe-azl-livenet/deploy-pxe-artifacts.sh
+++ toolkit/tools/imagecustomizer/pxe-azl-livenet/deploy-pxe-artifacts.sh
@@ -15,7 +15,7 @@ sourcePath=$1
 
 # ---- constants ----
 
-tftpbootLocalDir="/var/lib/tftpboot"
+tftpLocalDir="/var/lib/tftpboot"
 httpLocalDir="/etc/httpd/marineros"
 httpRootPlaceHolder="iso-publish-path"
 httpRoot="192.168.0.1/marineros/liveos"
@@ -38,10 +38,10 @@ function unmount_iso() {
 }
 
 function clean_tftp_folder() {
-    rm -rf $tftpbootLocalDir/boot
-    rm -rf $tftpbootLocalDir/liveos
-    rm -f  $tftpbootLocalDir/bootx64.efi
-    rm -f  $tftpbootLocalDir/grubx64.efi
+    rm -rf $tftpLocalDir/boot
+    rm -rf $tftpLocalDir/liveos
+    rm -f  $tftpLocalDir/bootx64.efi
+    rm -f  $tftpLocalDir/grubx64.efi
 }
 
 function clean_http_folder() {
@@ -58,27 +58,27 @@ function copy_file () {
 
 function deploy_tftp_folder() {
     local artifactsRootDir=$1
-    local tftpbootLocalDir=$2
+    local tftpLocalDir=$2
 
-    copy_file $artifactsRootDir/efi/boot/grubx64.efi $tftpbootLocalDir/grubx64.efi
-    copy_file $artifactsRootDir/efi/boot/bootx64.efi $tftpbootLocalDir/bootx64.efi
+    copy_file $artifactsRootDir/efi/boot/grubx64.efi $tftpLocalDir/grubx64.efi
+    copy_file $artifactsRootDir/efi/boot/bootx64.efi $tftpLocalDir/bootx64.efi
 
-    mkdir -p $tftpbootLocalDir/boot
-    copy_file $artifactsRootDir/boot/vmlinuz $tftpbootLocalDir/boot/vmlinuz
-    copy_file $artifactsRootDir/boot/initrd.img $tftpbootLocalDir/boot/initrd.img
+    mkdir -p $tftpLocalDir/boot
+    copy_file $artifactsRootDir/boot/vmlinuz $tftpLocalDir/boot/vmlinuz
+    copy_file $artifactsRootDir/boot/initrd.img $tftpLocalDir/boot/initrd.img
 
-    mkdir -p $tftpbootLocalDir/boot/grub2
+    mkdir -p $tftpLocalDir/boot/grub2
     if [[ -f $artifactsRootDir/boot/grub2/grub-pxe.cfg ]]; then
-        copy_file $artifactsRootDir/boot/grub2/grub-pxe.cfg $tftpbootLocalDir/boot/grub2/grub.cfg
+        copy_file $artifactsRootDir/boot/grub2/grub-pxe.cfg $tftpLocalDir/boot/grub2/grub.cfg
     else
-        copy_file $artifactsRootDir/boot/grub2/grub.cfg $tftpbootLocalDir/boot/grub2/grub.cfg
+        copy_file $artifactsRootDir/boot/grub2/grub.cfg $tftpLocalDir/boot/grub2/grub.cfg
     fi
-    copy_file $artifactsRootDir/boot/grub2/grubenv $tftpbootLocalDir/boot/grub2/grubenv    
+    copy_file $artifactsRootDir/boot/grub2/grubenv $tftpLocalDir/boot/grub2/grubenv    
 
     # replace every '/' with a '\/' to avoid breaking sed search/replace syntax.
     escapedHttpRoot=${httpRoot//\//\\\/}
     set -x
-    sed -i "s/$httpRootPlaceHolder/$escapedHttpRoot/g" $tftpbootLocalDir/boot/grub2/grub.cfg
+    sed -i "s/$httpRootPlaceHolder/$escapedHttpRoot/g" $tftpLocalDir/boot/grub2/grub.cfg
     set +x
 }
 
@@ -97,7 +97,7 @@ function deploy() {
     local sourceIsoPath=$2
 
     deploy_http_folder $sourceIsoPath $isoRelativePath $httpLocalDir
-    deploy_tftp_folder $artifactsRootDir $tftpbootLocalDir
+    deploy_tftp_folder $artifactsRootDir $tftpLocalDir
     systemctl restart httpd
 }
 
@@ -107,7 +107,7 @@ function clean() {
 }
 
 function list_deployed_files() {
-    sudo find $tftpbootLocalDir -type f
+    sudo find $tftpLocalDir -type f
     sudo find $httpLocalDir -type f
 }
 
