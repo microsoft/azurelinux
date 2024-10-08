71d0092f5bf05211e605511b280a089d9f54a8d7
Daniel McIlvaney <damcilva@microsoft.com>
2024-10-08 12:19:23 -0700

    Fallback lazy unmount of toolchain container

--- toolkit/scripts/toolchain/container/unmount_chroot.sh
+++ toolkit/scripts/toolchain/container/unmount_chroot.sh
@@ -8,10 +8,10 @@ if [[ -z "$LFS" ]]; then
 fi
 echo LFS root is: $LFS
 
-umount -v $LFS/dev/pts
-umount -v $LFS/dev
-umount -v $LFS/run
-umount -v $LFS/proc
-umount -v $LFS/sys
+umount -v $LFS/dev/pts || umount -lv $LFS/dev/pts
+umount -v $LFS/dev || umount -lv $LFS/dev
+umount -v $LFS/run || umount -lv $LFS/run
+umount -v $LFS/proc || umount -lv $LFS/proc
+umount -v $LFS/sys || umount -lv $LFS/sys
 rm -v $LFS/dev/console
 rm -v $LFS/dev/null
