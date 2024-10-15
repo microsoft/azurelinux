ef6c344b8384a7b5589a3394f2e87d6447052d03
George Mileka <gmileka@microsoft.com>
2024-10-15 16:42:04 -0700

    [mic][iso] Save input ISO configuration as yaml.
    'SavedConfigs' is a subset of the Image Customizer input configurations that
    needs to be saved on the output media so that it can be used in subsequent
    runs of the Image Customizer against that same output media.
    
    This preservation of input configuration is necessary for subsequent runs if
    the configuration does not result in updating root file system.
    
    For example, if the user specifies a kernel argument that is specific to the
    ISO image, it will not be present in any of the grub config files on the
    root file system - only in the final ISO image grub.cfg. When that ISO image
    is further customized, the root file system grub.cfg migith get re-generated
    and we need to remember to add the ISO specific arguments from the previous
    runs. SavedConfigs is the place where we can store such arguments so we can
    re-apply them.

---
+++ toolkit/tools/pkg/imagecustomizerlib/savedconfigs.go
@@ -0,0 +1,82 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package imagecustomizerlib
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/microsoft/azurelinux/toolkit/tools/imagecustomizerapi"
+	"github.com/microsoft/azurelinux/toolkit/tools/internal/file"
+)
+
+// 'SavedConfigs' is a subset of the Image Customizer input configurations that
+// needs to be saved on the output media so that it can be used in subsequent
+// runs of the Image Customizer against that same output media.
+//
+// This preservation of input configuration is necessary for subsequent runs if
+// the configuration does not result in updating root file system.
+//
+// For example, if the user specifies a kernel argument that is specific to the
+// ISO image, it will not be present in any of the grub config files on the
+// root file system - only in the final ISO image grub.cfg. When that ISO image
+// is further customized, the root file system grub.cfg migith get re-generated
+// and we need to remember to add the ISO specific arguments from the previous
+// runs. SavedConfigs is the place where we can store such arguments so we can
+// re-apply them.
+type SavedConfigs struct {
+	Iso imagecustomizerapi.Iso `yaml:"iso"`
+}
+
+func (c *SavedConfigs) IsValid() (err error) {
+	err = c.Iso.IsValid()
+	if err != nil {
+		return fmt.Errorf("invalid 'iso' field:\n%w", err)
+	}
+
+	return nil
+}
+
+func (c *SavedConfigs) IsEmpty() bool {
+	isoConfigEmpty := c.Iso.KernelCommandLine.ExtraCommandLine == ""
+	return isoConfigEmpty
+}
+
+func (c *SavedConfigs) persistSavedConfigs(savedConfigsFilePath string) (err error) {
+	if c.IsEmpty() {
+		return nil
+	}
+
+	err = os.MkdirAll(filepath.Dir(savedConfigsFilePath), os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to create directory for (%s):\n%w", savedConfigsFilePath, err)
+	}
+
+	err = imagecustomizerapi.MarshalYamlFile(savedConfigsFilePath, c)
+	if err != nil {
+		return fmt.Errorf("failed to persist saved configs file to (%s):\n%w", savedConfigsFilePath, err)
+	}
+
+	return nil
+}
+
+func loadSavedConfigs(savedConfigsFilePath string) (savedConfigs *SavedConfigs, err error) {
+	exists, err := file.PathExists(savedConfigsFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if (%s) exists:\n%w", savedConfigsFilePath, err)
+	}
+
+	if !exists {
+		return nil, nil
+	}
+
+	savedConfigs = &SavedConfigs{}
+	err = imagecustomizerapi.UnmarshalYamlFile(savedConfigsFilePath, savedConfigs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load saved configs file (%s):\n%w", savedConfigsFilePath, err)
+	}
+
+	return savedConfigs, nil
+}
