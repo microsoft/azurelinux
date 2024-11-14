4f880a0977590102ceada254602abc44e514991a
Daniel McIlvaney <damcilva@microsoft.com>
2024-11-14 14:52:54 -0800

    Add comments to json, clean up script

--- toolkit/scripts/prerequisites.sh
+++ toolkit/scripts/prerequisites.sh
@@ -1,11 +1,14 @@
-#!/bin/bash -e
+#!/bin/bash
+
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+
+set -e
 
 prereq_md_path=""
 prereq_json_path=""
 
-function help {
+function print_help {
     echo "Pre-requisite installer. Installs azurelinux pre-requisites for the toolkit."
     echo "Usage:"
     echo '[MANDATORY] -m PATH -> path to the pre-requisite md file'
@@ -22,14 +25,14 @@ while getopts "hpm:s:d:" OPTIONS; do
         s ) prereq_json_path=$OPTARG ;;
         d ) distro=$OPTARG ;;
         p ) do_print="true" ;;
-        h ) help; exit 0 ;;
-        ? ) echo -e "ERROR: INVALID OPTION.\n\n" >&2; help; exit 1 ;;
+        h ) print_help; exit 0 ;;
+        ? ) echo -e "ERROR: INVALID OPTION.\n\n" >&2; print_help; exit 1 ;;
     esac
 done
 
 if [ -z "$prereq_json_path" ]; then
     echo "ERROR: Missing src json file path." >&2
-    help
+    print_help
     exit 1
 fi
 
@@ -40,20 +43,33 @@ fi
 
 if [ -z "$distro" ]; then
     echo "ERROR: Missing distro." >&2
-    help
+    print_help
     exit 1
 fi
 
 if [ "$distro" != "azurelinux" ] && [ "$distro" != "ubuntu" ]; then
     echo "ERROR: Invalid distro. Must be one of 'azurelinux' or 'ubuntu'." >&2
-    help
+    print_help
     exit 1
 fi
 
-# For print mode, just print the pre-requisites and exit early.
+# Load the pre-requisite source json for the distro of interest
+# Json is of the form:
+# {
+#   "packages": [
+#       {
+#           "name": "pkg",
+#           "azurelinux": "pkg-b",
+#           "ubuntu": "pkg-a"
+#       },
+#       ...
+#   ]
+#}
 # A distro specific entry may not exist for every package, so get the entries that do exist, and
 # select the distro specific value. Convert the result to a space separated string.
-src_prereqs=$(jq -r --arg distro "$distro" '[.[] | select(has($distro)) | .[$distro]] | sort | join(" ")' "$prereq_json_path")
+src_prereqs=$(jq -r --arg distro "$distro" '[.packages[] | select(has($distro)) | .[$distro]] | sort | join(" ")' "$prereq_json_path")
+
+# For print mode, just print the pre-requisites and exit early.
 if "$do_print"; then
     echo "$src_prereqs"
     exit 0
@@ -61,7 +77,7 @@ fi
 
 if [ -z "$prereq_md_path" ]; then
     echo "ERROR: Missing pre-requisite file path." >&2
-    help
+    print_help
     exit 1
 fi
 
@@ -70,42 +86,24 @@ if [ ! -f "$prereq_md_path" ]; then
     exit 1
 fi
 
-# Load the pre-requisite source json for the distro of interest
-# Json is of the form:
-# [
-#     {
-#         "name": "pkg",
-#         "azurelinux": "pkg-b",
-#         "ubuntu": "pkg-a"
-#     },
-#     ...
-#]
-
-
+# Get the current entry in the .md file:
+# 1) Remove all line continuations (backslash followed by newline) and replace with a space
 current_prereqs="$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\\\n/ /g' "$prereq_md_path")"
+
+# 2) Remove all leading and trailing whitespace
+current_prereqs=$(echo "$current_prereqs" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+# 3) Find the line that starts with "sudo <pkg manager> -y install ..."
 if [[ "$distro" == "azurelinux" ]]; then
-    # Get the current entry in the .md file:
-    # 1) Remove all line continuations (backslash followed by newline) and replace with a space
-    # 2) Remove all leading and trailing whitespace
-    # 3) Find the line that starts with "sudo tdnf -y install ..."
-    # 4) Grab the package list from the line
-
-    current_prereqs=$(echo "$current_prereqs" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
     current_prereqs=$(echo "$current_prereqs" | grep -oP 'sudo tdnf -y install \K.*')
-    current_prereqs=$(echo "$current_prereqs" | xargs)
 elif [[ "$distro" == "ubuntu" ]]; then
-    # Get the current entry in the .md file:
-    # 1) Remove all line continuations (backslash followed by newline) and replace with a space
-    # 2) Remove all leading and trailing whitespace
-    # 3) Find the line that starts with "sudo apt -y install ..."
-    # 4) Grab the package list from the line
-
-    current_prereqs=$(echo "$current_prereqs" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
     current_prereqs=$(echo "$current_prereqs" | grep -oP 'sudo apt -y install \K.*')
-    current_prereqs=$(echo "$current_prereqs" | xargs)
 fi
 
-# Compare the current and source pre-requisites. Enforce a 1:1 match including order.
+# 4) Grab the package list from the line
+current_prereqs=$(echo "$current_prereqs" | xargs)
+
+# Compare the current and source pre-requisites. Enforce a 1:1 match including order for consistency.
 if [ "$current_prereqs" != "$src_prereqs" ]; then
     echo "ERROR: Pre-requisites do not match. Please update the pre-requisite file."
     echo ""
