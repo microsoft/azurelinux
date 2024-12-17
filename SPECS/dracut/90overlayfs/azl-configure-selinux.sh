05db65dc8565a8226a4d0d919f0bd13e760c12a3
George Mileka <gmileka@users.noreply.github.com>
2024-12-17 11:14:14 -0800

    Enable selinux for liveos iso flow (#11198)

---
+++ SPECS/dracut/90overlayfs/azl-configure-selinux.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
+
+# If SELinux is disabled exit now
+getarg "selinux=0" > /dev/null && return 0
+
+SELINUX="enforcing"
+# shellcheck disable=SC1090
+[ -e "$NEWROOT/etc/selinux/config" ] && . "$NEWROOT/etc/selinux/config"
+[ "$SELINUX" == "disabled" ] && return 0
+
+getargbool 0 rd.live.overlay.overlayfs && overlayfs="yes"
+
+if [ -n "$overlayfs" ]; then
+
+    # Get the current root folder context
+    rootDirContext=$($NEWROOT/usr/sbin/matchpathcon -f $NEWROOT/etc/selinux/targeted/contexts/files/file_contexts -m dir /)
+
+    # Parse the context to extract the root folder '/' context type.
+    # The line should be on the form: "/       system_u:object_r:root_t:s0"
+
+    # Split folder and context
+    IFS='\t' read -r _ selinuxContext <<< "$rootDirContext"
+    echo "root folder context: ($selinuxContext)"
+
+    # Split context and extract its type
+    IFS=':' read -r _ _ contextType _ <<< "$selinuxContext"
+    echo "root folder label  : ($contextType)"
+
+    # Set the type on the target folders
+    [ -e /sysroot ] && chcon -t $contextType /sysroot
+    [ -e /run/overlayfs ] && chcon -t $contextType /run/overlayfs
+    [ -e /run/ovlwork ] && chcon -t $contextType /run/ovlwork
+fi
