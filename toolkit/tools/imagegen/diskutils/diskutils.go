088093fd092e2de8945a2391d8e28bf58d13ad6b
Archana Choudhary <36061892+arc9693@users.noreply.github.com>
2024-12-19 21:08:20 +0530

    fix: ISO installer encrypted root doesn't boot (#11324)

--- toolkit/tools/imagegen/diskutils/diskutils.go
+++ toolkit/tools/imagegen/diskutils/diskutils.go
@@ -32,7 +32,9 @@ var (
 	DefaultMkfsOptions = map[string][]string{
 		"ext2": {"-b", "4096", "-O", "none,sparse_super,large_file,filetype,resize_inode,dir_index,ext_attr"},
 		"ext3": {"-b", "4096", "-O", "none,sparse_super,large_file,filetype,resize_inode,dir_index,ext_attr,has_journal"},
-		"ext4": {"-b", "4096", "-O", "none,sparse_super,large_file,filetype,resize_inode,dir_index,ext_attr,has_journal,extent,huge_file,flex_bg,metadata_csum,64bit,dir_nlink,extra_isize"},
+		// grub2 doesn't recognize ext4 with metadata_csum_seed enabled
+		// ^metadata_csum_seed disables filesystem to store the metadata checksum seed in the superblock, hence disables changing uuid of mounted filesystem
+		"ext4": {"-b", "4096", "-O", "none,sparse_super,large_file,filetype,resize_inode,dir_index,ext_attr,has_journal,extent,huge_file,flex_bg,metadata_csum,64bit,dir_nlink,extra_isize,^metadata_csum_seed"},
 	}
 
 	partedVersionRegex = regexp.MustCompile(`^parted \(GNU parted\) (\d+)\.(\d+)`)
