40687a79886dd96ef0c2bf43e14d8628f12355d3
Binu Jose Philip <bphilip@microsoft.com>
2025-01-29 16:59:50 +0000

    rename xpmem and knem singed packages

--- toolkit/scripts/check_entangled_specs.py
+++ toolkit/scripts/check_entangled_specs.py
@@ -60,7 +60,7 @@ version_release_matching_groups = [
         "SPECS/isert/isert.spec"
     ]),
     frozenset([
-        "SPECS-SIGNED/knem-signed/knem-modules-signed.spec",
+        "SPECS-SIGNED/knem-modules-signed/knem-modules-signed.spec",
         "SPECS/knem/knem.spec"
     ]),
     frozenset([
@@ -80,7 +80,7 @@ version_release_matching_groups = [
         "SPECS/srp/srp.spec"
     ]),
     frozenset([
-        "SPECS-SIGNED/xpmem-signed/xpmem-modules-signed.spec",
+        "SPECS-SIGNED/xpmem-modules-signed/xpmem-modules-signed.spec",
         "SPECS/xpmem/xpmem.spec"
     ])
 
