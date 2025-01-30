e6532426baec03c4cacd449dd95eae537de92c58
Mykhailo Bykhovtsev <mbykhovtsev@microsoft.com>
2025-01-29 16:00:37 -0800

    add vendor version parameter

--- SPECS/influx-cli/generate_source_tarball.sh
+++ SPECS/influx-cli/generate_source_tarball.sh
@@ -16,6 +16,7 @@ OUT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #                 and should be replaced with the new/modified src code
 # --outFolder   : folder where to copy the new tarball(s)
 # --pkgVersion  : package version
+# --vendorVersion : vendor version
 #
 PARAMS=""
 while (( "$#" )); do
@@ -47,6 +48,15 @@ while (( "$#" )); do
             exit 1
         fi
         ;;
+         --vendorVersion)
+        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+            VENDOR_VERSION=$2
+            shift 2
+        else
+            echo "Error: Argument for $1 is missing" >&2
+            exit 1
+        fi
+        ;;
         -*|--*=) # unsupported flags
         echo "Error: Unsupported flag $1" >&2
         exit 1
@@ -58,9 +68,10 @@ while (( "$#" )); do
   esac
 done
 
-echo "--srcTarball   -> $SRC_TARBALL"
-echo "--outFolder    -> $OUT_FOLDER"
-echo "--pkgVersion   -> $PKG_VERSION"
+echo "--srcTarball      -> $SRC_TARBALL"
+echo "--outFolder       -> $OUT_FOLDER"
+echo "--pkgVersion      -> $PKG_VERSION"
+echo "--vendorVersion   -> $VENDOR_VERSION"
 
 if [ -z "$PKG_VERSION" ]; then
     echo "--pkgVersion parameter cannot be empty"
@@ -102,4 +113,4 @@ tar  --sort=name \
      -cf "$VENDOR_TARBALL" vendor
 
 popd > /dev/null
-echo "$PKG_NAME vendored modules are available at $VENDOR_TARBALL and static assets in $STATIC_ASSETS_TARBALL"
+echo "$PKG_NAME vendored modules are available at $VENDOR_TARBALL"
