e8f3c60852345359e02f088ae42f7afdde591b42
Binu Jose Philip <bphilip@microsoft.com>
2025-02-26 15:51:30 +0000

    refactor check-entrangled-specs and add unit test

--- toolkit/scripts/check_entangled_specs.py
+++ toolkit/scripts/check_entangled_specs.py
@@ -11,6 +11,10 @@ import sys
 
 from pyrpm.spec import replace_macros, Spec
 
+# Control output verbosity, keeping this module global since we do not
+# have a containing top-level scope
+verbose=False
+
 version_release_matching_groups = [
     frozenset([
         "SPECS-SIGNED/kernel-signed/kernel-signed.spec",
@@ -147,104 +151,53 @@ oot_kmodule_matching_groups = [
     ])
 ]
 
-def check_spec_tags(base_path: str, tags: List[str], groups: List[FrozenSet]) -> Set[FrozenSet]:
-    """Returns spec sets which violate matching rules for given tags. """
-    err_groups = set()
+def check_spec_tags(base_path: str, tags: List[str], groups: List[FrozenSet], tag_expected={}) -> bool:
+    """Check if spec set violates matching rules for any of given tags. Return True/False accordingly."""
+    has_error = False
     for group in groups:
-        variants = defaultdict(set)
-
+        if verbose: 
+            print(f"Processing group: {group}")
+        spec_tag_map = {tag: {} for tag in tags}
         for spec_filename in group:
             parsed_spec = Spec.from_file(path.join(base_path, spec_filename))
+            if verbose: print(f"\t{spec_filename}")
             for tag in tags:
                 tag_value = get_tag_value(parsed_spec, tag)
-                variants[tag].add(tag_value)
+                spec_tag_map[tag][spec_filename] = tag_value
+                if verbose:
+                    print(f"\t\ttag({tag}) value: {tag_value}")
+        
+        for tag, specs_values in spec_tag_map.items():
+            # Skip to next tag if tag value is unique and it matches "tag_expected_value" if set
+            value_list = list(specs_values.values())
+            if len(set(value_list)) > 1 or (tag_expected and value_list[0] != tag_expected[tag]):
+                has_error = True
+                print(f'Mismatch in expected value of "{tag}":{tag_expected[tag] if tag_expected else ""}')
+                for spec_name, value in specs_values.items():
+                    print(f"\t{value:30} => {spec_name}")
 
-        for tag in tags:
-            if len(variants[tag]) > 1: err_groups.add(group)
-    return err_groups
+    return has_error
 
-
-def check_oot_kmodules(base_path: str, tag: str, groups: List[FrozenSet]) -> Set[FrozenSet]:
-    """Returns OOT kernel modules which violate matching with kernel-headers version. """
-    err_groups = set()
-
+def check_matches(base_path: str):
     kernel_headers_spec = Spec.from_file(path.join(base_path, "SPECS/kernel-headers/kernel-headers.spec"))
     kernel_headers_version = get_tag_value(kernel_headers_spec, 'version')
     kernel_headers_release = get_tag_value(kernel_headers_spec, 'release')
     kernel_version_release = f"{kernel_headers_version}-{kernel_headers_release}"
-
-    for group in groups:
-        for spec_filename in group:
-            parsed_spec = Spec.from_file(path.join(base_path, spec_filename))
-            tag_value = get_tag_value(parsed_spec, tag)
-            if tag_value != kernel_version_release:
-                print(f"kernel version mismatch: {spec_filename} {tag_value} {kernel_version_release}")
-                err_groups.add(spec_filename)
-    return err_groups
-
-def check_mstflintver_match_groups(base_path: str) -> Set[FrozenSet]:
-    return check_spec_tags(base_path, ['mstflintver'], mstflintver_matching_groups)
-
-def check_sdkver_match_groups(base_path: str) -> Set[FrozenSet]:
-    return check_spec_tags(base_path, ['sdkver'], sdkver_matching_groups)
-
-def check_version_release_match_groups(base_path: str) -> Set[FrozenSet]:
-    return check_spec_tags(base_path, ['epoch', 'version', 'release'], version_release_matching_groups)
-
-def check_version_match_groups(base_path: str) -> Set[FrozenSet]:
-    return check_spec_tags(base_path, ['epoch', 'version'], version_matching_groups)
-
-def check_oot_kmodule_matching_groups(base_path: str) -> Set[FrozenSet]:
-    return check_oot_kmodules(base_path, 'last-known-kernel', oot_kmodule_matching_groups)
-
-def check_matches(base_path: str):
-    version_match_errors = check_version_match_groups(base_path)
-    version_release_match_errors = check_version_release_match_groups(base_path)
-    sdkver_match_errors = check_sdkver_match_groups(base_path)
-    mstflintver_match_errors = check_mstflintver_match_groups(base_path)
-    oot_kmodule_match_errors = check_oot_kmodule_matching_groups(base_path)
-
-    printer = pprint.PrettyPrinter()
-
-    if len(version_match_errors) or \
-       len(version_release_match_errors) or \
-       len(sdkver_match_errors) or \
-       len(mstflintver_match_errors) or \
-       len(oot_kmodule_match_errors):
-        print('The current repository state violates a spec entanglement rule!')
-
-        if len(version_match_errors):
-            print(
-                '\nPlease update the following sets of specs to have the same "Epoch" and "Version" tags:')
-            for e in version_match_errors:
-                printer.pprint(e)
-
-        if len(version_release_match_errors):
-            print(
-                '\nPlease update the following sets of specs to have the same "Epoch", "Version", and "Release" tags:')
-            for e in version_release_match_errors:
-                printer.pprint(e)
-
-        if len(sdkver_match_errors):
-            print(
-                '\nPlease update the following sets of specs to have the same "sdkver" global variables:')
-            for e in sdkver_match_errors:
-                printer.pprint(e)
-
-        if len(mstflintver_match_errors):
-            print(
-                '\nPlease update the following sets of specs to have the same "mstflintver" global variables:')
-            for e in mstflintver_match_errors:
-                printer.pprint(e)
-
-        if len(oot_kmodule_match_errors):
-            print(
-                '\nPlease update the following sets of specs to match the "last-known-kernel" global variable with kernel-headers "version":')
-            for e in oot_kmodule_match_errors:
-                printer.pprint(e)
-
+    
+    groups_to_check = [(['mstflintver'], mstflintver_matching_groups, None),
+                       (['sdkver'], sdkver_matching_groups, None),
+                       (['epoch', 'version', 'release'], version_release_matching_groups, None),
+                       (['epoch', 'version'], version_matching_groups, None),
+                       (['last-known-kernel'], oot_kmodule_matching_groups,
+                         {'last-known-kernel' : kernel_version_release})]
+    
+    check_result = []
+    for check_args in groups_to_check:
+        check_result.append(check_spec_tags(base_path, *check_args))
+    if any(check_result):
+        print('The current repository state violates one or more spec entanglement rule!')
         sys.exit(1)
-
+    print('Repository state is consistent with spec entanglement rules.')
 
 def get_tag_value(spec: "Spec", tag: str) -> str:
     value = getattr(spec, tag)
@@ -252,10 +205,15 @@ def get_tag_value(spec: "Spec", tag: str) -> str:
         value = replace_macros(value, spec)
     return value
 
+def main():
+    global verbose
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('repo_root', help='path to the root of the Azure Linux repository')
+    parser.add_argument ("--verbose", action="store_true", help='Print details about each action')
+    args = parser.parse_args()
+    verbose = args.verbose
+    check_matches(args.repo_root)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'repo_root', help='path to the root of the Azure Linux repository')
-    args = parser.parse_args()
-    check_matches(args.repo_root)
+    main()
