61f245af18d3031a8a0e79ed31fd67eac0a9b6f3
Mykhailo Bykhovtsev <mbykhovtsev@microsoft.com>
2025-03-25 10:31:27 -0700

    fixed git initialize repo

--- toolkit/scripts/vendor_tools/build_git_submodules_vendor.sh
+++ toolkit/scripts/vendor_tools/build_git_submodules_vendor.sh
@@ -9,6 +9,9 @@ PKG_VERSION=""
 SRC_TARBALL=""
 OUT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VENDOR_VERSION="1"
+GIT_URL=""
+
+source "$OUT_FOLDER/common.sh"
 
 # parameters:
 #
@@ -58,6 +61,15 @@ while (( "$#" )); do
             exit 1
         fi
         ;;
+        --gitUrl)
+        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+            GIT_URL=$2
+            shift 2
+        else
+            echo "Error: Argument for $1 is missing" >&2
+            exit 1
+        fi
+        ;;
         -*|--*=) # unsupported flags
         echo "Error: Unsupported flag $1" >&2
         exit 1
@@ -69,111 +81,103 @@ while (( "$#" )); do
   esac
 done
 
-echo "--srcTarball      -> $SRC_TARBALL"
-echo "--outFolder       -> $OUT_FOLDER"
-echo "--pkgVersion      -> $PKG_VERSION"
-echo "--vendorVersion   -> $VENDOR_VERSION"
+log "${LOG_LEVEL:-debug}" "--srcTarball             -> $SRC_TARBALL"
+log "${LOG_LEVEL:-debug}" "--outFolder              -> $OUT_FOLDER"
+log "${LOG_LEVEL:-debug}" "--pkgVersion             -> $PKG_VERSION"
+log "${LOG_LEVEL:-debug}" "--vendorVersion          -> $VENDOR_VERSION"
+log "${LOG_LEVEL:-debug}" "--submoduleDirectoryName -> $SUBMODULE_DIRECTORY_NAME"
+log "${LOG_LEVEL:-debug}" "--gitUrl                 -> $GIT_URL"
+
+if [ -z "$SRC_TARBALL" ]; then
+    log "${LOG_LEVEL:-error}" "--srcTarball parameter cannot be empty"
+    exit 1
+fi
 
 if [ -z "$PKG_VERSION" ]; then
-    echo "--pkgVersion parameter cannot be empty"
+    log "${LOG_LEVEL:-error}" "--pkgVersion parameter cannot be empty"
     exit 1
 fi
 
 if [ -z "$VENDOR_VERSION" ]; then
-    echo "--vendorVersion parameter cannot be empty"
+    log "${LOG_LEVEL:-error}" "--vendorVersion parameter cannot be empty"
     exit 1
 fi
+
+if [ -z "$GIT_URL" ]; then
+    log "${LOG_LEVEL:-error}" "--gitUrl parameter cannot be empty"
+    exit 1
+fi
+
+trap cleanup EXIT
+TARBALL_SUFFIX="submodules"
+VENDOR_ROOT_FINDER_FILE_NAME=""
+VENDOR_FOLDER_NAME=""
+read -r PKG_NAME PKG_VERSION < <(get_name_version "$SRC_TARBALL")
 
 function generate_vendor {
     local pkg_name=$1
     local pkg_version=$2
-    local checkout_tags=$3
-    local folder_name=$4
-    local git_url=$5
+    local git_url=$3
+
+    log "${LOG_LEVEL:-debug}" "Fetching git submodules for $pkg_name"
 
     # makesure we get the correct git url
-    local git_url_clone
-    git_url_clone=$(echo "$git_url" | grep -oP '^https://github\.com/[^/]+/[^/]+')
+    local git_origin_url
+    git_origin_url=$(echo "$git_url" | grep -oP '^https://github\.com/[^/]+/[^/]+')
 
     local branch_name="$pkg_name-$pkg_version"
+    local remote_name="origin"
 
-    echo "cloning $git_url_clone"
+    local current_dir
+    current_dir=$(pwd)
 
-    git clone --depth 1 "$git_url_clone" -b "$pkg_name-$pkg_version"
+    log "${LOG_LEVEL:-debug}" "using remote git url $git_origin_url"
 
-    pushd $folder_name &> /dev/null
+    git init
+    git remote add "$remote_name" "$git_origin_url"
+    git fetch --tags --depth 1 "$remote_name"
 
-    if [[ "$checkout_tags" = "true" ]]; then
-        git fetch --all --tags
+    local checkout_name="tags/v$pkg_version"
+    log "${LOG_LEVEL:-debug}" "checking out $checkout_name"
+    git checkout "$checkout_name" -b "$branch_name" -f
 
-        echo "checkout tags $branch_name and make branch $branch_name"
-        git checkout tags/v"$pkg_version" -b "$branch_name"
-    fi
-
-    echo "fetching submodules with depth 1, recursive"
+    log "${LOG_LEVEL:-debug}" "fetching submodules with depth 1, recursive"
     git submodule update --depth 1 --init --recursive
-    popd
 
-    echo "move $folder_name to $branch_name"
-    # move the vendored files to the correct location
-    mv $folder_name $branch_name
+    local submodules_list
+    submodules_list=($(git config --file .gitmodules --get-regexp path | awk '{ print $2 }'))
+
+    log "${LOG_LEVEL:-debug}" "removing .git folder from"
+    find "$current_dir" -name ".git" | xargs rm -rf
+
+    local another_temp_dir
+    another_temp_dir=$(mktemp -d)
+
+    log "${LOG_LEVEL:-debug}" "copying all submodules to $another_temp_dir"
+    for submodule in "${submodules_list[@]}"; do
+        log "${LOG_LEVEL:-debug}" "removing $submodule"
+        cp -r "$submodule" "$another_temp_dir"
+    done
+
+    popd > /dev/null
+
+    log "${LOG_LEVEL:-debug}" "deleting all files except submodules"
+    rm -rf "${current_dir:?}/"*
+
+    mv "$another_temp_dir"/* "$current_dir"
+    rm -rf "$another_temp_dir"
+
+    popd > /dev/null
 }
 
-echo "-- create temp folder"
-TEMP_DIR=$(mktemp -d)
-function cleanup {
-    echo "cleanup -> remove $TEMP_DIR"
-    rm -rf $TEMP_DIR
-}
-trap cleanup EXIT
+common_setup "$SRC_TARBALL" "$VENDOR_VERSION" "$TARBALL_SUFFIX" "$VENDOR_ROOT_FINDER_FILE_NAME" "$OUT_FOLDER"
 
-pushd $TEMP_DIR > /dev/null
+VENDOR_FOLDER_NAME=$(pwd)
 
-TARBALL_NAME=$(basename "$SRC_TARBALL")
+generate_vendor "$PKG_NAME" "$PKG_VERSION" "$GIT_URL"
 
-NAME_VER=${TARBALL_NAME%.*}
-if [[ "$NAME_VER" =~ \.tar$ ]]
-then
-    NAME_VER=${NAME_VER%.*}
-fi
+create_vendor_tarball "$VENDOR_TARBALL" "$VENDOR_FOLDER_NAME" "$OUT_FOLDER"
 
-VENDOR_TARBALL="$NAME_VER-cargovendor-v$VENDOR_VERSION.tar.gz"
 
-if [[ -f "$TARBALL_NAME" ]]
-then
-    cp "$SRC_TARBALL" "$TEMP_DIR"
-else
-    echo "Tarball '$TARBALL_NAME' doesn't exist. Will attempt to download from blobstorage."
-    if ! wget -q "https://azurelinuxsrcstorage.blob.core.windows.net/sources/core/$TARBALL_NAME" -O "$TEMP_DIR/$TARBALL_NAME"
-    then
-        echo "ERROR: failed to download the source tarball."
-        exit 1
-    fi
-    echo "Download successful."
-fi
 
-echo "Unpacking source tarball..."
-tar -xf $SRC_TARBALL
 
-echo "Vendor cargo ..."
-DIRECTORY_NAME=($(ls -d */))
-
-# assume there is only one directory in the tarball
-DIRECTORY_NAME=${DIRECTORY_NAME[0]%//}
-
-pushd "$DIRECTORY_NAME" &> /dev/null
-echo "Fetching dependencies to a temporary cache in $DIRECTORY_NAME."
-generate_vendor $PKG_NAME $PKG_VERSION $CHECKOUT_TAGS $SUBMODULE_DIRECTORY_NAME
-
-echo ""
-echo "========================="
-echo "Tar vendored tarball"
-tar  --sort=name \
-     --mtime="2021-04-26 00:00Z" \
-     --owner=0 --group=0 --numeric-owner \
-     --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime \
-     -I pigz -cf "$VENDOR_TARBALL" $SUBMODULE_DIRECTORY_NAME
-
-cp $VENDOR_TARBALL $OUT_FOLDER
-popd > /dev/null
-echo "$NAME_VER vendored modules are available at $VENDOR_TARBALL"
