a0193be3d8ae5357cc907d7f0b07e1cd68bc1f48
Dallas Delaney <dadelan@microsoft.com>
2025-06-09 14:48:00 -0700

    add copyrights to scripts

--- toolkit/imageconfigs/scripts/linuxguard/duid-type-to-link-layer.sh
+++ toolkit/imageconfigs/scripts/linuxguard/duid-type-to-link-layer.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 sed -i 's/#DUIDType=vendor/DUIDType=link-layer/' /etc/systemd/networkd.conf
--- toolkit/imageconfigs/scripts/linuxguard/performance-tuning.sh
+++ toolkit/imageconfigs/scripts/linuxguard/performance-tuning.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 set -euxo pipefail
 
 # Remove the dracut-cmdline-ask service because it isn't used and slows down
