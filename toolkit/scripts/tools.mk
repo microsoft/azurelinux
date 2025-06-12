e2779c52d58f1cf174e75a43cf0aba846f109f38
Sam Meluch (from Dev Box) <sammeluch@microsoft.com>
2025-06-11 18:54:13 -0700

    fix formatting

--- toolkit/scripts/tools.mk
+++ toolkit/scripts/tools.mk
@@ -24,11 +24,13 @@ go_current_version = $(shell go version | awk '{print $$3}')
 go_version_check = $(shell printf '%s\n%s\n' "$(go_min_version)" "$(go_current_version)" | sort -V | head -n1)
 ifneq ($(go_version_check),$(go_min_version))
 $(error Go version '$(go_current_version)' is less than minimum required version '$(go_min_version)' \
--------------------------------------------------------------------------------------------------- \
-| If this is a personal machine, use 'sudo make install-prereqs' to get the latest dependencies. | \
-| If this is an automated system, consider 'sudo make install-prereqs-and-configure'             | \
-|     to get the latest dependencies and configure go symlinks and docker user.                  | \
---------------------------------------------------------------------------------------------------)
+----------------------------------------------------------------------- \
+| If this is a personal machine,                                      | \
+|     use 'sudo make install-prereqs' to get the latest dependencies. | \
+| If this is an automated system,                                     | \
+|     consider 'sudo make install-prereqs-and-configure' to get the   | \
+|     latest dependencies and configure go symlinks and docker user.  | \
+----------------------------------------------------------------------)
 endif
 endif
 
