78a2a0246e4371dedfd086084fef9707f7942ff8
Your Name <you@example.com>
2025-07-03 00:17:11 -0700

    Build 64k aarch64 ISO using imagecustomizer

--- toolkit/imageconfigs/postinstallscripts/imagecustomizer/isoinstaller_postinstalltask.sh
+++ toolkit/imageconfigs/postinstallscripts/imagecustomizer/isoinstaller_postinstalltask.sh
@@ -44,3 +44,7 @@ createrepo /RPMS
 # RPM packages are generated under rootfs RPMS folder.
 # Hence update baseurl path 
 sed -i 's|baseurl=file:///mnt/cdrom/RPMS|baseurl=file:///RPMS|' /etc/yum.repos.d/mariner-iso.repo
+
+# RPM packages should be installed from ISO local repo
+# Remove PMC official base repo from ISO
+rm -r /etc/yum.repos.d/azurelinux-official-base.repo
