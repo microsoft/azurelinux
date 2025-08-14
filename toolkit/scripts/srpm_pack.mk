02e821190d87b0f5fe2e470e84d991a4ea49606a
Manuel Huber <mahuber@microsoft.com>
2025-08-14 11:10:17 -0700

    hack test work pawel #2

--- toolkit/scripts/srpm_pack.mk
+++ toolkit/scripts/srpm_pack.mk
@@ -89,7 +89,6 @@ $(STATUS_FLAGS_DIR)/build_srpms.flag: $(chroot_worker) $(local_specs) $(local_sp
 		--azure-client-id=$(AZURE_CLIENT_ID) \
 		--build-dir=$(SRPM_BUILD_CHROOT_DIR) \
 		--signature-handling=$(SRPM_FILE_SIGNATURE_HANDLING) \
-		--worker-tar=$(chroot_worker) \
 		$(if $(filter y,$(RUN_CHECK)),--run-check) \
 		$(if $(SRPM_PACK_LIST),--pack-list="$(SRPM_PACK_LIST)") \
 		--log-file=$(SRPM_BUILD_LOGS_DIR)/srpmpacker.log \
