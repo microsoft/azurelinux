dfc0349f8343bf9615e34c0508d2e4f57bfc4f21
Pawel Winogrodzki <pawelwi@microsoft.com>
2025-09-20 15:59:50 -0700

    Optimized build time.

--- toolkit/tools/scheduler/schedulerutils/implicitprovides.go
+++ toolkit/tools/scheduler/schedulerutils/implicitprovides.go
@@ -37,23 +37,26 @@ func InjectMissingImplicitProvides(res *BuildResult, pkgGraph *pkggraph.PkgGraph
 			return
 		}
 
+		didInjectAny = didInjectAny || len(provideToNodes) > 0
+
 		for provide, nodes := range provideToNodes {
-			err = replaceNodesWithProvides(res, pkgGraph, provide, nodes, rpmFile)
+			err = replaceNodesWithProvides(pkgGraph, provide, nodes, rpmFile)
 			if err != nil {
 				return
 			}
-
-			didInjectAny = true
 		}
 	}
 
-	// Make sure the graph is still a directed acyclic graph (DAG) after manipulating it.
-	err = pkgGraph.MakeDAG()
+	if didInjectAny {
+		// Make sure the graph is still a directed acyclic graph (DAG) after manipulating it.
+		err = pkgGraph.MakeDAG()
+	}
+
 	return
 }
 
 // replaceNodesWithProvides will replace a slice of nodes with a new node with the given provides in the graph.
-func replaceNodesWithProvides(res *BuildResult, pkgGraph *pkggraph.PkgGraph, provides *pkgjson.PackageVer, nodes []*pkggraph.PkgNode, rpmFileProviding string) (err error) {
+func replaceNodesWithProvides(pkgGraph *pkggraph.PkgGraph, provides *pkgjson.PackageVer, nodes []*pkggraph.PkgNode, rpmFileProviding string) (err error) {
 	var parentNode *pkggraph.PkgNode
 
 	// Find a local run node that is backed by the same rpm as the one providing the implicit provide.
