83a6ba47e04681bf688c64e554cd4a80e1c81e76
Muhammad Falak R Wani <falakreyaz@gmail.com>
2026-09-11 22:24:26 +0530

    toolkit: fix encrypted ISO installer hang (#18795)
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- toolkit/tools/imagegen/diskutils/diskutils.go
+++ toolkit/tools/imagegen/diskutils/diskutils.go
@@ -589,8 +589,9 @@ func CreatePartitions(diskDevPath string, disk configuration.Disk, rootEncryptio
 		partIDToFsTypeMap[partition.ID] = partFsType
 	}
 
-	// Wait for the disk's metadata to populate.
-	err = RefreshPartitions(diskDevPath)
+	// sfdisk already updated the partition table. LUKS/LVM may now hold
+	// partions open, so wait for metadata without issuing BLKRRPART
+	err = WaitForDiskDevice(diskDevPath)
 	if err != nil {
 		return
 	}
@@ -1110,7 +1111,7 @@ func requestKernelRereadPartitionTable(diskDevPath string) error {
 
 	waitTime := 125 * time.Millisecond
 	retries := 10
-	for i := 0; ; i = 1 {
+	for i := 0; ; i++ {
 		_, _, errno := unix.Syscall(unix.SYS_IOCTL, diskFile.Fd(), unix.BLKRRPART, 0)
 		switch {
 		case errno == unix.EBUSY && i < retries:
